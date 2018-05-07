74963a3a21fae827934d3482b208c7678ced8650
Akira Komamura <akira.komamura@gmail.com>
2018-05-08 08:21:15 +0900

    Log init-time to a file

--- misc/init-startup.el
+++ misc/init-startup.el
@@ -1,11 +1,27 @@
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (message "Emacs ready in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)))
-
+(use-package uptimes)
 (use-package esup)
+
+(defcustom akirak/init-time-log-file nil
+  "Log file to record a history of `emacs-init-time'.")
+
+(defun akirak/log-init-time ()
+  "Record the Emacs initialization time to a text log file."
+  (let ((log-file akirak/init-time-log-file)
+        (init-time (emacs-init-time)))
+    ;; Save to the log file if and only if its parent directory exists
+    (when (and (stringp akirak/init-time-log-file)
+               (file-directory-p (file-name-directory log-file)))
+      (with-temp-buffer
+        (insert (format-time-string "%FT%R%:z") ","
+                (format "%s@%s" user-login-name (system-name)) ","
+                init-time "\n")
+        (append-to-file nil nil log-file)))
+    (message "Emacs ready in %s with %d garbage collections."
+             init-time gcs-done)))
+(add-hook 'emacs-startup-hook 'akirak/log-init-time)
+
+(defun akirak/view-init-log ()
+  (interactive)
+  (find-file-read-only akirak/init-time-log-file))
 
 (provide 'init-startup)
