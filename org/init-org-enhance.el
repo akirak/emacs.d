600b202ddf92cf1d804c9041b2fbce4db2840ebd
Akira Komamura <akira.komamura@gmail.com>
2018-05-08 20:00:31 +0900

    Load org enhancements after org

--- org/init-org-enhance.el
+++ org/init-org-enhance.el
@@ -8,17 +8,20 @@
 
 ;; Prettify headline bullets
 (use-package org-bullets
+  :after org
   :init
   (add-hook 'org-mode-hook 'org-bullets-mode))
 
 ;; Edit Org-Mode lists like in word processors
 (use-package org-autolist
+  :after org
   :diminish 'org-autolist-mode
   :init
   (add-hook 'org-mode-hook #'org-autolist-mode))
 
 ;; Allow you to bookmark headings in Org-Mode
-(use-package org-bookmark-heading)
+(use-package org-bookmark-heading
+  :after org)
 
 (provide 'init-org-enhance)
 ;;; init-org-enhance.el ends here
