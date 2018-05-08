787450017b692292571b1e24fc14502b4735169e
Akira Komamura <akira.komamura@gmail.com>
2018-05-08 20:00:22 +0900

    Fix org-version

--- org/init-org-base.el
+++ org/init-org-base.el
@@ -5,37 +5,36 @@
 
 (use-package org
   :config
-  (require 'org-loaddefs)
+  (require 'org-loaddefs))
 
-  ;; Workarounds for org-mode installed by straight.el
-  ;; See https://github.com/raxod502/radian/blob/master/radian-emacs/radian-org.el
-
-  (defun org-git-version ()
-    "The Git version of org-mode.
+;; Workarounds for org-mode installed by straight.el
+;; See https://github.com/raxod502/radian/blob/master/radian-emacs/radian-org.el
+(defun org-git-version ()
+  "The Git version of org-mode.
   Inserted by installing org-mode or when a release is made."
-    (require 'git)
-    (let ((git-repo (expand-file-name
-                     "straight/repos/org/" user-emacs-directory)))
-      (string-trim
-       (git-run "describe"
-                "--match=release\*"
-                "--abbrev=6"
-                "HEAD"))))
+  (require 'git)
+  (let ((git-repo (expand-file-name
+                   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (git-run "describe"
+              "--match=release\*"
+              "--abbrev=6"
+              "HEAD"))))
 
-  (defun org-release ()
-    "The release version of org-mode.
+(defun org-release ()
+  "The release version of org-mode.
   Inserted by installing org-mode or when a release is made."
-    (require 'git)
-    (let ((git-repo (expand-file-name
-                     "straight/repos/org/" user-emacs-directory)))
-      (string-trim
-       (string-remove-prefix
-        "release_"
-        (git-run "describe"
-                 "--match=release\*"
-                 "--abbrev=0"
-                 "HEAD")))))
+  (require 'git)
+  (let ((git-repo (expand-file-name
+                   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (string-remove-prefix
+      "release_"
+      (git-run "describe"
+               "--match=release\*"
+               "--abbrev=0"
+               "HEAD")))))
 
-  (provide 'org-version))
+(provide 'org-version)
 
 (provide 'init-org-base)
