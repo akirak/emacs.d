415063520f848b3700a01a9093be51b9fcee8a38
Akira Komamura <akira.komamura@gmail.com>
2018-05-12 01:58:59 +0900

    Enable helm-linux-disks

--- apps/init-linux-disk.el
+++ apps/init-linux-disk.el
@@ -1,6 +1,7 @@
 (use-package helm-linux-disks
-  :disabled t
-  :straight (helm-linux-disks :host github :repo "akirak/helm-linux-disks")
+  :straight (helm-linux-disks :host github
+                              :repo "akirak/helm-linux-disks")
+  :commands (helm-linux-disks)
   :custom
   (linux-disk-terminal-type (quote multi-term)))
 
