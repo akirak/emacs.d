7b140871a7e744839f2731290a50b6a12cb99515
Akira Komamura <akira.komamura@gmail.com>
2018-05-12 20:11:48 +0900

    Remove duplicate import

--- org/init-org.el
+++ org/init-org.el
@@ -1,7 +1,6 @@
 (require 'init-org-base)
 (require 'init-org-starter)
 (require 'init-org-enhance)
-(require 'init-org-web-tools)
 (require 'init-org-download)
 (require 'init-org-web-tools)
 (require 'init-org-offtime)
