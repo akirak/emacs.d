a8e239f30f024275ce6f0cd2ee8e4676bbf1a3ec
Akira Komamura <akira.komamura@gmail.com>
2018-05-16 20:16:49 +0900

    autoload

--- hydra-launcher.el
+++ hydra-launcher.el
@@ -1,7 +1,10 @@
+(autoload 'akirak/emacs-lisp-hydra "emacs-lisp-hydra")
+
 (defcustom akirak/hydra-launcher-major-mode-command-alist
   '((emacs-lisp-mode . akirak/emacs-lisp-hydra))
   "Alist of major modes and commands for hydra-launcher.")
 
+;;;###autoload
 (defun akirak/hydra-launcher-for-major-mode ()
   (interactive)
   (cl-destructuring-bind command
