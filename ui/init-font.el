3c414b630ea99d3f1d0cd80c129a9bab8eba7402
Akira Komamura <akira.komamura@gmail.com>
2018-05-16 15:11:03 +0900

    Set the default font properly

--- ui/init-font.el
+++ ui/init-font.el
@@ -1,14 +1,10 @@
-(defun akirak/set-frame-font ()
-  (interactive)
-  (set-frame-font "Hack-10.5"))
+;; Setting the default font
+;;
+;; This solution seem to work for now, but may be changed in the future.
+;; https://superuser.com/questions/692173/emacs-default-font-does-not-work-with-new-frames
+(when (member "Hack" (font-family-list))
+  (set-default-font "Hack-10.5" nil t))
 
-(defun akirak/set-all-frame-font ()
-  (interactive)
-  (dolist (frame (frame-list))
-    (with-selected-frame frame
-      (akirak/set-frame-font))))
-
-;; TODO: There may be a better way to do this
-(akirak/set-frame-font)
-
+;; This doesn't allow setting a size
+;; (set-face-attribute 'default nil :family "Hack")
 (provide 'init-font)
