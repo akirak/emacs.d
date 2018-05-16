8aeb9e753e11836cf4f8678609d5b3c304b71d1c
Akira Komamura <akira.komamura@gmail.com>
2018-05-16 17:11:32 +0900

    Add features related to flycheck-checker
    - Add the hydra title
    - Display flycheck-checker if it is set
    - Add a binding to select flycheck-checker
    - Add a binding to quit the hydra

--- toggle-modes/toggle-modes-hydra.el
+++ toggle-modes/toggle-modes-hydra.el
@@ -3,12 +3,19 @@
 (defhydra akirak/toggle-modes-hydra
   (:hint nil)
   "
-_c_ flycheck  [%s(if flycheck-mode \"on\" \"off\")]
+Toggle modes
+
+_c_ flycheck  [%s(if flycheck-mode (or flycheck-checker \"on\") \"off\")]
 _d_ eldoc     [%s(if eldoc-mode \"on\" \"off\")]
 _s_ spell     [%s(if flyspell-mode \"on\" \"off\")]
+
 "
   ("c" flycheck-mode)
   ("d" eldoc-mode)
-  ("s" flyspell-mode))
+  ("s" flyspell-mode)
+
+  (",c" flycheck-select-checker "Select flycheck-checker")
+
+  ("q" nil "quit"))
 
 (provide 'toggle-modes-hydra)
