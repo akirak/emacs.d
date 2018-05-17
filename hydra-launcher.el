25c9e80f72524e21ae04c47356145cc5f6f3e5d9
Akira Komamura <akira.komamura@gmail.com>
2018-05-17 17:53:45 +0900

    Add a hydra for org-mode
    Add commands to org-hydra

--- hydra-launcher.el
+++ hydra-launcher.el
@@ -1,8 +1,11 @@
 (autoload 'akirak/emacs-lisp-hydra "emacs-lisp-hydra"
   "Hydra for Emacs Lisp." t)
+(autoload 'akirak/org-hydra "org-hydra"
+  "Hydra for Org." t)
 
 (defcustom akirak/hydra-launcher-major-mode-command-alist
-  '((emacs-lisp-mode . akirak/emacs-lisp-hydra))
+  '((emacs-lisp-mode . akirak/emacs-lisp-hydra)
+    (org-mode . akirak/org-hydra))
   "Alist of major modes and commands for hydra-launcher.")
 
 ;;;###autoload
@@ -12,6 +15,8 @@
       (alist-get major-mode akirak/hydra-launcher-major-mode-command-alist)
     (if command
         (funcall command)
-      (message "No hydra is available in this major mode"))))
+      (cond
+       ((derived-mode-p 'org-mode) (akirak/org-hydra))
+       ((message "No hydra is available in this major mode"))))))
 
 (provide 'hydra-launcher)
