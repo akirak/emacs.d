5942edce820f6b3884af1f0dc515ba7cfdee0c8e
Akira Komamura <akira.komamura@gmail.com>
2018-05-17 16:28:33 +0900

    Add macroexpand to emacs-lisp-hydra
    -----------
    
    emacs-lisp-macroexpand is an interactive compiled Lisp function in
    ‘elisp-mode.el’.
    
    (emacs-lisp-macroexpand)
    
    Macroexpand the form after point.
    Comments in the form will be lost.
    
    -----------
    
    macroexpand is a built-in function in ‘C source code’.
    
    (macroexpand FORM &optional ENVIRONMENT)
    
    Return result of expanding macros at top level of FORM.
    If FORM is not a macro call, it is returned unchanged.
    Otherwise, the macro is expanded and the expansion is considered
    in place of FORM.  When a non-macro-call results, it is returned.
    
    The second optional arg ENVIRONMENT specifies an environment of macro
    definitions to shadow the loaded ones for use in file byte-compilation.

--- emacs-lisp-hydra.el
+++ emacs-lisp-hydra.el
@@ -3,12 +3,14 @@
 (defhydra akirak/emacs-lisp-hydra
   (:hint nil)
   "
-^^Eval            ^^Check            ^^Toggle
-^^--------------  ^^---------------  ^^------------------------
-_e_: eval-buffer  _l_: package-lint  _td_: debug-on-error [%s(if debug-on-error \"on\" \"off\")]
+^^Eval             ^^Check            ^^Toggle
+^^--------------   ^^---------------  ^^------------------------
+_e_: eval-buffer   _l_: package-lint  _td_: debug-on-error [%s(if debug-on-error \"on\" \"off\")]
+_me_: macroexpand
 
 "
   ("e" eval-buffer :exit t)
+  ("me" emacs-lisp-macroexpand :exit t)
   ("l" package-lint-current-buffer :exit t)
   ("td" toggle-debug-on-error))
 
