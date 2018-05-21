a2513ff57e5763cddb9ceb673ec0f54e7eb9bd76
Akira Komamura <akira.komamura@gmail.com>
2018-05-21 18:15:52 +0900

    Support symlinked refile targets

--- org/akirak-org-refile-path.el
+++ org/akirak-org-refile-path.el
@@ -36,13 +36,18 @@
 ;; a new node. Advise `completing-read' temporarily instead.
 ;; (advice-add #'org-refile-get-targets :filter-return #'akirak/ad-filter-org-refile-targets)
 
+(require 'dash)
+
 (defun akirak/ad-around-org-refile-completing-read (oldfun prompt candidates &rest r)
   "Advice function for `completing-read' for use inside `org-refile-get-location'."
   (if (eq org-refile-use-outline-path 'full-file-path)
-      (expand-file-name (substring-no-properties
-                         (apply oldfun prompt
-                                (akirak/ad-filter-org-refile-targets candidates)
-                                r)))
+      (let* ((modified-candidates (akirak/ad-filter-org-refile-targets candidates))
+             ;; Store the mapping to support symlinked targets
+             (alist (-zip (mapcar #'car modified-candidates)
+                          (mapcar #'car candidates)))
+             (raw (apply oldfun prompt modified-candidates r)))
+        (or (cdr (assoc raw alist))
+            (expand-file-name (substring-no-properties raw))))
     (apply oldfun prompt candidates r)))
 
 (defun akirak/ad-around-org-refile-get-location (oldfun &rest r)
