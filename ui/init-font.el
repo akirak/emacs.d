01fc58a085e4d04f6e6543d0d49f5de58cb78a8a
Akira Komamura <akira.komamura@gmail.com>
2018-05-26 12:18:58 +0900

    Fix: Make the default font properly set on new frames

--- ui/init-font.el
+++ ui/init-font.el
@@ -2,8 +2,11 @@
 ;;
 ;; This solution seem to work for now, but may be changed in the future.
 ;; https://superuser.com/questions/692173/emacs-default-font-does-not-work-with-new-frames
-(when (member "Hack" (font-family-list))
-  (set-default-font "Hack-10.5" nil t))
+(defun akirak/set-default-font ()
+  (when (member "Hack" (font-family-list))
+    (set-default-font "Hack-10.5" nil t)))
+
+(add-hook 'emacs-startup-hook 'akirak/set-default-font)
 
 ;; This doesn't allow setting a size
 ;; (set-face-attribute 'default nil :family "Hack")
