3d186fd7fdfa8cd91cdd3d8e1acfd1117ea5bf66
Akira Komamura <akira.komamura@gmail.com>
2018-05-31 21:34:49 +0900

    Bind a key to org-journal-new-date-entry

--- keybindings/init-global-org-map.el
+++ keybindings/init-global-org-map.el
@@ -2,6 +2,7 @@
 
 (general-def akirak/global-org-map
   "j" #'org-journal-new-entry
+  "nj" #'org-journal-new-date-entry
   "r" #'helm-org-rifle-agenda-files
   "f" #'helm-org-starter
   "sj" #'org-journal-search)
