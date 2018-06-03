f41031e55f0361ca1d94556ce8b28a26d074fd40
Akira Komamura <akira.komamura@gmail.com>
2018-06-03 22:24:22 +0900

    Add an option to use helpful to view help using counsel

--- misc/init-helpful.el
+++ misc/init-helpful.el
@@ -3,6 +3,13 @@
              helpful-variable
              helpful-at-point
              helpful-command)
+  :config
+  (with-eval-after-load 'counsel
+    (cl-loop for (command . action) in '((counsel-describe-function . helpful-function)
+                                         (counsel-describe-variable . helpful-variable)
+                                         (counsel-M-x . helpful-command))
+             do (ivy-add-actions command
+                                 `(("f" ,(-compose action 'intern) "helpful")))))
   ;; :general
   ;; ([help ?f] 'helpful-function
   ;;  [help ?v] 'helpful-variable
