286861d563662834d1a96e6763033dc523cbc269
Akira Komamura <akira.komamura@gmail.com>
2018-06-03 18:12:53 +0900

    Add keybindings for common special buffers

--- keybindings/init-c-c.el
+++ keybindings/init-c-c.el
@@ -1,6 +1,12 @@
 (general-def
   "C-c l" 'org-store-link
   "C-c p" 'yankpad-map
+  "C-c v" '(nil :wk "display-buffer")
+  "C-c v b" '((lambda () (interactive) (display-buffer "*Backtrace*")) :wk "backtrace")
+  "C-c v c" '((lambda () (interactive) (display-buffer "*compilation*")) :wk "compilation")
+  "C-c v m" '((lambda () (interactive) (display-buffer "*Messages*")) :wk "messages")
+  "C-c v w" '((lambda () (interactive) (display-buffer "*Warnings*")) :wk "warnings")
+  "C-c v s" '((lambda () (interactive) (pop-to-buffer "*scratch*")) :wk "scratch")
   "C-c y" 'yankpad-insert)
 
 (provide 'init-c-c)
