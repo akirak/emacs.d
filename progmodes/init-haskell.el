4d6602a124066f5279a1bc113b33c67fe3fc9fbb
Akira Komamura <akira.komamura@gmail.com>
2018-06-05 12:35:27 +0900

    Haskell: Use dante

--- progmodes/init-haskell.el
+++ progmodes/init-haskell.el
@@ -1,5 +1,14 @@
 (use-package intero
+  :disabled t
   :init
   (add-hook 'haskell-mode-hook 'intero-mode))
+
+(use-package haskell-mode
+  :mode ("\\.hs\\'" . haskell-mode))
+
+(use-package dante
+  :commands (dante-mode)
+  :init
+  (add-hook 'haskell-mode-hook 'dante-mode))
 
 (provide 'init-haskell)
