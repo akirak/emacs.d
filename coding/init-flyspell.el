98b2290a24ab69f6d24c60ae1d278124928c2020
Akira Komamura <akira.komamura@gmail.com>
2018-06-05 21:38:11 +0900

    flyspell: Concise hook definitions using :hook keyword

--- coding/init-flyspell.el
+++ coding/init-flyspell.el
@@ -1,12 +1,8 @@
 (use-package flyspell
   :diminish 'flyspell-mode
-  :init
-  ;; Turn on flyspell in text-mode
-  (add-hook 'text-mode-hook (lambda () (flyspell-mode 1)))
-  ;; Turn off flyspell in these modes
-  (dolist (hook '(change-log-mode-hook log-edit-mode-hook))
-    (add-hook hook (lambda () (flyspell-mode -1))))
-  ;; Turn on flyspell in comments in prog-mode
-  (add-hook 'prog-mode-hook 'flyspell-prog-mode))
+  :hook
+  ((text-mode . (lambda () (flyspell-mode 1)))
+   ((change-log-mode log-edit-mode) . (lambda () (flyspell-mode -1)))
+   (prog-mode . flyspell-prog-mode)))
 
 (require 'init-flyspell)
