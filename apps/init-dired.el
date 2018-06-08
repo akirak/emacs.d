9a23d7a05269a1eb3ab4cb9604fdf36709e6632d
Akira Komamura <akira.komamura@gmail.com>
2018-06-08 17:35:36 +0900

    [dired] Add dired-hide-dotfiles and dired-filter

--- apps/init-dired.el
+++ apps/init-dired.el
@@ -1,6 +1,15 @@
 (setq-default dired-dwim-target t)
 
-(use-package dired-hacks
-  :disabled t)
+;;;; Packages for enhancing dired
+(use-package dired-hide-dotfiles
+  :commands (dired-hide-dotfiles-mode))
+
+(use-package dired-filter
+  :hook
+  (dired . dired-filter-mode))
+
+;;;; Additional keybindings in dired-mode
+(general-def dired-mode-map
+  "zh" #'dired-hide-dotfiles-mode)
 
 (provide 'init-dired)
