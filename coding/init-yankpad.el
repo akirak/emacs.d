d442f2ffdf0b021d2a5d4378f747c940b0ddc652
Akira Komamura <akira.komamura@gmail.com>
2018-06-09 20:30:17 +0900

    [yankpad] Add yankpad-hydra

--- coding/init-yankpad.el
+++ coding/init-yankpad.el
@@ -6,4 +6,30 @@
   (with-eval-after-load 'company
     (add-hook 'company-backends #'company-yankpad t)))
 
+(defun yankpad-hydra--format-active-snippets ()
+  (mapconcat (lambda (ent)
+               (concat (car ent) (if-let ((key (nth 1 ent)))
+                                     (format " %s" key)
+                                   "")))
+             (yankpad-active-snippets)
+             "\n"))
+
+(defhydra yankpad-hydra (:hint nil)
+  "
+Category: %s`yankpad-category
+
+%s(yankpad-hydra--format-active-snippets)
+
+^Expand^       ^Category^           ^Snippets^
+^^----------   ^^----------------   ^^-------------------
+_SPC_ insert   _<tab>_     set      _e_ edit    _C_ capture
+_m_   map      _<backtab>_ append   _R_ reload"
+  ("SPC" yankpad-insert :exit t)
+  ("m" yankpad-map :exit t)
+  ("<tab>" yankpad-set-category)
+  ("<backtab>" yankpad-append-category)
+  ("e" yankpad-edit :exit t)
+  ("R" yankpad-reload)
+  ("C" yankpad-capture-snippet :exit t))
+
 (provide 'init-yankpad)
--- keybindings/init-c-c.el
+++ keybindings/init-c-c.el
@@ -3,10 +3,9 @@
   "C-c i" 'scratch
   "C-c l" 'org-store-link
   "C-c m" 'bm-toggle
-  "C-c p" 'yankpad-map
+  "C-c p" 'yankpad-hydra/body
   "C-c s" 'symbol-overlay-put
   "C-c u" '(nil :wk "ui toggle")
-  "C-c u s" 'symbol-overlay-remove-all
-  "C-c y" 'yankpad-insert)
+  "C-c u s" 'symbol-overlay-remove-all)
 
 (provide 'init-c-c)
