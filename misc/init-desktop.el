a7ac236eccba13322a4814692d9ea1a79f01db05
Akira Komamura <akira.komamura@gmail.com>
2018-06-11 21:57:19 +0900

    [desktop] Add an option to kill Emacs without confirmation

--- misc/init-desktop.el
+++ misc/init-desktop.el
@@ -1,10 +1,18 @@
-(defun akirak/maybe-save-desktop-and-kill-emacs ()
-  (interactive)
-  (when (and (server-running-p)
-             (y-or-n-p "Save all the buffers as well as the desktop?"))
-    (save-some-buffers t)
-    (desktop-save-in-desktop-dir))
-  (save-buffers-kill-emacs))
+(defun akirak/maybe-save-desktop-and-kill-emacs (arg)
+  "Kill the desktop.
+
+With prefix ARG, save all the buffers silently and kill Emacs without
+confirmation."
+  (interactive "P")
+  (unless arg
+    (when (and (server-running-p)
+               (y-or-n-p "Save all the buffers as well as the desktop?"))
+      (save-some-buffers t)
+      (desktop-save-in-desktop-dir)))
+  (if arg
+      (let ((confirm-kill-emacs nil))
+        (save-buffers-kill-emacs t))
+    (save-buffers-kill-emacs)))
 
 (global-set-key (kbd "C-x C-c") #'akirak/maybe-save-desktop-and-kill-emacs)
 
