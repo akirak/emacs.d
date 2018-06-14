71d299de5c0262a568f2fe1c6227b06d166bcb45
Akira Komamura <akira.komamura@gmail.com>
2018-06-15 04:28:40 +0900

    [olivetti] Stop automatically turning on olivetti-mode

--- ui/init-olivetti.el
+++ ui/init-olivetti.el
@@ -1,8 +1,8 @@
 (use-package olivetti
   :commands (turn-on-olivetti-mode)
-  :hook
-  (org-mode . turn-on-olivetti-mode)
-  (markdown-mode . turn-on-olivetti-mode)
+  ;; :hook
+  ;; (org-mode . turn-on-olivetti-mode)
+  ;; (markdown-mode . turn-on-olivetti-mode)
   :custom
   (olivetti-body-width 92))
 
