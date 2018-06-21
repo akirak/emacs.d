2840620017f9375168bcced10bac617b2aabc81a
Akira Komamura <akira.komamura@gmail.com>
2018-06-22 00:57:25 +0900

    [org] Organize the module list

--- org/init-org.el
+++ org/init-org.el
@@ -1,18 +1,26 @@
+;;;; Basic configuration 
 (require 'init-org-base)
 (require 'init-org-starter)
-(require 'init-org-enhance)
-(require 'init-org-download)
-(require 'init-org-web-tools)
-(require 'init-org-offtime)
-(require 'init-org-super-agenda)
-(require 'init-helm-org-rifle)
-(require 'init-counsel-org-clock)
-(require 'init-org-bindings)
-(require 'init-org-make-toc)
-(require 'init-org-edna)
-(require 'org-refile-hydra)
 
+;;;; Enhancements to org-mode
+(require 'init-org-enhance)
+(require 'init-org-edna)
 (with-eval-after-load 'org
   (require 'akirak-org-refile-path))
+
+;;;; Convenient tools for editing org files
+(require 'init-org-download)
+(require 'init-org-web-tools)
+(require 'init-org-make-toc)
+
+;;;; Search and navigation
+(require 'init-helm-org-rifle)
+(require 'init-org-offtime)
+(require 'init-counsel-org-clock)
+(require 'init-org-super-agenda)
+
+;;;; Keybindings
+(require 'init-org-bindings)
+(require 'org-refile-hydra)
 
 (provide 'init-org)
