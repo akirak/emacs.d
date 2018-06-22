84a541bd7eef6f8a1d4dbe3eba090a756264b9b3
Akira Komamura <akira.komamura@gmail.com>
2018-06-23 03:25:35 +0900

    [org] Hook org-store-agenda-views on offtime and midnight

--- misc/init-midnight.el
+++ misc/init-midnight.el
@@ -4,4 +4,6 @@
 (with-eval-after-load 'midnight
   (add-to-list 'clean-buffer-list-kill-regexps "\\*helm"))
 
+(add-hook 'midnight-hook 'org-store-agenda-views)
+
 (provide 'init-midnight)
--- org/init-org-offtime.el
+++ org/init-org-offtime.el
@@ -1,5 +1,8 @@
 (use-package org-offtime
   :after org
-  :straight (org-offtime :host github :repo "akirak/org-offtime"))
+  :straight (org-offtime :host github :repo "akirak/org-offtime")
+  :config
+  (add-hook 'org-offtime-hook 'bookmark-save)
+  (add-hook 'org-offtime-hook 'org-store-agenda-views))
 
 (provide 'init-org-offtime)
