33c878a78dbb6c4f8b49b522649c57174e79541c
Akira Komamura <akira.komamura@gmail.com>
2018-06-26 13:33:25 +0900

    [midnight] Set delay; Add Helm to clean-buffer-list

--- misc/init-midnight.el
+++ misc/init-midnight.el
@@ -1,3 +1,7 @@
-(setq midnight-mode t)
+(setq midnight-mode t
+      clean-buffer-list-delay-general 2)
+
+(with-eval-after-load 'midnight
+  (add-to-list 'clean-buffer-list-kill-regexps "\\*helm"))
 
 (provide 'init-midnight)
