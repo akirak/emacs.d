5bded12353b89d4770a4c93925abd1a42b29d771
Akira Komamura <akira.komamura@gmail.com>
2018-07-03 09:47:30 +0900

    [autosave] Turn off if olivetti-mode is on

--- misc/init-autosave.el
+++ misc/init-autosave.el
@@ -1,6 +1,10 @@
 (use-package real-auto-save
+  :config
+  (defun akirak/turn-on-auto-save-unless-olivetti ()
+    (real-auto-save-mode (if (bound-and-true-p olivetti-mode) nil 1)))
   :hook
   (org-mode . real-auto-save-mode)
+  (olivetti . akirak/turn-on-auto-save-unless-olivetti)
   :custom
   (real-auto-save-interval 30))
 
