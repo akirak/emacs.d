7046e96bbb2dddcc1a29517b70b7a691a97609b9
Akira Komamura <akira.komamura@gmail.com>
2018-07-05 03:34:07 +0900

    [midnight] Clean up every day; Add magit to kill-regexps

--- misc/init-midnight.el
+++ misc/init-midnight.el
@@ -1,8 +1,9 @@
 (setq midnight-mode t
-      clean-buffer-list-delay-general 2)
+      clean-buffer-list-delay-general 1)
 
 (with-eval-after-load 'midnight
-  (add-to-list 'clean-buffer-list-kill-regexps "\\*helm"))
+  (add-to-list 'clean-buffer-list-kill-regexps "\\*helm")
+  (add-to-list 'clean-buffer-list-kill-regexps "\\*magit"))
 
 (add-hook 'midnight-hook 'org-store-agenda-views)
 
