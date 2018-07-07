5b0564d9c9c3e2f87b2cba6e7f73313759764dea
Akira Komamura <akira.komamura@gmail.com>
2018-07-07 15:39:51 +0900

    [corefighter] Add a module for Git dirty statuses

--- ui/init-corefighter.el
+++ ui/init-corefighter.el
@@ -1,10 +1,23 @@
 ;;; init-corefighter.el --- corefighter -*- lexical-binding: t -*-
 
+(straight-use-package '(corefighter-extras :host github
+                                           :repo "akirak/corefighter-extras"))
+
 (use-package corefighter
-  :straight (corefighter :host github :repo "akirak/corefighter.el")
+  :straight (corefighter :host github
+                         :repo "akirak/corefighter.el")
   :commands (corefighter-sidebar)
   :config
+  (require 'corefighter-repom)
+  (setq corefighter-modules
+        '((corefighter-repom-dirty)
+          (corefighter-org-agenda)))
   (corefighter-load-modules))
+
+(require 'init-repom)
+
+(use-package corefighter-repom
+  :straight corefighter-extras)
 
 (provide 'init-corefighter)
 ;;; init-corefighter.el ends here
