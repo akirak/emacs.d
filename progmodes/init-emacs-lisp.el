495c5fd742805387e7ef72aad5d575df4c2964d7
Akira Komamura <akira.komamura@gmail.com>
2018-07-16 20:06:34 +0900

    [emacs-lisp] Add extra commands for development
    - Add a command to update the straight recipe for the project
    - Add a command to run load-file or eval-buffer
    - Organize headings

--- progmodes/init-emacs-lisp.el
+++ progmodes/init-emacs-lisp.el
@@ -14,18 +14,40 @@
 (use-package suggest
   :commands (suggest))
 
+;;;; Commands
+(defun akirak/straight-pull-package-projectile (name)
+  "Pull the package recipe for the current projectile project."
+  (interactive
+   (list (completing-read "Package: "
+                          (--map (f-filename it)
+                                 (f-directories
+                                  (expand-file-name "straight/repos"
+                                                    user-emacs-directory)))
+                          nil t
+                          (projectile-project-name))))
+  (straight-pull-package (intern name)))
+
+(defun akirak/eval-buffer-or-load-file ()
+  (interactive)
+  (when (eq major-mode 'emacs-lisp-mode)
+    (if buffer-file-name
+        (load-file buffer-file-name)
+      (eval-buffer))))
+
 ;;;; Testing
 (require 'init-emake)
 
-;;;; Help
+;;;; Keybindings
+
+;;;;; Help
 (akirak/bind-help-key :keymaps 'emacs-lisp-mode-map
   "i" #'counsel-info-lookup-symbol
   "s" #'suggest
   "." #'helpful-at-point)
 
-;;;; Extra commands
+;;;;; Extra commands
 (general-def :keymaps 'emacs-lisp-mode-map :prefix "C-z"
-  "e" #'eval-buffer
+  "e" #'akirak/eval-buffer-or-load-file
   "l" #'package-lint-current-buffer
   "m" #'emacs-lisp-macroexpand)
 
