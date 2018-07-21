9da60a3b400403e4efbe1b6559541233df1e0650
Akira Komamura <akira.komamura@gmail.com>
2018-07-21 20:25:25 +0900

    [frame-title] Fix: Prevent error on null frame-workflow-mode

--- ui/init-frame-title.el
+++ ui/init-frame-title.el
@@ -1,13 +1,12 @@
 ;;; init-frame-title.el --- Set the frame title format -*- lexical-binding: t -*-
 
 (setq frame-title-format
-      '((:eval
-         (or (and frame-workflow-mode
-                  (frame-workflow--frame-subject-name))
-             (and buffer-file-name
-                  (abbreviate-file-name buffer-file-name))
-             dired-directory
-             buffer-name))))
+      '(or (and (bound-and-true-p frame-workflow-mode)
+                (frame-workflow--frame-subject-name))
+           (and buffer-file-name
+                (abbreviate-file-name buffer-file-name))
+           dired-directory
+           buffer-name))
 
 (provide 'init-frame-title)
 ;;; init-frame-title.el ends here
