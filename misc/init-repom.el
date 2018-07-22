6f5c2ee2ea69055d85f5d00d928fd6980901f4aa
Akira Komamura <akira.komamura@gmail.com>
2018-07-22 21:11:25 +0900

    [repom] Delay repository synchronization at startup

--- misc/init-repom.el
+++ misc/init-repom.el
@@ -6,16 +6,18 @@
   :straight (repom :host github :repo "akirak/repom.el")
   :commands (helm-repom))
 
-(defun akirak/repom-async-get-lists ()
-  (when (version< "26" emacs-version)
+(when (version< "26" emacs-version)
+  (defun akirak/repom-async-get-lists ()
     (make-thread
      (lambda ()
        (message "Retrieving lists of GitHub repositories...")
        (require 'repom-github)
        (repom-github-fetch-repo-lists)
-       (message "Finished retrieving repository lists.")))))
+       (message "Finished retrieving repository lists."))))
 
-(add-hook 'emacs-startup-hook #'akirak/repom-async-get-lists)
+  (add-hook 'emacs-startup-hook
+            (lambda ()
+              (run-with-timer 2 nil #'akirak/repom-async-get-lists))))
 
 (provide 'init-repom)
 ;;; init-repom.el ends here
