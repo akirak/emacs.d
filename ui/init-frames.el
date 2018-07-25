2631bba5b619b05880f282e7a9c479654728ec54
Akira Komamura <akira.komamura@gmail.com>
2018-07-26 00:05:37 +0900

    [frame-workflow] Fix up again
    The previous implementation somehow (why?) didn't show the two
    (emacs.org and magit) buffers as expected, but this seems to work
    well.
    
    Note that, if you first close the sidebar window and then quit magit,
    the previous window layout is restored. This is awkward, but I cannot
    find a solution for now.

--- ui/init-frames.el
+++ ui/init-frames.el
@@ -25,9 +25,9 @@
   :layout
   '(progn
      (delete-other-windows)
+     (find-file (expand-file-name "emacs.org" user-emacs-directory))
      (when (fboundp 'ibuffer-sidebar-show-sidebar)
        (ibuffer-sidebar-show-sidebar))
-     (find-file (expand-file-name "emacs.org" user-emacs-directory))
      (split-window-sensibly)
      (frame-workflow-magit-same-window))
   :make-frame
