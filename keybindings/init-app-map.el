bf9a33c4bde1a1489e57c6016f8f6b55aabb1f23
Akira Komamura <akira.komamura@gmail.com>
2018-07-29 10:32:22 +0900

    Fix: typo

--- keybindings/init-app-map.el
+++ keybindings/init-app-map.el
@@ -5,7 +5,7 @@
   "c" 'calendar
   "d" 'helm-linux-disks
   "i" 'docker-images
-  "j" 'org-journal
+  "j" 'org-journal-new-entry
   "k" 'docker-containers
   "p" 'prodigy
   "P" 'helm-system-packages
