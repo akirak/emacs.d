7ab96ef6dd185def39e5a58d6e7abc0d51e435c0
Akira Komamura <akira.komamura@gmail.com>
2018-08-04 21:34:38 +0900

    [org-bindings] Add bindings (incl. hydra) to create/edit a table
    - [org-insert-hydra] Add a binding to create a table
    - Add org-table-hydra, which is experimental

--- org/org-insert-hydra.el
+++ org/org-insert-hydra.el
@@ -2,11 +2,35 @@
   "
 Insert into org-mode
 
-^^From URL
-^^--------
-_l_ link
+^^From URL  ^^Misc
+^^--------  ^^--------
+_l_ link    _t_ table
 "
-  ("l" org-web-tools-insert-link-for-url :exit t))
+  ("l" org-web-tools-insert-link-for-url :exit t)
+  ("t" (if (org-at-table-p)
+           (org-table-hydra/body)
+         (org-table-create)) :exit t))
+
+(defhydra org-table-hydra (:hint nil)
+  "
+Org Table
+
+        ^^Insert  ^^Delete
+Row     _ir_      _dr_
+Column  _ic_      _dc_
+
+Edit: _e_
+Navigation: _n_ _p_ _f_ _b_
+"
+  ("dc" org-table-delete-column)
+  ("dr" org-table-kill-row)
+  ("ic" org-table-insert-column)
+  ("ir" org-table-insert-row)
+  ("e" org-edit-special)
+  ("f" org-table-next-field)
+  ("b" org-table-previous-field)
+  ("n" org-table-next-row)
+  ("p" previous-line))
 
 (provide 'org-insert-hydra)
 ;;; org-insert-hydra.el ends here
