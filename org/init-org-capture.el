f19e51cb641c44cfc10aaff2dc7a40ff688d2bf7
Akira Komamura <akira.komamura@gmail.com>
2018-08-05 22:56:45 +0900

    [org-capture] Add reverse-date tree function

--- org/init-org-capture.el
+++ org/init-org-capture.el
@@ -62,5 +62,37 @@
           (if body (concat body "\n") "")
           "\n\n"))
 
+;;;; Jump to a heading
+
+(defun akirak/org-find-heading-or-prepend (level text)
+  "Find or create a heading with the given text at the given level."
+  (let ((prefix (concat (make-string (org-get-valid-level level) ?*) " "))
+        (bound (unless (= level 1)
+                 (save-excursion (org-end-of-subtree)))))
+    (unless (re-search-forward (concat "^" (regexp-quote prefix) text)
+                               bound t)
+      (if (re-search-forward (concat "^" prefix) bound t)
+          (end-of-line 0)
+        (end-of-line 1))
+      (insert "\n" prefix text))))
+
+(defun akirak/org-reverse-date-tree (&optional time)
+  "Jump to the specified date in a reverse date tree.
+
+A reverse date tree is a reversed version of the date tree in
+`org-capture', i.e. a date tree where the newest date is the first.
+This is especially useful for a notes archive, because the latest
+entry on a particular topic is displayed at the top in
+a command like `helm-org-rifle'.
+
+TIME is the date to be inserted. If omitted, this will be today."
+  (let* ((time (or time (current-time))))
+    (save-restriction
+      (widen)
+      (goto-char (point-min))
+      (akirak/org-find-heading-or-prepend 1 (format-time-string "%Y" time))
+      (akirak/org-find-heading-or-prepend 2 (format-time-string "%Y-%m %B" time))
+      (akirak/org-find-heading-or-prepend 3 (format-time-string "%Y-%m-%d %A" time)))))
+
 (provide 'init-org-capture)
 ;;; init-org-capture.el ends here
