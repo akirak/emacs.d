ebf1410feacbb0fc47d4553e2b92e5e18f505b06
Akira Komamura <akira.komamura@gmail.com>
2018-08-13 23:16:12 +0900

    [helpful] Bind <menu> . to helpful-at-point in help modes

--- misc/init-helpful.el
+++ misc/init-helpful.el
@@ -8,6 +8,8 @@
   ;; From https://www.reddit.com/r/emacs/comments/8t3imt/helpful_one_year_on/e1eoy6v/
   (setq counsel-describe-variable-function 'helpful-variable
         counsel-describe-function-function 'helpful-callable)
+  (akirak/bind-help-key :keymaps '(helpful-mode-map help-mode-map)
+    "." #'helpful-at-point)
   :config
   (with-eval-after-load 'counsel
     (cl-loop for (command . action) in '((counsel-describe-function . helpful-function)
