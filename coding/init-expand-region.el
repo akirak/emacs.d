c8be1ffbd5d72bd9f9d9ec0aa3da47f524683093
Akira Komamura <akira.komamura@gmail.com>
2018-08-19 12:12:12 +0900

    [expand-region] Define commands to kill word/sentence/defun

--- coding/init-expand-region.el
+++ coding/init-expand-region.el
@@ -1,3 +1,21 @@
 (use-package expand-region)
 
+(defun akirak/kill-word ()
+  (interactive)
+  (er/mark-word)
+  (call-interactively #'kill-region)
+  (just-one-space))
+
+(defun akirak/kill-sentence ()
+  (interactive)
+  (er/mark-sentence)
+  (call-interactively #'kill-region)
+  (just-one-space))
+
+(defun akirak/kill-defun ()
+  (interactive)
+  (er/mark-defun)
+  (call-interactively #'kill-region)
+  (just-one-space))
+
 (provide 'init-expand-region)
--- keybindings/init-fix-map.el
+++ keybindings/init-fix-map.el
@@ -1,10 +1,13 @@
 (akirak/bind-fix-map
- :keymaps '(prog-mode-map text-mode-map)
- "e" 'akirak/hydra-flycheck
- "u" 'fix-word-upcase
- "l" 'fix-word-downcase
- "c" 'fix-word-capitalize
- ;; M-SPC was originally bound to just-one-space
- "SPC" 'just-one-space)
+  :keymaps '(prog-mode-map text-mode-map)
+  "e" 'akirak/hydra-flycheck
+  "u" 'fix-word-upcase
+  "l" 'fix-word-downcase
+  "c" 'fix-word-capitalize
+  "s" #'akirak/kill-sentence
+  "w" #'akirak/kill-word
+  "f" #'akirak/kill-defun
+  ;; M-SPC was originally bound to just-one-space
+  "SPC" 'just-one-space)
 
 (provide 'init-fix-map)
