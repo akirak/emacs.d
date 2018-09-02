d133765c34e1a09116470a8c19b5f14ce38856b5
Akira Komamura <akira.komamura@gmail.com>
2018-09-02 17:31:31 +0900

    [anzu] Remap query-replace-regexp

--- ui/init-anzu.el
+++ ui/init-anzu.el
@@ -2,6 +2,7 @@
   :config
   (global-anzu-mode 1)
   :bind
-  ([remap query-replace] . anzu-query-replace))
+  ([remap query-replace] . anzu-query-replace)
+  ([remap query-replace-regexp] . anzu-query-replace-regexp))
 
 (provide 'init-anzu)
