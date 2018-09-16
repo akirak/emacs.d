97800609b19a88f9d010dcc4121280ea47fb3bf1
Akira Komamura <akira.komamura@gmail.com>
2018-09-16 16:01:45 +0900

    [beancount] Remove company-ledger
    I find this completion annoying.

--- progmodes/init-beancount.el
+++ progmodes/init-beancount.el
@@ -3,11 +3,6 @@
 (defcustom akirak/ledger-primary-currency "JPY"
   "The currency primarily used in the country I am living in.")
 
-(use-package company-ledger
-  :after company
-  :straight (company-ledger :host github
-                            :repo "debanjum/company-ledger"))
-
 (use-package beancount
   :straight (beancount :host github
                        :repo "beancount/beancount"
