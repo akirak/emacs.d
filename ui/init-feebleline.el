213855e9dcd8eb6a46f64ed02f13270c7a1da2e3
Akira Komamura <akira.komamura@gmail.com>
2018-10-14 10:04:16 +0900

    [feebleline] Set a custom time format
    - Switch the repository to my fork which supports custom format
    - Set the time format

--- ui/init-feebleline.el
+++ ui/init-feebleline.el
@@ -1,10 +1,12 @@
 (use-package feebleline
+  :straight (feebleline :host github :repo "akirak/feebleline")
   :init
   (feebleline-mode 1)
   :custom
   (feebleline-show-git-branch t)
   (feebleline-show-dir t)
   (feebleline-show-time t)
+  (feebleline-time-format "[%b %d %a %H:%M:%S]")
   (feebleline-show-previous-buffer t))
 
 (provide 'init-feebleline)
