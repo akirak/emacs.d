eb9e75140346f4f7e481b8c5cdc5d76f87521ee6
Akira Komamura <akira.komamura@gmail.com>
2018-10-18 00:26:21 +0900

    [dracula-theme] Customize the face for line-number columns

--- ui/init-dracula-theme.el
+++ ui/init-dracula-theme.el
@@ -4,28 +4,24 @@
   :config
   (let ((key2 "#0189cc")
         (bg1 "#282a36")
+        (bg2 "#373844")
         (bg4 "#565761")
-        (type  "#bd93f9"))
+        (fg2 "#e2e2dc")
+        (fg4 "#b6b6b2")
+        (const "#8be9fd")
+        (type "#bd93f9"))
     (custom-theme-set-faces
      'dracula
      `(header-line ((default
                       :background ,bg1
                       :foreground ,type
                       :box t)))
-     ;; `(outshine-level-1 SPEC
-     ;;                    )
-     ))
-  ;; Add a custom face for the header line so that it looks distinct
-  )
-
-;; (with-eval-after-load 'whitespace
-;;   (set-face-attribute 'whitespace-tab nil
-;;                       :background "#f0f0f0"
-;;                       :foreground "#00a8a8"
-;;                       :weight 'bold)
-;;   (set-face-attribute 'whitespace-trailing nil
-;;                       :background "#e4eeff"
-;;                       :foreground "#183bc8"
-;;                       :weight 'normal))
+     `(line-number ((default
+                      :background ,bg2
+                      :foreground ,fg4)))
+     `(line-number-current-line ((default
+                                   :inherit 'line-number
+                                   :bold t
+                                   :foreground ,fg2))))))
 
 (provide 'init-dracula-theme)
