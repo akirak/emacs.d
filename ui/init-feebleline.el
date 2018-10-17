924bfadb949f546fcb825fb709a5be9e8e695271
Akira Komamura <akira.komamura@gmail.com>
2018-10-17 21:53:24 +0900

    [feebleline] Reconfigure for development version
    - Switch to the development version as suggested by the author
    - Configure feebleline-msg-functions with a bunch of sections

--- ui/init-feebleline.el
+++ ui/init-feebleline.el
@@ -1,15 +1,38 @@
-(require 'init-magit)
-
 (use-package feebleline
-  :straight (feebleline :host github :repo "akirak/feebleline")
+  :straight (feebleline :host github :repo "tautologyclub/feebleline"
+                        :branch "development")
   :init
-  (require 'magit nil t)
   (feebleline-mode 1)
-  :custom
-  (feebleline-show-git-branch t)
-  (feebleline-show-dir t)
-  (feebleline-show-time t)
-  (feebleline-time-format "[%b %d %a %H:%M:%S]")
-  (feebleline-show-previous-buffer t))
+  :config
+  (setq feebleline-msg-functions
+        '(((lambda () (format-time-string "%b %d %a %H:%M")) :post " " :face font-lock-comment-face)
+          (feebleline-file-directory :face feebleline-dir-face :post "")
+          (feebleline-file-or-buffer-name :face font-lock-keyword-face :post " ")
+          ((lambda () (when (and buffer-file-name (require 'magit nil t))
+                        (magit-get-current-branch))) :face font-lock-string-face :post " ")
+          ((lambda () mode-name) :post " " :face font-lock-comment-face)
+          ((lambda () (format-mode-line "%I")) :post " " :face font-lock-comment-face)
+          (akirak/org-clock-summary :face font-lock-builtin-face :pre " :: "))))
+
+(defun akirak/org-clock-summary ()
+  (when (org-clocking-p)
+    (concat (let* ((duration (time-subtract (current-time)
+                                            org-clock-start-time))
+                   (seconds (float-time duration)))
+              (cond
+               ((< seconds 3600) (format-time-string "%-M:%S" duration))
+               ((< seconds 86400) (format-time-string "%-Hh %-Mm" duration))
+               (t (org-minutes-to-clocksum-string
+                   (floor seconds 60)))))
+            " on "
+            (or org-clock-current-task
+                (with-current-buffer (marker-buffer org-clock-marker)
+                  (org-with-wide-buffer
+                   (goto-char org-clock-marker)
+                   (nth 4 (org-heading-components))))
+                "-")
+            " (in "
+            (buffer-name (marker-buffer org-clock-marker))
+            ")")))
 
 (provide 'init-feebleline)
