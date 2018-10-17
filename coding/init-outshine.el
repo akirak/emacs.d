3d0b28dae97ceabe7a5daaf0781d3764ec679336
Akira Komamura <akira.komamura@gmail.com>
2018-10-18 00:26:48 +0900

    [outshine] Set the headline faces from org headline faces

--- coding/init-outshine.el
+++ coding/init-outshine.el
@@ -2,6 +2,12 @@
 (add-hook 'prog-mode-hook 'outline-minor-mode)
 
 (use-package outshine
+  :config
+  (dolist (i (mapcar #'int-to-string (number-sequence 1 8)))
+    (let ((target (intern (concat "outshine-level-" i)))
+          (parent (intern (concat "org-level-" i))))
+      (custom-theme-set-faces
+       'dracula `(,target ((default :inherit ,parent))))))
   :general
   (:keymaps 'outline-minor-mode-map
             "C-M-i" nil)
