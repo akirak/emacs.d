3f54f1011e94baffc702a7cb771f6b5ca99b3332
Akira Komamura <akira.komamura@gmail.com>
2018-11-28 08:41:46 +0900

    [keybindings] Remap C-x to counsel-rg
    Prever ripgrep over ag (the silver searcher).

--- keybindings/init-c-x.el
+++ keybindings/init-c-x.el
@@ -5,7 +5,7 @@
   "C-x p" #'counsel-projectile
   "C-x t" #'helm-tail
   "C-x k" #'kill-this-buffer
-  "C-x /" #'counsel-ag
+  "C-x /" #'counsel-rg
   "C-x =" #'narrow-or-widen-dwim
   "C-x C-b" #'ibuffer
   "C-x C-j" #'dired-jump)
