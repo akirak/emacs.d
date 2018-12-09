b26e5ff327c1144d0f46a0a99f05791f185c4365
Akira Komamura <akira.komamura@gmail.com>
2018-12-09 03:55:47 +0000

    [terminal] Allow easily switching the default shell
    through a newly added custom variable `akirak/default-shell'.
    
    I plan to switch back to multi-term after reconfiguring zsh,
    but it has not been configured yet, so I will continue to use
    aweshell for now.

--- apps/init-terminal.el
+++ apps/init-terminal.el
@@ -2,24 +2,32 @@
 
 ;;;; Configure the default terminal/shell application
 
+(setq akirak/shell-settings
+      '((aweshell :open-dedicated aweshell-dedicated-open
+                  :new aweshell-new
+                  :next aweshell-next
+                  :prev aweshell-prev)
+        (multi-term :open-dedicated multi-term-dedicated-open
+                    :new multi-term
+                    :next multi-term-next
+                    :prev multi-term-prev)))
+
+(defcustom akirak/default-shell 'aweshell
+  "The defaut shell for use."
+  :set
+  (lambda (key value)
+    (set-default key value)
+    (let ((settings (alist-get value akirak/shell-settings)))
+      ;; Open a dedicated window for the default shell/terminal application.
+      (cl-loop for (key value) on settings by #'cddr
+               do (let ((name (intern (concat "akirak/shell-"
+                                              (string-remove-prefix ":" (symbol-name key))))))
+                    (fset name value))))))
+
 ;; Use aweshell as the default terminal
 (require 'init-aweshell)
 
 ;; Map commands
-
-(defalias 'akirak/shell-open-dedicated #'aweshell-dedicated-open
-  "Open a dedicated window for the default shell/terminal application.")
-
-(defalias 'akirak/shell-new #'aweshell-new
-  "Switch to a new shell buffer in the current window.")
-
-(defalias 'akirak/shell-next #'aweshell-next
-  "Switch to the previous shell buffer.")
-
-(defalias 'akirak/shell-previous #'aweshell-prev
-  "Switch to the next shell buffer.")
-
-;; Alternatively, you can use multi-term, but I prefer aweshell
 
 ;; (require 'init-multi-term)
 
