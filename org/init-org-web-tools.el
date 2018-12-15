72cbb337d4280bf2efadf1598532f775bb67c878
Akira Komamura <akira.komamura@gmail.com>
2018-12-15 03:36:40 +0000

    [org-web-tools] Change the recipe temporarily to the bugfixed branch

--- org/init-org-web-tools.el
+++ org/init-org-web-tools.el
@@ -1,3 +1,6 @@
-(use-package org-web-tools)
+(use-package org-web-tools
+  ;; TODO: Revert this spec to master of alphapapa after merging
+  :straight (org-web-tools :host github :repo "akirak/org-web-tools"
+                           :branch "empty-title"))
 
 (provide 'init-org-web-tools)
