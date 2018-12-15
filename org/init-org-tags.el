b0d869a6c24de195cb943e9281f76c2f0d140d62
Akira Komamura <akira.komamura@gmail.com>
2018-12-15 01:13:11 +0000

    [org] Disable fast tag selection in org-mode

--- org/init-org-tags.el
+++ org/init-org-tags.el
@@ -1,6 +1,6 @@
-(setq-default org-use-fast-tag-selection t
-              org-fast-tag-selection-single-key nil
-              org-group-tags t
+(setq-default org-group-tags t
+              ;; org-use-fast-tag-selection t
+              ;; org-fast-tag-selection-single-key nil
               org-agenda-use-tag-inheritance t
               org-tags-exclude-from-inheritance
               '(
