f7346907c99353fcdf11a5099cbfe1d88ac57129
Akira Komamura <akira.komamura@gmail.com>
2018-12-22 23:21:23 +0900

    [nix] Add nixos-options (experimental)

--- progmodes/init-nix.el
+++ progmodes/init-nix.el
@@ -1,4 +1,20 @@
 (use-package nix-mode
   :mode "\\.nix\\'")
 
+(use-package nixos-options :after nix-mode
+  :config
+  (setq akirak/nixos-options-available
+        (and (bound-and-true-p nixos-options-json-file)
+             (file-exists-p nixos-options-json-file))))
+
+(use-package company-nixos-options :after (company nixos-options)
+  :when akirak/nixos-options-available
+  :straight (nix-emacs :host github :repo "travisbhartwell/nix-emacs")
+  :init
+  (add-to-list 'company-backends 'company-nixos-options t))
+
+(use-package helm-nixos-options :after (helm nixos-options)
+  :when akirak/nixos-options-available
+  :straight nix-emacs)
+
 (provide 'init-nix)
