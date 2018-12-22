9cf0a0c281a55b727d909095d8e2ae4908ae72a9
Akira Komamura <akira.komamura@gmail.com>
2018-12-23 01:06:15 +0900

    [nix] Fix the startup error
    Prevent the following error raised at startup:
    
    Symbol's value as variable is void: akirak/nixos-options-available

--- progmodes/init-nix.el
+++ progmodes/init-nix.el
@@ -2,19 +2,19 @@
   :mode "\\.nix\\'")
 
 (use-package nixos-options :after nix-mode
-  :config
+  :init
   (setq akirak/nixos-options-available
         (and (bound-and-true-p nixos-options-json-file)
              (file-exists-p nixos-options-json-file))))
 
 (use-package company-nixos-options :after (company nixos-options)
-  :when akirak/nixos-options-available
+  :if (bound-and-true-p akirak/nixos-options-available)
   :straight (nix-emacs :host github :repo "travisbhartwell/nix-emacs")
   :init
   (add-to-list 'company-backends 'company-nixos-options t))
 
 (use-package helm-nixos-options :after (helm nixos-options)
-  :when akirak/nixos-options-available
+  :if (bound-and-true-p akirak/nixos-options-available)
   :straight nix-emacs)
 
 (provide 'init-nix)
