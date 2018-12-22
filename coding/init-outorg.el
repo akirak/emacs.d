cee636bef04b02f63467d501e7cb3e8d96b0eb90
Akira Komamura <akira.komamura@gmail.com>
2018-12-22 17:08:35 +0900

    [outorg] Bind C-c ' to outorg-copy-edits-and-exits
    Use C-c ' consistently for switching between Org and source code.
    
    - In prog-mode, open the current source code selection in outorg.
    - In outorg-edit-minor-mode, exit the outorg buffer.
    
    - On a source block in org-mode, edit the source code block
      (special mode).
    - When you are editing source code in org-mode using the special
      mode, save the edits and go back to org-mode.

--- coding/init-outorg.el
+++ coding/init-outorg.el
@@ -1,3 +1,9 @@
-(use-package outorg)
+;;; outorg
+(use-package outorg
+  :general
+  (:keymaps 'outorg-edit-minor-mode-map :package 'outorg
+            "C-c '" #'outorg-copy-edits-and-exit)
+  :custom
+  (outorg-edit-buffer-message-body "C-' to exit"))
 
 (provide 'init-outorg)
