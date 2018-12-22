21389f32e64e745b7c706f31d35e39957e4e550c
Akira Komamura <akira.komamura@gmail.com>
2018-12-22 16:54:36 +0900

    [keybindings] Remove unnecessary keybindings
    Remove keybindings I am currently not using and unlikely to use
    in the future.

--- keybindings/init-c-c.el
+++ keybindings/init-c-c.el
@@ -10,19 +10,15 @@
   "C-c l" 'org-store-link
   "C-c m" 'bm-toggle
   "C-c n" #'counsel-org-capture-string
-  "C-c o" #'outorg-edit-as-org
   "C-c p" 'yankpad-insert
-  "C-c r" #'rename-buffer
   "C-c s" 'symbol-overlay-put
-  "C-c t" #'akirak/shell-toggle-dedicated
   "C-c u" #'winner-undo-repeat
   "C-c U" #'winner-redo-repeat
   "C-c y" 'ivy-yasnippet
-  "C-c [" (defrepeater #'akirak/previous-frame)
-  "C-c ]" (defrepeater #'other-frame))
+  "C-c '" #'outorg-edit-as-org)
 
 (general-def :keymaps 'org-mode-map :package 'org
-  "C-c o" #'outorg-copy-edits-and-exit)
+  "C-c o" nil)
 
 ;; (general-def origami-mode-map
 ;;   ;; "C-c o" #'origami-recursively-toggle-node
