f6a569e20867b40c843feb9d31e6a1318b97091d
Akira Komamura <akira.komamura@gmail.com>
2018-12-31 06:12:44 +0900

    Disable display-line-numbers-mode

--- ui/init-ui.el
+++ ui/init-ui.el
@@ -58,13 +58,6 @@
      ))
   (whitespace-style '(face tabs trailing tab-mark)))
 
-(when (version<= "26.1" emacs-version)
-  (defun turn-on-display-line-numbers-mode ()
-    (interactive)
-    (display-line-numbers-mode 1))
-  (add-hook 'prog-mode-hook #'turn-on-display-line-numbers-mode)
-  (add-hook 'text-mode-hook #'turn-on-display-line-numbers-mode))
-
 (use-package git-gutter
   :diminish git-gutter-mode
   :init
