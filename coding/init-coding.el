e106f352e8ac55acea8d730b85dce4d796c85c2a
Akira Komamura <akira.komamura@gmail.com>
2019-01-05 15:11:38 +0900

    Display eldoc messages in a lv window

--- coding/init-coding.el
+++ coding/init-coding.el
@@ -1,7 +1,3 @@
-;; Turn off global-eldoc-mode, as I use some alternative help systems
-;; e.g. lsp-mode, depending on the language.
-(global-eldoc-mode -1)
-
 ;;;; Expansion, completion, and templates
 (require 'init-lsp)
 
---
+++ setup/setup-eldoc.el
@@ -0,0 +1,16 @@
+;; Turn off global-eldoc-mode, as I use some alternative help systems
+;; e.g. lsp-mode, depending on the language.
+(global-eldoc-mode -1)
+
+(autoload 'lv-message "lv")
+(autoload 'lv-delete-window "lv")
+
+;; Use lv.el (which is in the same repo as hydra) to display
+;; eldoc messages.
+(setq eldoc-message-function (cl-function
+                              (lambda (format &rest args)
+                                (if format
+                                    (apply #'lv-message format args)
+                                  (lv-delete-window)))))
+
+(provide 'setup-eldoc)
