55bc532b3bae8657131fbd4884807a2f5aa8019d
Akira Komamura <akira.komamura@gmail.com>
2019-01-06 10:43:31 +0900

    Suppress messages on toggle-word-wrap

--- setup/setup-typography.el
+++ setup/setup-typography.el
@@ -27,6 +27,21 @@
 (add-hook 'org-mode-hook 'akirak/set-typeface-for-writing)
 (add-hook 'markdown-mode-hook 'akirak/set-typeface-for-writing)
 
+;; Suppress a message by `toggle-word-wrap' unless the function is
+;; called interactively. The message is annoying as the option is set
+;; on Org files by default.
+(defun akirak/ad-around-toggle-word-wrap (orig &optional arg)
+  (unwind-protect
+      (progn
+        (unless (called-interactively-p)
+          (advice-add 'message :override #'akirak/ignore-toggle-word-wrap-message))
+        (funcall orig arg))
+    (advice-remove 'message #'akirak/ignore-toggle-word-wrap-message)))
+(advice-add #'toggle-word-wrap :around #'akirak/ad-around-toggle-word-wrap)
+
+(defun akirak/ignore-toggle-word-wrap-message (&rest args)
+  (apply 'ignore args))
+
 ;;;; Setting the typeface for reading
 
 (defvar akirak/reading-font)
