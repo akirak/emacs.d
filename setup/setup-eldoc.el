f6836feeb9c1b5791aeb8df239c36d935026427e
Akira Komamura <akira.komamura@gmail.com>
2019-01-13 23:25:18 +0900

    [eldoc] Ensure that the lv window is deleted before display-buffer

--- setup/setup-eldoc.el
+++ setup/setup-eldoc.el
@@ -5,13 +5,26 @@
 (autoload 'lv-message "lv")
 (autoload 'lv-delete-window "lv")
 
+(defvar akirak/eldoc-lv-window nil)
+
+(defun akirak/eldoc-message (message &rest args)
+  (apply #'lv-message format args)
+  (setq akirak/eldoc-lv-window t))
+
+(defun akirak/eldoc-delete-window (&rest _args)
+  (when akirak/eldoc-lv-window
+    (lv-delete-window)
+    (setq akirak/eldoc-lv-window nil)))
+
 ;; Use lv.el (which is in the same repo as hydra) to display
 ;; eldoc messages.
 (setq eldoc-message-function (cl-function
                               (lambda (format &rest args)
                                 (if (and format
                                          (not (string-empty-p format)))
-                                    (apply #'lv-message format args)
-                                  (lv-delete-window)))))
+                                    (apply #'akirak/eldoc-message format args)
+                                  (akirak/eldoc-delete-window)))))
+
+(advice-add #'display-buffer :before #'akirak/eldoc-delete-window)
 
 (provide 'setup-eldoc)
