e236f4a252dc451561498699a1bec23f31b51b88
Akira Komamura <akira.komamura@gmail.com>
2019-01-14 02:06:21 +0900

    Supply an alternative paste function for Wayland on Chrome OS

---
+++ setup/setup-clipboard.el
@@ -0,0 +1,20 @@
+;; If WAYLAND_DISPLAY environment variable is defined, the machine
+;; is considered to be running on Chrome OS, because I am currently
+;; using Xorg in non Chrome OS Linux environments.
+;; This can be changed in the future.
+(when (and (getenv "WAYLAND_DISPLAY")
+           (executable-find "wl-paste"))
+  (advice-add 'interprogram-paste-function
+              :filter-return #'akirak/ad-after-interprogram-paste-function))
+
+(defun akirak/ad-after-interprogram-paste-function (r)
+  ;; If the result from the original function is nil, use wl-paste
+  ;; to get the clipboard content.
+  (or r (akirak/paste-using-wl-paste)))
+
+(defun akirak/paste-using-wl-paste ()
+  (let ((s (shell-command-to-string "wl-paste")))
+    (unless (string-empty-p s)
+      (string-trim-right s))))
+
+(provide 'setup-clipboard)
