bb2f3dca759b14d1993c31cdbbeb9fe606d73041
Akira Komamura <akira.komamura@gmail.com>
2019-01-19 16:01:03 +0900

    Use helm-descbinds rather than a counsel equivalent

--- setup/setup-helm-descbinds.el
+++ setup/setup-helm-descbinds.el
@@ -1,7 +1,7 @@
 (use-package helm-descbinds :after helm
   :commands (helm-descbinds)
   :bind
-  ([remap describe-bindings] . helm-descbinds)
+  ([help ?b] . helm-descbinds)
   :custom
   (helm-descbinds-window-style 'split "Use pop-up style window for descbinds"))
 
