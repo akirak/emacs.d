3e7784eebf902feb509947b29500b3a388e9ccfe
Akira Komamura <akira.komamura@gmail.com>
2019-01-19 17:28:53 +0900

    [elixir] Set the alchemist prefix to C-,; Add which-key prefix helps

--- setup/setup-elixir.el
+++ setup/setup-elixir.el
@@ -1,10 +1,26 @@
 ;; Alchemist package for Elixir support on Emacs
 ;; See https://alchemist.readthedocs.io/en/latest/configuration/ for setup
-(use-package alchemist
-  :general
-  (:keymaps 'elixir-mode-map
-            "C-c d s" 'alchemist-server-start)
-  :custom
-  (alchemist-key-command-prefix "C-c ,"))
+(use-package alchemist)
+
+(defconst akirak/alchemist-key-command-prefix "C-,")
+(general-setq alchemist-key-command-prefix
+              (kbd akirak/alchemist-key-command-prefix))
+
+(general-define-key :keymaps 'alchemist-mode-map
+                    :package 'alchemist
+                    :prefix akirak/alchemist-key-command-prefix
+                    "c" '(nil :wk "compile")
+                    "e" '(nil :wk "execute")
+                    "f" '(nil :wk "point")
+                    "m" '(nil :wk "mix")
+                    "mt" '(nil :wk "mix-test")
+                    "X" '(nil :wk "hex")
+                    "h" '(nil :wk "help")
+                    "p" '(nil :wk "project")
+                    "i" '(nil :wk "iex")
+                    "v" '(nil :wk "eval")
+                    "o" '(nil :wk "macroexpand"))
+
+(akirak/which-key-add-stripped-prefix "alchemist-")
 
 (provide 'setup-elixir)
