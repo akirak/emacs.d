48ae7245ecc914f01dc33167aff1de7d55c8f9f9
Akira Komamura <akira.komamura@gmail.com>
2019-01-28 21:20:57 +0900

    Avoid using GTK tooltips

--- setup/setup-defaults.el
+++ setup/setup-defaults.el
@@ -33,6 +33,7 @@
 
 (setq inhibit-startup-screen t
       ring-bell-function 'ignore
+      x-gtk-use-system-tooltips nil
       ediff-window-setup-function #'ediff-setup-windows-plain)
 
 (setq bookmark-default-file "~/.emacs-bookmarks")
