2c3d980919d6b024b77c1a6f3192590e0cedf4a2
Akira Komamura <akira.komamura@gmail.com>
2019-01-28 21:19:54 +0900

    [counsel] Show entries visited by counsel-org-goto

--- setup/setup-counsel.el
+++ setup/setup-counsel.el
@@ -6,4 +6,9 @@
   (global-set-key [remap recentf-open-files] 'counsel-recentf)
   (global-set-key [remap insert-char] 'counsel-unicode-char))
 
+(defun akirak/ad-after-counsel-org-goto-action (_x)
+  (org-show-entry))
+(advice-add 'counsel-org-goto-action :after
+            'akirak/ad-after-counsel-org-goto-action)
+
 (provide 'setup-counsel)
