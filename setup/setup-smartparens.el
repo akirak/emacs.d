c16c624175824ec3512da33a51760e6f4c34cba1
Akira Komamura <akira.komamura@gmail.com>
2019-01-29 07:13:09 +0900

    [smartparens] Fix: Remove redundant/unbound keybindings

--- setup/setup-smartparens.el
+++ setup/setup-smartparens.el
@@ -4,12 +4,7 @@
   (show-smartparens-global-mode 1)
   :general
   (:keymaps 'smartparens-mode-map
-            [remap forward-sexp] #'sp-forward-sexp
-            [remap backward-sexp] #'sp-backward-sexp
-            [remap backward-up-list] #'sp-backward-up-sexp
-            [remap down-list] #'sp-down-sexp
-            [remap forward-list] #'sp-forward-sexp
-            [remap backward-list] #'sp-backward-sexp)
+            [remap down-list] #'sp-down-sexp)
   :hook
   (prog-mode . turn-on-smartparens-strict-mode))
 
