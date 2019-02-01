27fedfdc1e2be3aa19a9a177c0b3aa514ba0c3c3
Akira Komamura <akira.komamura@gmail.com>
2019-02-02 00:36:28 +0900

    [elixir] Rewrite counsel-outline-settings (#2)

--- setup/setup-elixir.el
+++ setup/setup-elixir.el
@@ -23,35 +23,99 @@
 
 (akirak/which-key-add-stripped-prefix "alchemist-")
 
+(defconst akirak/elixir-outline-regexp
+  (rx bol (or (seq (group (* space))
+                   (group (or (sequence "def" (* alpha))
+                              (sequence "@" (+ (not space)))
+                              "alias"
+                              "import"
+                              "require"
+                              "use"))
+                   (+ space)
+                   (group (*? any))
+                   (or ", do:" (seq " do" eol) eol))
+              (seq (group-n 1 "  ")
+                   (group-n 2 (or (seq (not (any "e" space)) (* alpha))
+                                  (seq "e" (not (any "n" space)) (* alpha))
+                                  (seq "en" (not (any "d" space)) (* alpha))))
+                   (+ space)
+                   (group-n 3 (*? any))
+                   (or ", do:" (seq " do" eol)))
+              (seq (group-n 1 (* space))
+                   (group-n 4 (sequence "def" (* alpha)))
+                   "("
+                   (group-n 5 (+ alnum))
+                   (group-n 6 ", do:" (*\? any))
+                   ")"
+                   eol))))
+
+(defun akirak/elixir-outline-title ()
+  (let ((s1 (match-string 2))
+        (s2 (match-string 3))
+        (s3 (match-string 4)))
+    (or (when (match-string 5)
+          (concat (match-string 4)
+                  " "
+                  (match-string 5)
+                  (match-string 6)))
+        (pcase s1
+          ("@derive" (concat s1 " " s2))
+          ((pred (string-prefix-p "@")) s1)
+          ("defstruct" s1)
+          ;; ("defprotocol" s2)
+          ("defprotocol" (concat s2 " " s3))
+          ((pred (string-prefix-p "def")) (concat s1 " " s2 s3))
+          (_ (concat s1 " " s2))))))
+
+(defun akirak/elixir-outline-level ()
+  (/ (length (match-string 1)) 2))
+
+(defun akirak/elixir-counsel-outline-action (x)
+  (counsel-outline-action x)
+  (back-to-indentation))
+
 (add-to-list 'counsel-outline-settings
-             `(elixir-mode
-               :outline-regexp
-               ,(rx (seq (* space)
-                         (group (or (sequence "def" (* (not space)))
-                                    (sequence "@" (* (not space)))
-                                    "alias"
-                                    "import"
-                                    "require"
-                                    "use"
-                                    "test"
-                                    "doctest"
-                                    "setup_all"))
-                         (+ space)
-                         (group (*? any))
-                         (or ", do:" " do" eol)))
-               :outline-level
-               (lambda ()
-                 (pcase (match-string 1)
-                   ("defmodule" 1)
-                   (_ 2)))
-               :outline-title
-               (lambda ()
-                 (let ((s1 (match-string 1))
-                       (s2 (match-string 2)))
-                   (pcase s1
-                     ((pred (string-prefix-p "@")) s1)
-                     ("defstruct" s1)
-                     ((pred (string-prefix-p "def")) s2)
-                     (_ (concat s1 " " s2)))))))
+             `(elixir-mode :outline-regexp ,akirak/elixir-outline-regexp
+                           :outline-level akirak/elixir-outline-level
+                           :outline-title akirak/elixir-outline-title
+                           :action akirak/elixir-counsel-outline-action
+                           :caller akirak/counsel-outline-elixir))
+
+(defun akirak/counsel-outline-elixir-display-transformer (orig)
+  (--> orig
+       (split-string it counsel-outline-path-separator)
+       (-map #'string-trim it)
+       (-map (lambda (s)
+               (cond
+                ((string-match (rx bol
+                                   (group "def" (* (not space)))
+                                   " ")
+                               s)
+                 (let ((prefix (pcase (substring-no-properties (match-string 1 s))
+                                 ("defmodule" nil)
+                                 ("defp" nil)
+                                 ("def" nil)
+                                 (s1 s1)))
+                       (body (string-remove-prefix (match-string 0 s) s)))
+                   (if prefix
+                       (concat (propertize prefix 'face 'font-lock-type-face)
+                               " " body)
+                     body)))
+                ((string-prefix-p "@" s)
+                 s)
+                ((string-match (rx bol (* (not space))) s)
+                 (let* ((prefix (substring-no-properties (match-string 0 s)))
+                        (body (string-remove-prefix prefix s)))
+                   (if (string-empty-p (string-trim body))
+                       s
+                     (concat (propertize prefix 'face 'font-lock-type-face)
+                             body))))
+                (t (error s))))
+             it)
+       (mapconcat #'identity it counsel-outline-path-separator)))
+
+(ivy-set-display-transformer
+ 'akirak/counsel-outline-elixir
+ 'akirak/counsel-outline-elixir-display-transformer)
 
 (provide 'setup-elixir)
