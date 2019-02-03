c96d69c8cd95ff467ad052414cdd6f8418657004
Akira Komamura <akira.komamura@gmail.com>
2019-02-03 19:01:11 +0900

    Add plumber

--- setup/setup-smartparens.el
+++ setup/setup-smartparens.el
@@ -2,9 +2,6 @@
   :config
   (require 'smartparens-config)
   (show-smartparens-global-mode 1)
-  :general
-  (:keymaps 'smartparens-mode-map
-            [remap down-list] #'sp-down-sexp)
   :hook
   (prog-mode . turn-on-smartparens-strict-mode))
 
