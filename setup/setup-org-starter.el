60792f352d55a92c76a679fb2a7ebbac8d03259c
Akira Komamura <akira.komamura@gmail.com>
2019-02-25 01:27:39 +0900

    [org-starter] Tidy

--- setup/setup-org-starter.el
+++ setup/setup-org-starter.el
@@ -64,18 +64,18 @@ DEADLINE: %T SCHEDULED: %T
 ;; https://www.reddit.com/r/emacs/comments/9ycgoe/do_you_guys_have_a_personal_wiki_using_orgmode_or/ea0j8nw/
 
 (org-starter-def-capture "cp" "cpb.org: Plain entry (with %i as body)"
-                         entry (file+function "cpb.org" org-reverse-datetree-goto-date-in-file)
-                         "* %^{Heading}
+  entry (file+function "cpb.org" org-reverse-datetree-goto-date-in-file)
+  "* %^{Heading}
 :PROPERTIES:
 :CREATED_TIME: %U
 :END:
 
 %(unless (string-empty-p \"%i\") \"%i\n\n\")%?"
-                         :clock-in t :clock-resume t :empty-lines 1)
+  :clock-in t :clock-resume t :empty-lines 1)
 
 (org-starter-def-capture "cq" "cpb.org: Quote"
-                         entry (file+function "cpb.org" org-reverse-datetree-goto-date-in-file)
-                         "* %(if (string-empty-p \"%a\") \"%^{Heading}\" \"%a\")
+  entry (file+function "cpb.org" org-reverse-datetree-goto-date-in-file)
+  "* %(if (string-empty-p \"%a\") \"%^{Heading}\" \"%a\")
 :PROPERTIES:
 :CREATED_TIME: %U
 :END:
@@ -86,7 +86,7 @@ DEADLINE: %T SCHEDULED: %T
 %x
 #+END_QUOTE
 \")"
-                         :clock-in t :clock-resume t :empty-lines 1)
+  :clock-in t :clock-resume t :empty-lines 1)
 
 (org-starter-def-capture "cl" "cpb.org: Link to web page"
                          entry (file+function "cpb.org" org-reverse-datetree-goto-date-in-file)
