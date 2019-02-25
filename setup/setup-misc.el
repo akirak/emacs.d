033f25418cc49d278d06a89cea01c7fd3a9b463b
Akira Komamura <akira.komamura@gmail.com>
2019-02-25 23:16:07 +0900

    [org-starter] Configure more capture templates and refile functions

--- setup/setup-misc.el
+++ setup/setup-misc.el
@@ -61,10 +61,6 @@
   :config
   (add-to-list 'scratch-mode-alist '(helpful-mode . emacs-lisp-mode))
   (add-to-list 'scratch-mode-alist '(help-mode . emacs-lisp-mode)))
-(use-package embrace
-  ;; I probably don't need this package. Use sp-{rewrap,unwrap}-sexp
-  ;; instead
-  :disabled t)
 (use-package dumb-jump
   ;; Don't enable dumb-jump-mode. Bind only necessary commands. 
   :custom
@@ -79,9 +75,6 @@
   :custom
   (counsel-org-clock-goto-fallback-function 'org-agenda)
   (counsel-org-clock-default-action 'clock-in))
-(use-package org-reverse-datetree
-  :straight (org-reverse-datetree :host github
-                                  :repo "akirak/org-reverse-datetree"))
 ;; Edit Org-Mode lists like in word processors
 (use-package org-autolist 
   :after org
--- setup/setup-org-starter.el
+++ setup/setup-org-starter.el
@@ -9,10 +9,13 @@
   ;; `org-starter-initial-capture-templates` is defined in setup-org-capture.el
   (org-starter-initial-capture-templates
    '(("a" "Append to the current clock")
-     ("c" "cpb.org & code.org")))
+     ("c" "cpb.org & code.org")
+     ("t" "Task")))
   (org-starter-require-file-by-default nil)
   (org-starter-exclude-from-recentf '(known-files path))
   (org-starter-enable-local-variables :all))
+
+(use-package org-reverse-datetree)
 
 (defun helm-org-rifle-known-files ()
   (interactive)
@@ -22,9 +25,36 @@
 (akirak/bind-mode :keymaps 'org-mode-map :package 'org
   "r" #'org-starter-refile-by-key)
 
+;;;; Helper macros and functions
+
+(cl-defmacro akirak/def-org-reverse-datetree-refile (file
+                                                     &rest args
+                                                     &key key prefer
+                                                     &allow-other-keys)
+  "Define a refile function as well as a keybinding."
+  (declare (indent 1))
+  (let* ((basename (file-name-base file))
+         (name (intern (concat "akirak/org-refile-to-" basename))))
+    `(defun ,name (arg)
+       (interactive "P")
+       (org-reverse-datetree-refile-to-file
+        (org-starter-locate-file ,file nil t) nil
+        :prefer ,(or prefer '("CREATED_TIME" "CREATED_AT" "CLOSED"))
+        ,@args))
+    `(when (quote ,key)
+       (add-to-list 'org-starter-extra-refile-map
+                    '(,key ,name ,basename)))))
+
+(defun akirak/buffer-mode-name (filename)
+  "Return the current major mode name without \"-mode\".
+
+Used in source blocks."
+  (with-current-buffer (find-buffer-visiting filename)
+    (string-remove-suffix "-mode" (symbol-name major-mode))))
+
 ;;;; Workflow
 
-;;;;; Generic tasks and notes
+;;;;; Basic task templates
 
 (org-starter-def-capture "g" "Generic entry in the inbox (with %i as title)"
   entry (file "scratch.org")
@@ -35,27 +65,92 @@
 "
   :clock-in t :clock-resume t :empty-lines 1)
 
-(org-starter-def-capture "t" "Scheduled task (with %i as title)"
+(org-starter-def-capture "tt" "Task without schedule"
   entry (file "taskpool.org")
-  "* TODO %i%?
+  "* TODO %^{Heading}
+:PROPERTIES:
+:CREATED_TIME: %U
+:END:
+
+%i%?
+"
+  :clock-in t :clock-resume t :empty-lines 1)
+
+(org-starter-def-capture "ts" "Scheduled task (with %i as title)"
+  entry (file "taskpool.org")
+  "* TODO %^{Heading}
 DEADLINE: %^{Deadline}T SCHEDULED: %^{When to do}t
 :PROPERTIES:
 :CREATED_TIME: %U
 :END:
+
+%i%?
 "
   :clock-in t :clock-resume t :empty-lines 1)
 
-(org-starter-def-capture "u" "Urgent task (with %i as title)"
+(org-starter-def-capture "tu" "Urgent task (with %i as title)"
   entry (file "taskpool.org")
-  "* STARTED %i%?
-DEADLINE: %T SCHEDULED: %T
+  "* STARTED %^{Heading} @urgent
+DEADLINE: %^{Deadline}T SCHEDULED: %t
 :PROPERTIES:
 :CREATED_TIME: %U
 :END:
+
+%i%?
 "
   :clock-in t :clock-resume t)
 
 ;;;; Appending contents to the current clock
+
+(org-starter-def-capture "as" "Source block"
+  plain (clock)
+  "#+BEGIN_SRC %(akirak/buffer-mode-name \"%F\")
+%i
+#+END_SRC" :empty-lines 1 :immediate-finish t :no-save t)
+
+(org-starter-def-capture "al" "Link"
+  plain (clock)
+  "%A"
+  :empty-lines 1 :immediate-finish t :no-save t)
+
+(org-starter-def-capture "ai" "Item"
+  item (clock)
+  "%i%?"
+  :unnarrowed t :no-save t)
+
+(org-starter-def-capture "ac" "Check item"
+  checkitem (clock)
+  "[ ] %i%?"
+  :unnarrowed t :no-save t)
+
+(org-starter-def-capture "at" "Entry"
+  entry (clock)
+  "* %?"
+  :unnarrowed t :clock-in t :clock-resume t :empty-lines 1 :no-save t)
+
+(org-starter-def-capture "ae" "Example"
+  plain (clock)
+  "#+begin_example
+%i
+#+end_example"
+  :empty-lines 1 :immediate-finish t :no-save t)
+
+(org-starter-def-capture "aa" "Text"
+                         plain (clock)
+                         "%?"
+                         :empty-lines 1 :unnarrowed t :no-save t)
+
+(org-starter-def-capture "aw" "Web page")
+
+(org-starter-def-capture "aww" "Plain link"
+  plain (clock)
+  "%(org-web-tools--org-link-for-url)"
+  :immediate-finish t :no-save t)
+
+(org-starter-def-capture "awi" "Link as item"
+  item (clock)
+  "%(org-web-tools--org-link-for-url)"
+  :immediate-finish t :no-save t :empty-lines 1)
 
 ;;;; Commonplace book
 
@@ -103,17 +198,9 @@ DEADLINE: %T SCHEDULED: %T
   :clock-in t :clock-resume t :empty-lines 1)
 
 ;;;;; Misc
+;;;;;; journal.org
 (org-starter-def-capture "j" "journal.org: New entry (with %i as body)"
-                         entry (file+function "journal.org" org-reverse-datetree-goto-read-date-in-file)
-                         "* %^{Title}
-:PROPERTIES:
-:CREATED_TIME: %U
-:END:
-%(unless (string-empty-p \"%i\") \"%i\n\n\")%?"
-                         :clock-in t :clock-resume t :empty-lines 1)
-
-(org-starter-def-capture "co" "code.org: Stub entry (with %i as body)"
-  entry (file+function "code.org" org-reverse-datetree-goto-read-date-in-file)
+  entry (file+function "journal.org" org-reverse-datetree-goto-read-date-in-file)
   "* %^{Title}
 :PROPERTIES:
 :CREATED_TIME: %U
@@ -121,6 +208,34 @@ DEADLINE: %T SCHEDULED: %T
 
 %(unless (string-empty-p \"%i\") \"%i\n\n\")%?"
   :clock-in t :clock-resume t :empty-lines 1)
+
+(akirak/def-org-reverse-datetree-refile "journal.org"
+  :key "j"
+  :ask-always t :prefer '("SCHEDULED" "CLOSED" "CREATED_TIME" "CREATED_AT"))
+
+(defun akirak/org-schedule-journal-entry (date)
+  (interactive (list (org-read-date nil nil nil "Scheduled date: ")))
+  (unless (derived-mode-p 'org-mode)
+    (user-error "Not in org-mode"))
+  (org-schedule nil date)
+  (org-reverse-datetree-refile-to-file
+   (org-starter-locate-file "journal.org" nil t) date))
+
+;;;;;; code.org
+(org-starter-def-capture "co" "code.org: Stub entry (with %i as body)"
+  entry (file+function "code.org" org-reverse-datetree-goto-read-date-in-file)
+  "* %^{Title}
+:PROPERTIES:
+:CREATED_TIME: %U
+:working_on: %k
+:END:
+
+%(unless (string-empty-p \"%i\") \"%i\n\n\")%?"
+  :clock-in t :clock-resume t :empty-lines 1)
+
+(akirak/def-org-reverse-datetree-refile "code.org"
+  :key "co"
+  :ask-always arg :prefer '("CLOSED" "CREATED_TIME" "CREATED_AT"))
 
 ;;;; Resources
 ;;;;; org-capture
