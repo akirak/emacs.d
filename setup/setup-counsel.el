ba744dc12375e7807ad3457a59cf1ef4cf3774df
Akira Komamura <akira.komamura@gmail.com>
2019-03-02 10:35:32 +0900

    Add find-*-other-window action to counsel-describe-* commands and M-x

--- setup/setup-counsel.el
+++ setup/setup-counsel.el
@@ -4,6 +4,13 @@
   (counsel-mode 1) ; Remap built-in functions with counsel equivalents
   (ivy-add-actions #'counsel-find-library
                    '(("l" load-library "load")))
+  (cl-loop for (command find-other-window)
+           in '((counsel-describe-function find-function-other-window)
+                (counsel-describe-variable find-variable-other-window)
+                (counsel-M-x find-function-other-window))
+           do (ivy-add-actions command
+                               `(("j" ,(-compose find-other-window 'intern)
+                                  "definition in other window"))))
   (global-set-key [remap recentf-open-files] 'counsel-recentf)
   (global-set-key [remap insert-char] 'counsel-unicode-char))
 
