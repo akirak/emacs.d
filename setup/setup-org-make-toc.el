984ba2502174a30a3a352ffff93c52dbce0d4f32
Akira Komamura <akira.komamura@gmail.com>
2019-03-09 15:30:31 +0900

    [org-make-toc] Add akirak/org-insert-toc-for-top-level command
    and clean up unused functions.

--- setup/setup-org-make-toc.el
+++ setup/setup-org-make-toc.el
@@ -1,13 +1,28 @@
 (use-package org-make-toc
   :after (org)
+  :commands (org-make-toc)
   :config
-  (defvar org-make-toc nil
-    "If non-nil, turn on `org-make-toc-mode' in the buffer.")
-  (make-variable-buffer-local 'org-make-toc)
-  (defun maybe-turn-on-org-make-toc-mode ()
-    (when org-make-toc
-      (org-make-toc-mode 1)))
-  :hook
-  (org-mode . maybe-turn-on-org-make-toc-mode))
+  (defun akirak/org-insert-toc-for-top-level ()
+    (interactive)
+    (unless (derived-mode-p 'org-mode)
+      (user-error "Not in org-mode"))
+    (org-with-wide-buffer
+     (or (re-search-backward (rx bol "* ") nil t)
+         (re-search-forward (rx bol "* ") nil t))
+     (org-narrow-to-subtree)
+     (when (org-find-property "TOC")
+       (user-error "Already has a TOC"))
+     (let ((heading (concat (make-string (org-get-valid-level 2) ?\*)
+                            " ")))
+       (if (re-search-forward (concat "^" (regexp-quote heading))
+                              nil t)
+           (beginning-of-line 1)
+         (org-end-of-subtree)
+         (unless (= 0 (car (posn-col-row (posn-at-point))))
+           (insert "\n")))
+       (insert heading "Table of contents\n")
+       (beginning-of-line 0)
+       (org-set-property "TOC" "siblings")))
+    (add-file-local-variable 'before-save-hook 'org-make-toc)))
 
 (provide 'setup-org-make-toc)
