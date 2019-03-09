0edd4690d8b84c5922157ddab47f50c8d050c86b
Akira Komamura <akira.komamura@gmail.com>
2019-03-09 19:17:42 +0900

    [themes] Add support for multiple themes
    - Allow the user to choose a theme from the command line
    - Set the outshine faces for dracula if and only if dracula theme is loaded

--- setup/setup-outshine.el
+++ setup/setup-outshine.el
@@ -3,11 +3,14 @@
 
 (use-package outshine
   :config
-  (dolist (i (mapcar #'int-to-string (number-sequence 1 8)))
-    (let ((target (intern (concat "outshine-level-" i)))
-          (parent (intern (concat "org-level-" i))))
-      (custom-theme-set-faces
-       'dracula `(,target ((default :inherit ,parent))))))
+  (defun akirak/set-outshine-level-faces (theme)
+    (dolist (i (mapcar #'int-to-string (number-sequence 1 8)))
+      (let ((target (intern (concat "outshine-level-" i)))
+            (parent (intern (concat "org-level-" i))))
+        (custom-theme-set-faces
+         theme `(,target ((default :inherit ,parent)))))))
+  (eval-after-load 'dracula-theme
+    (akirak/set-outshine-level-faces 'dracula))
   :general
   (:keymaps 'outline-minor-mode-map
             "C-M-i" nil)
