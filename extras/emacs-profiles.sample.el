fa3af82443619cc31ae84540e46570373de28b20
Akira Komamura <akira.komamura@gmail.com>
2019-03-16 20:46:31 +0900

    General maintenance
    Changes in README:
    
    - Add CI status badges for master and maint branches
    - Delete the meta section
    - Minor rewrite of a sentence
    
    Other changes:
    
    - Stop loading the external babel configuration
      since I've shifted to local configuration with org-starter.
    - Delete the old sample config for chemacs

--- extras/emacs-profiles.sample.el
+++
@@ -1,8 +0,0 @@
-;;; Chemacs configuration file
-
-;;; Usage:
-;; Install chemacs https://github.com/plexus/chemacs
-;; and use this file as ~/.emacs-profiles.el.
-
-(("default" . ((user-emacs-directory . "~/.emacs.d")
-               (custom-file . "~/.custom.el"))))
