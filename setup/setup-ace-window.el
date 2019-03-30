c3072274a4c33cf352d7c186455c14fe9304daa2
Akira Komamura <akira.komamura@gmail.com>
2019-03-31 01:04:27 +0900

    [ace-window] Replace window-hydra with ace-window

--- setup/setup-ace-window.el
+++ setup/setup-ace-window.el
@@ -1,18 +1,25 @@
 (use-package ace-window
   :custom
-  (aw-keys '(?a ?s ?d ?f ?g))
+  (aw-keys (string-to-list "qwertyui"))
   (aw-background nil)
-  ;; (aw-dispatch-alist
-  ;;  '((?m aw-swap-window "Swap Windows")
-  ;;    (?x aw-delete-window "Delete Window")
-  ;;    (?c aw-split-window-horz "Split horizontally")
-  ;;    (?v aw-split-window-vert "Split vertically")
-  ;;    (?b balance-windows)
-  ;;    (?r toggle-window-split)
-  ;;    (?o delete-other-windows)
-  ;;    (?D delete-frame)
-  ;;    (?z iconify-frame)
-  ;;    (?? aw-show-dispatch-help)))
+  ;; Ergonomic bindings
+  (aw-dispatch-alist
+   '((?o aw-swap-window "Swap Windows")
+     (?p aw-delete-window "Delete Window")
+     (32 toggle-window-split)
+     (?d delete-frame)
+     (?f make-frame-command)
+     (?g magit-status)
+     (?z akirak/minor-mode-hydra/body)
+     (?a treemacs)
+     (?n aweshell-dedicated-open)
+     (?s ibuffer-sidebar-toggle-sidebar)
+     (?x dired-sidebar-toggle-sidebar)
+     (?h windmove-left)
+     (?j windmove-down)
+     (?k windmove-up)
+     (?l windmove-right)
+     (?? aw-show-dispatch-help)))
   (aw-ignored-buffers '("\\*helm"
                         " *LV*"
                         minibuffer-mode
@@ -20,5 +27,16 @@
   (aw-dispatch-always t)
   (aw-scope 'frame)
   (aw-ignore-on t))
+
+(defhydra akirak/minor-mode-hydra (:hint nil)
+  "
+_d_ debug-on-error [%s(if debug-on-error \"on\" \"off\")]
+_f_ focus-mode [%s(if focus-mode \"on\" \"off\")]
+_v_ view-mode [%s(if view-mode \"on\" \"off\")]
+"
+  ("d" toggle-debug-on-error)
+  ("f" focus-mode)
+  ("v" view-mode)
+  ("q" nil "quit" :exit t))
 
 (provide 'setup-ace-window)
