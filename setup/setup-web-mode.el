800c08a40cb1aca483da57b2a98a02e5fc450ce0
Akira Komamura <akira.komamura@gmail.com>
2019-03-31 22:16:28 +0900

    [web-mode] Add company-web

--- setup/setup-web-mode.el
+++ setup/setup-web-mode.el
@@ -1,4 +1,11 @@
 (use-package web-mode
   :mode "\\.vue\\'")
 
+(use-package company-web
+  :hook
+  (web-mode . (lambda ()
+                (setq-local compan-backends
+                            '(company-web-html
+                              company-capf)))))
+
 (provide 'setup-web-mode)
