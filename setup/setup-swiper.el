a3fac6058653525a1c13c313c6045439cc12f5cd
Akira Komamura <akira.komamura@gmail.com>
2019-04-08 00:03:58 +0900

    [swiper] Adopt swiper-isearch

--- setup/setup-swiper.el
+++ setup/setup-swiper.el
@@ -2,8 +2,13 @@
   :config
   (add-to-list 'swiper-font-lock-exclude 'emacs-lisp-mode)
   (add-to-list 'swiper-font-lock-exclude 'org-mode)
+  (when (and (not (fboundp 'swiper-isearch))
+             (yes-or-no-p "swiper-isearch is unavailable. Would you want to upgrade swiper? "))
+    (straight-pull-package "swiper")
+    (straight-rebuild-package "swiper")
+    (load "swiper"))
   :general
-  ("C-s" #'swiper
-   "C-r" #'swiper))
+  ("C-s" #'swiper-isearch
+   "C-r" #'swiper-isearch))
 
 (provide 'setup-swiper)
