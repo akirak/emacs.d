ec0e9f1c1a3b2cf0ea3c6457623440692582a4a6
Akira Komamura <akira.komamura@gmail.com>
2019-04-09 00:28:58 +0900

    [counsel] Add other-window and other-frame actions to counsel-rg

--- setup/setup-counsel.el
+++ setup/setup-counsel.el
@@ -11,6 +11,13 @@
            do (ivy-add-actions command
                                `(("j" ,(-compose find-other-window 'intern)
                                   "definition in other window"))))
+  (ivy-add-actions #'counsel-rg
+                   `(("j" ,(-partial #'akirak/counsel-git-grep-action-with-find-file
+                                     #'find-file-other-window)
+                      "other window")
+                     ("f" ,(-partial #'akirak/counsel-git-grep-action-with-find-file
+                                     #'find-file-other-frame)
+                      "other frame")))
   (global-set-key [remap recentf-open-files] 'counsel-recentf)
   (global-set-key [remap insert-char] 'counsel-unicode-char))
 
@@ -18,5 +25,22 @@
   (org-show-entry))
 (advice-add 'counsel-org-goto-action :after
             'akirak/ad-after-counsel-org-goto-action)
+
+(defun akirak/counsel-git-grep-action-with-find-file (find-file-func x)
+  "Go to occurrence X in current Git repository."
+  (when (string-match "\\`\\(.*?\\):\\([0-9]+\\):\\(.*\\)\\'" x)
+    (let ((file-name (match-string-no-properties 1 x))
+          (line-number (match-string-no-properties 2 x)))
+      (funcall find-file-func (expand-file-name
+                               file-name
+                               (ivy-state-directory ivy-last)))
+      (goto-char (point-min))
+      (forward-line (1- (string-to-number line-number)))
+      (re-search-forward (ivy--regex ivy-text t) (line-end-position) t)
+      (swiper--ensure-visible)
+      (run-hooks 'counsel-grep-post-action-hook)
+      (unless (eq ivy-exit 'done)
+        (swiper--cleanup)
+        (swiper--add-overlays (ivy--regex ivy-text))))))
 
 (provide 'setup-counsel)
