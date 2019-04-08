ec6e97cbe5a27aa540273a37d2316a50bc9bd338
Akira Komamura <akira.komamura@gmail.com>
2019-04-09 00:36:09 +0900

    [clipurl] Add action for ivy-clipurl to add a link to the clock

--- setup/setup-clipurl.el
+++ setup/setup-clipurl.el
@@ -8,6 +8,11 @@
   (ivy-add-actions 'ivy-clipurl
                    '(("c" akirak/git-clone "git clone")
                      ("r" org-web-tools-read-url-as-org "Read as Org")
-                     ("u" akirak/straight-use-package-git-url "use-package"))))
+                     ("u" akirak/straight-use-package-git-url "use-package")
+                     ("a" akirak/capture-url-as-link-item "Append item to clock"))))
+
+(defun akirak/capture-url-as-link-item (url)
+  (akirak/org-capture-item
+   (org-web-tools--org-link-for-url url)))
 
 (provide 'setup-clipurl)
