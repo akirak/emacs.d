0c8f0ba48158b6cde8ed1f2fb0d9a87760c6006c
Akira Komamura <akira.komamura@gmail.com>
2019-04-13 23:04:17 +0900

    [python] Change the status to todo, since I don't use Pytnon

--- setup/setup-python.el
+++ setup/setup-python.el
@@ -1,7 +1,7 @@
 (use-package lsp-python :after lsp
   :hook
   (python-mode . lsp-python-enable)
-  ;; FIXME: Which package on Nix?
+  ;; TODO: Install the executable using nix or something
   ;; :ensure-system-package python-language-server
   )
 
