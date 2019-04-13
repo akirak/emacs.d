a06b0bb9c844a4d54401e9082d9b1cbe9cc7f4dd
Akira Komamura <akira.komamura@gmail.com>
2019-04-13 15:23:27 +0900

    [haskell] Enable haskell-mode again and add haskell-interactive-mode

--- setup/setup-haskell.el
+++ setup/setup-haskell.el
@@ -4,21 +4,24 @@
   (add-hook 'haskell-mode-hook 'intero-mode))
 
 (use-package haskell-mode
-  :disabled t
   :mode ("\\.hs\\'" . haskell-mode))
 
 (use-package dante
-  :disabled t
   :commands (dante-mode)
   :init
   (add-hook 'haskell-mode-hook 'dante-mode))
 
-;; To use lsp-haskell, you also need to install haskell-ide-server.
-;; This can take a lot of time, so I will disable lsp-haskell for now.
 (use-package lsp-haskell
-  :disabled t
+  ;; To use lsp-haskell, you also need to install haskell-ide-server.
+  ;; This can take a lot of time, so install it manually if you want
+  ;; to write Haskell code.
+  :if (executable-find "haskell-ide-server")
   :straight (lsp-haskell :host github :repo "emacs-lsp/lsp-haskell")
   :hook
   (haskell-mode . lsp-haskell-enable))
+
+(use-package haskell-interactive-mode
+  :disabled t
+  :commands interactive-haskell-mode)
 
 (provide 'setup-haskell)
