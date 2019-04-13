eb21d7aa12764b730503fd8c69f7be94a8de83d6
Akira Komamura <akira.komamura@gmail.com>
2019-04-13 12:40:57 +0900

    [web-mode] Associate the mode with a bunch of extensions

--- setup/setup-web-mode.el
+++ setup/setup-web-mode.el
@@ -1,3 +1,16 @@
-(use-package web-mode)
+(use-package web-mode
+  :mode (("\\.erb\\'" . web-mode)
+         ("\\.mustache\\'" . web-mode)
+         ("\\.html?\\'" . web-mode)
+         ("\\.php\\'" . web-mode)
+         ("\\.jsp\\'" . web-mode)
+         ;; ("\\.jsx?$" . web-mode)
+         ("\\.es6\\'" . web-mode)
+         ("\\.ejs\\'" . web-mode)
+         ("\\.phtml\\'" . web-mode)
+         ("\\.tpl\\.php\\'" . web-mode)
+         ("\\.[agj]sp\\'" . web-mode)
+         ("\\.as[cp]x\\'" . web-mode)
+         ("\\.djhtml\\'" . web-mode)))
 
 (provide 'setup-web-mode)
