979620c5ff47e9f47c7996e2e6bbc5d2fd51cb89
Akira Komamura <akira.komamura@gmail.com>
2019-04-20 17:26:24 +0900

    [minor-mode-hydra] Change the display

--- setup/setup-minor-mode-hydra.el
+++ setup/setup-minor-mode-hydra.el
@@ -1,23 +1,28 @@
 (defun akirak/minor-mode-hydra-status (mode)
-  (format "%s %-5s"
-          (symbol-name mode)
-          (format "[%s]" (if (bound-and-true-p mode) "on" "off"))))
+  (format "(%s) %s"
+          (if (and (boundp mode) mode) "+" "-")
+          (string-remove-suffix "-mode" (symbol-name mode))))
 
-(defhydra akirak/minor-mode-hydra (:exit nil)
+(defhydra akirak/minor-mode-hydra (:exit nil
+                                         :hint nil
+                                         :foreign-keys warn)
   "
-_d_ %s(akirak/minor-mode-hydra-status 'debug-on-error)
-_f_ %s(akirak/minor-mode-hydra-status 'focus-mode)
-_v_ %s(akirak/minor-mode-hydra-status 'view-mode)
-
-_s_ %s(akirak/minor-mode-hydra-status 'smartparens-mode)
-_S_ %s(akirak/minor-mode-hydra-status 'smartparens-strict-mode)
-_l_ %s(akirak/minor-mode-hydra-status 'lispy-mode)
-_p_ %s(akirak/minor-mode-hydra-status 'page-break-lines-mode)
-_ed_ %s(akirak/minor-mode-hydra-status 'eldoc-mode)
-_el_ %s(akirak/minor-mode-hydra-status 'electric-layout-mode)
-_ei_ %s(akirak/minor-mode-hydra-status 'electric-indent-local-mode)
-_eq_ %s(akirak/minor-mode-hydra-status 'elecric-quote-local-mode)
-
+[focus]
+  _v_ %s(akirak/minor-mode-hydra-status 'view-mode)\
+  _f_ %s(akirak/minor-mode-hydra-status 'focus-mode)
+[info]
+  _d_ %s(akirak/minor-mode-hydra-status 'debug-on-error)\
+  _ed_ %s(akirak/minor-mode-hydra-status 'eldoc-mode)
+[edit]
+  _s_ %s(akirak/minor-mode-hydra-status 'smartparens-mode)\
+  _S_ %s(akirak/minor-mode-hydra-status 'smartparens-strict-mode)\
+  _l_ %s(akirak/minor-mode-hydra-status 'lispy-mode)
+[assistance]
+  _el_ %s(akirak/minor-mode-hydra-status 'electric-layout-mode)\
+  _ei_ %s(akirak/minor-mode-hydra-status 'electric-indent-local-mode)\
+  _eq_ %s(akirak/minor-mode-hydra-status 'elecric-quote-local-mode)
+[other display]
+  _p_ %s(akirak/minor-mode-hydra-status 'page-break-lines-mode)
 "
   ("d" toggle-debug-on-error)
   ("l" lispy-mode)
