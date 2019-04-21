9c538f95494f28a15fc661c3aed68a144ec43411
Akira Komamura <akira.komamura@gmail.com>
2019-04-21 20:08:52 +0900

    Add basic config for exwm

--- setup/setup-perfect-margin.el
+++ setup/setup-perfect-margin.el
@@ -1,6 +1,13 @@
 (use-package perfect-margin
   :straight (perfect-margin :host github :repo "mpwang/perfect-margin")
   :config
+  (defun akirak/exwm-window-p (window)
+    ;; It seems that a frame is sometimes passed to this function,
+    ;; so I have to guard it
+    (and (windowp window)
+         (eq 'exwm-mode
+             (buffer-local-value 'major-mode (window-buffer window)))))
+  (add-to-list 'perfect-margin-ignore-filters 'akirak/exwm-window-p)
   (perfect-margin-mode 1)
   :custom
   (perfect-margin-visible-width 92)
