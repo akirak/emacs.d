06bf90c43cefc1f5a69149559b748dfd3600ef5b
Akira Komamura <akira.komamura@gmail.com>
2019-04-25 20:02:02 +0900

    [perfect-margin] Override the function for window splitting
    After adding perfect-margin, split-window-sensibly didn't work like
    before. I seem to have solved this issue by overriding
    window-splittable-p function, which is used to determine
    the window splitting behaviour.

--- setup/setup-perfect-margin.el
+++ setup/setup-perfect-margin.el
@@ -15,4 +15,27 @@
                                    "^ \\*LV\\*"
                                    "^ \\*which-key\\*")))
 
+(defun akirak/perfect-margin-window-splittable-p (window &optional horizontal)
+  (when (and (window-live-p window)
+             (not (window-parameter window 'window-side)))
+    (with-current-buffer (window-buffer window)
+      (if horizontal
+	  (and (memq window-size-fixed '(nil height))
+	       (numberp split-width-threshold)
+               ;; This part has been changed from the original version.
+	       (>= (if (bound-and-true-p perfect-margin-mode)
+                       (perfect-margin--width-with-margins window)
+                     (window-width window))
+		   (max split-width-threshold
+			(* 2 (max window-min-width 2)))))
+	(and (memq window-size-fixed '(nil width))
+	     (numberp split-height-threshold)
+	     (>= (window-height window)
+		 (max split-height-threshold
+		      (* 2 (max window-min-height
+				(if mode-line-format 2 1))))))))))
+
+(advice-add #'window-splittable-p :override
+            #'akirak/perfect-margin-window-splittable-p)
+
 (provide 'setup-perfect-margin)
