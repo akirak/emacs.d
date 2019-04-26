cfbb84d733b52ecd5fbc3b5a000967a4f25f57d8
Akira Komamura <akira.komamura@gmail.com>
2019-04-27 06:14:06 +0900

    [perfect-margin] Use perfect-margin-ignore-modes

--- setup/setup-perfect-margin.el
+++ setup/setup-perfect-margin.el
@@ -1,16 +1,12 @@
 (use-package perfect-margin
   :straight (perfect-margin :host github :repo "mpwang/perfect-margin")
   :config
-  (defun akirak/exwm-window-p (window)
-    ;; It seems that a frame is sometimes passed to this function,
-    ;; so I have to guard it
-    (and (windowp window)
-         (eq 'exwm-mode
-             (buffer-local-value 'major-mode (window-buffer window)))))
-  (add-to-list 'perfect-margin-ignore-filters 'akirak/exwm-window-p)
   (perfect-margin-mode 1)
   :custom
   (perfect-margin-visible-width 92)
+  (perfect-margin-ignore-modes '(exwm-mode
+                                 doc-view-mode
+                                 nov-mode))
   (perfect-margin-ignore-regexps `("^minibuf" "^[*]"
                                    "^ \\*LV\\*"
                                    "^ \\*which-key\\*")))
