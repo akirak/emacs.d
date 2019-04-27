73fd57dff02fd0eb635efd1a3057b7af80d35710
Akira Komamura <akira.komamura@gmail.com>
2019-04-27 14:23:33 +0900

    Meta: Add project-wide settings for whitespace-cleanup-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,9 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-status-mode
-  (magit-todos-depth . 1)))
+((nil
+  (whitespace-cleanup-mode-only-if-initially-clean))
+ (magit-status-mode
+  (magit-todos-depth . 1))
+ (prog-mode
+  (whitespace-cleanup-mode-enabled . t)))
