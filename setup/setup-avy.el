caa8f75fb67530ac862fd9437b8f886ce382d960
Akira Komamura <akira.komamura@gmail.com>
2019-05-14 08:34:09 +0900

    [avy] Add kill-line action to avy-dispatch-alist

--- setup/setup-avy.el
+++ setup/setup-avy.el
@@ -4,6 +4,10 @@
            "w" #'avy-goto-char-2
            "l" #'avy-goto-line
            "o" #'avy-org-goto-heading-timer)
+  :config
+  (add-to-list 'avy-dispatch-alist
+               `(?K . akirak/avy-action-kill-line)
+               t)
   :config/el-patch
   (el-patch-defun avy-forward-item ()
     (el-patch-swap (if (eq avy-command 'avy-goto-line)
@@ -23,6 +27,15 @@
                       (avy-goto-char-timer . at)
                       (akirak/avy-goto-symbol-in-window . pre)))
   (avy-keys (string-to-list "asdfghjkl")))
+
+(defun akirak/avy-action-kill-line (pt)
+  (goto-char pt)
+  (cond
+   ((bound-and-true-p smartparens-mode)
+    (sp-kill-hybrid-sexp nil))
+   (t
+    (kill-line)))
+  (message "Killed: %s" (current-kill 0)))
 
 (use-package akirak/avy-extra
   :straight nil
