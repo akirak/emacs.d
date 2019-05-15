1dc5591aa32e725892ff4a337e65744034e71f4c
Akira Komamura <akira.komamura@gmail.com>
2019-05-15 13:44:16 +0900

    [yasnippet] Reconfigure snippet directories

--- setup/setup-yasnippet.el
+++ setup/setup-yasnippet.el
@@ -1,16 +1,12 @@
-(use-package yasnippet-snippets
-  :straight (yasnippet-snippets :host github :repo "akirak/yasnippet-snippets"
-                                :files ("*.el" "snippets" ".nosearch"))
-  :config
-  ;; This should be set before loading yasnippet package
-  (setq yas-snippet-dirs `(,(expand-file-name "straight/repos/yasnippet-snippets/snippets"
-                                              user-emacs-directory))))
-
 (use-package yasnippet
-  :after (yasnippet-snippets)
   ;; :diminish 'yas-minor-mode
   :config
   (yas-global-mode 1))
+
+(use-package yasnippet-snippets
+  :after yasnippet
+  :load-path "contrib/yasnippet-snippets"
+  :straight (yasnippet-snippets :type built-in))
 
 (use-package auto-yasnippet
   :commands (aya-create aya-expand))
