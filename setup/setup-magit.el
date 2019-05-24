aa670c9f1e1cc7c0074dbb5523bb1cd285f990ce
Akira Komamura <akira.komamura@gmail.com>
2019-05-25 04:50:48 +0900

    [magit] Remap custom keybindings for magit

--- setup/setup-magit.el
+++ setup/setup-magit.el
@@ -64,8 +64,10 @@ command was called, go to its unstaged changes section."
   (when (fboundp 'unpackaged/magit-log-date-headers-mode)
     (unpackaged/magit-log-date-headers-mode 1))
   :general
-  ("C-x v w" #'magit-worktree
-   "C-x M-w" #'magit-worktree-status)
+  ;; C-c M-g is bound to magit-file-dispatch by default.
+  ;; I will bind C-c M-KEY to some other frequently used magit commands.
+  ("C-S-g" #'magit-dispatch
+   "<S-f7>" #'magit-branch-checkout)
   :custom
   (magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
   ;; Automatically save file buffers in the repository
