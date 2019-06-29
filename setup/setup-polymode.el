7c7f283ba2c2e75b3f41b743f9fcc6e42adbe667
Akira Komamura <akira.komamura@gmail.com>
2019-06-29 12:20:34 +0900

    Add polymode

---
+++ setup/setup-polymode.el
@@ -0,0 +1,14 @@
+(use-package polymode)
+
+(use-package poly-markdown)
+
+;; poly-org is a questionable package, because it seems to break
+;; indentation in existing files.
+(use-package poly-org
+  ;; Disable poly-org
+  :disabled t)
+
+(use-package poly-vue
+  :straight (poly-vue :host github :repo "akirak/poly-vue"))
+
+(provide 'setup-polymode)
