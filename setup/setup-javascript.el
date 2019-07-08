3797fef11c157e9205f36f51f5c0a6b158c88baf
Akira Komamura <akira.komamura@gmail.com>
2019-07-08 12:35:01 +0900

    Meta: Merge setup-typescript with setup-javascript

--- setup/setup-javascript.el
+++ setup/setup-javascript.el
@@ -9,4 +9,22 @@
 (use-package typescript-mode
   :mode "\\.ts\\'")
 
+(use-package tide
+  :disabled t
+  :config
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode 1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode 1)
+    (tide-hl-identifier-mode 1)
+    (add-hook 'before-save-hook 'tide-format-before-save t t)
+    ;; company is an optional dependency. You have to
+    ;; install it separately via package-install
+    ;; `M-x package-install [ret] company`
+    (company-mode 1))
+  :hook
+  (typescript-mode . (setup-tide-mode)))
+
 (provide 'setup-javascript)
