dab8f80b47792be59c9c6bf98d27c6caa9945d1e
Akira Komamura <akira.komamura@gmail.com>
2019-07-13 11:50:38 +0900

    Use gcmh for managing gc-cons-threshold

--- init.el
+++ init.el
@@ -2,30 +2,10 @@
 (when (version< emacs-version "25.1")
   (error "Use GNU Emacs version 25.1 or later"))
 
-;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
-;; http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
-;; https://github.com/kaushalmodi/.emacs.d/blob/master/init.el
-(defconst modi/gc-cons-threshold--orig gc-cons-threshold)
-
-(defsubst akirak/expand-gc-threshold ()
-  (setq gc-cons-threshold most-positive-fixnum
-        ;; The previous value for gc-cons-threshold.
-        ;; (* 100 1024 1024)
-        ))
-
-(defsubst akirak/restore-original-gc-threshold ()
-  ;; I am not sure if garbage collection should be run on minibuffer
-  ;; exit events. If I remove this line, I will change the
-  ;; `after-init-hook' to include garbage collection.
-  (garbage-collect)
-  (setq gc-cons-threshold modi/gc-cons-threshold--orig))
-
-;; GC hack for startup
-(akirak/expand-gc-threshold)
-
-;; GC hack for minibuffers
-(add-hook 'minibuffer-setup-hook #'akirak/expand-gc-threshold)
-(add-hook 'minibuffer-exit-hook #'akirak/restore-original-gc-threshold)
+;; Expand the GC threshold until gcmh-mode is activated.
+;; gcmh-mode updates this value later, so you don't have to reset it.
+;; The value is stolen from http://akrl.sdf.org/
+(setq gc-cons-threshold #x40000000)
 
 (unless (fboundp 'whitespace-cleanup-mode)
   (defun whitespace-cleanup-mode (&rest args)
@@ -62,7 +42,3 @@
 (setq-default enable-local-variables :all)
 
 (org-babel-load-file (expand-file-name "main.org" user-emacs-directory))
-
-(run-with-idle-timer 3 nil #'akirak/restore-original-gc-threshold)
-
-(run-with-idle-timer 0 1.5 #'garbage-collect)
---
+++ setup/setup-gc.el
@@ -0,0 +1,7 @@
+(use-package gcmh
+  :config
+  (gcmh-mode 1)
+  :custom
+  (gcmh-idle-delay 3))
+
+(provide 'setup-gc)
