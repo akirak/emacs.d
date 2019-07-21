de3a0590b6ba92f36b71b961489c05b6b3c88dcf
Akira Komamura <akira.komamura@gmail.com>
2019-07-22 08:36:15 +0900

    [org-web-tools] Tidy

--- setup/setup-org-web-tools.el
+++ setup/setup-org-web-tools.el
@@ -11,17 +11,17 @@ If URL is not given, look for first URL in `kill-ring'."
   (cond
    ;; Just show the repository path (USER/REPO) if the URL is a GitHub repository
    ((and (boundp 'akirak/github-https-url-regexp)
-	 (string-match akirak/github-https-url-regexp url))
+         (string-match akirak/github-https-url-regexp url))
     (let ((repo-path (match-string 1 url)))
       (org-make-link-string (concat "https://github.com/" repo-path) repo-path)))
    (t
     (let* ((html (org-web-tools--get-url url))
-	   (title (org-web-tools--html-title html))
-	   (link (org-make-link-string url title)))
+           (title (org-web-tools--html-title html))
+           (link (org-make-link-string url title)))
       link))))
 
 (advice-add 'org-web-tools--org-link-for-url
-	    :override #'akirak/org-web-tools--org-link-for-url)
+            :override #'akirak/org-web-tools--org-link-for-url)
 
 ;;;; Overriding the URL picking mechanism
 
@@ -33,6 +33,6 @@ If URL is not given, look for first URL in `kill-ring'."
   (clipurl-complete-url "Select a URL to insert: "))
 
 (advice-add 'org-web-tools--get-first-url
-	    :override 'akirak/get-recent-url)
+            :override 'akirak/get-recent-url)
 
 (provide 'setup-org-web-tools)
