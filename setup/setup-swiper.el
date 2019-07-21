6fc56b842cb28255446c264716aa7c4a4c94adcb
Akira Komamura <akira.komamura@gmail.com>
2019-07-21 11:50:39 +0900

    [swiper] Update on ivy 0.12

--- setup/setup-swiper.el
+++ setup/setup-swiper.el
@@ -2,13 +2,15 @@
   :config
   (add-to-list 'swiper-font-lock-exclude 'emacs-lisp-mode)
   (add-to-list 'swiper-font-lock-exclude 'org-mode)
-  (when (and (not (fboundp 'swiper-isearch))
+  (when (and (not (fboundp 'swiper-isearch-backward))
              (yes-or-no-p "swiper-isearch is unavailable. Would you want to upgrade swiper? "))
     (straight-pull-package "swiper")
     (straight-rebuild-package "swiper")
     (load "swiper"))
   :general
   ("C-s" #'swiper
-   "C-M-s" #'swiper-isearch))
+   "C-r" #'swiper-backward
+   "C-M-s" #'swiper-isearch
+   "C-M-r" #'swiper-isearch-backward))
 
 (provide 'setup-swiper)
