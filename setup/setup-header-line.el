d1355a2c90e3f428d63db79dcf9fe0cf05145c92
Akira Komamura <akira.komamura@gmail.com>
2019-08-02 21:46:46 +0900

    [header-line] Rewrite

--- setup/setup-header-line.el
+++ setup/setup-header-line.el
@@ -1,3 +1,4 @@
+;;;; Faces
 (defface akirak/header-line-buffer-name
   '()
   "Face for the buffer name segment in a header line.")
@@ -17,6 +18,52 @@
   '((default :inherit font-lock-function-name-face))
   "Face for the function name or header in the header line.")
 
+;;;; Setting the header line
+(defun akirak/set-header-line ()
+  (let* ((modes (let ((mode major-mode)
+                      modes)
+                  (catch 'ok
+                    (while mode
+                      (push mode modes)
+                      (setq mode (get mode 'derived-mode-parent))))
+                  modes))
+         (fmt (cond
+               ((memq 'prog-mode modes)
+                (akirak/make-header-line-format))
+               ((memq 'org-mode modes)
+                (akirak/make-header-line-format
+                 '(:eval
+                   (akirak/header-line-org-outline-path))))
+               ((memq 'org-agenda-mode modes)
+                '((:eval (and (featurep 'all-the-icons)
+                              (all-the-icons-icon-for-buffer)))
+                  " "
+                  (:eval (pcase org-agenda-redo-command
+                           (`(org-agenda-run-series ,desc . ,_)
+                            (when-let ((key (caar (cl-remove-if-not
+                                                   (lambda (list) (equal (nth 1 list) desc))
+                                                   org-agenda-custom-commands))))
+                              (format "[%s]%s" key (car (split-string desc ":")))))
+                           (x (prin1-to-string x))))))
+               ((memq 'dired-mode modes)
+                (progn
+                  (setq dired-filter-header-line-format
+                        `("  "
+                          ,(if (featurep 'all-the-icons)
+                               (all-the-icons-icon-for-buffer)
+                             'mode-name)
+                          " "
+                          dired-directory
+                          " "
+                          (:eval (dired-filter--describe-filters))))
+                  nil)))))
+    (when fmt
+      (setq header-line-format fmt))))
+
+(add-hook 'after-change-major-mode-hook 'akirak/set-header-line)
+(add-hook 'clone-indirect-buffer-hook 'akirak/set-header-line)
+
+;;;; Default header line format
 (defun akirak/make-header-line-format (&rest body)
   "Build a header line format with the standard set of segments."
   (let* ((filep (when buffer-file-name t))
@@ -68,52 +115,7 @@
       ;; Append any segments
       ,@body)))
 
-;;;; Default header line with which-function
-
-;; The default header line format with which-function.
-(which-function-mode 1)
-
-(defun akirak/set-default-header-line ()
-  "Set the default header line with which-function."
-  (unless header-line-format
-    (setq header-line-format (akirak/make-header-line-format
-                              ;; Omit which-func if the buffer is indirect
-                              ;; (when (and (not (buffer-base-buffer))
-                              ;;            (derived-mode-p 'prog-mode 'text-mode))
-                              ;;   `(which-function-mode
-                              ;;     ,which-func-current))
-                              ))))
-
-;;;;; Setting the default header line
-
-;; As I don't know how to disable the header line in the which-key window
-;; (I tried `which-key-init-buffer-hook', but it didn't work),
-;; I turn on the default header line only in certain groups of buffers
-;; rather than setting it using `setq-default'.
-
-;; Display a header line whenever you visit a file
-(add-hook 'find-file-hook #'akirak/set-default-header-line)
-
-;; Display a header line in prog-mode even if you are not visiting
-;; a file, e.g. in a scratch buffer
-(add-hook 'prog-mode-hook #'akirak/set-default-header-line)
-
-(add-hook 'sgml-mode-hook #'akirak/set-default-header-line)
-
-;; Magit buffers should display their names
-(add-hook 'magit-mode-hook #'akirak/set-default-header-line)
-
-;; Help buffers should display their names
-(add-hook 'help-mode-hook #'akirak/set-default-header-line)
-(add-hook 'helpful-mode-hook #'akirak/set-default-header-line)
-
-;; Interactive shells
-(add-hook 'eshell-mode-hook #'akirak/set-default-header-line)
-(add-hook 'term-mode-hook #'akirak/set-default-header-line)
-(add-hook 'comint-mode-hook #'akirak/set-default-header-line)
-
-;;;; Header line formats for specific modes
-;;;;; org-mode
+;;;; org-mode
 ;; Set the header line format for org-mode with the outline path.
 (defvar akirak/header-line-org-outline-path-root-level nil)
 (make-variable-buffer-local 'akirak/header-line-org-outline-path-root-level)
@@ -147,39 +149,5 @@
                               (substring s 0 seg-length)
                             s))
                         (cdr orig-rev)))))))))
-
-(require 'mode-local)
-(setq-mode-local org-mode
-                 header-line-format
-                 (akirak/make-header-line-format
-                  '(:eval
-                    (akirak/header-line-org-outline-path))))
-
-;;;;; org-agenda-mode
-(defun akirak/set-org-agenda-header-line ()
-  (setq header-line-format
-        '((:eval (and (featurep 'all-the-icons)
-                      (all-the-icons-icon-for-buffer)))
-          " "
-          (:eval (pcase org-agenda-redo-command
-                   (`(org-agenda-run-series ,desc . ,_)
-                    (when-let ((key (caar (cl-remove-if-not
-                                           (lambda (list) (equal (nth 1 list) desc))
-                                           org-agenda-custom-commands))))
-                      (format "[%s]%s" key (car (split-string desc ":")))))
-                   (x (prin1-to-string x)))))))
-
-(add-hook 'org-agenda-mode-hook #'akirak/set-org-agenda-header-line)
-
-;;;;; dired with dired-filter
-(setq dired-filter-header-line-format
-      `("  "
-        (:eval (or (and (featurep 'all-the-icons)
-                        (all-the-icons-icon-for-buffer))
-                   mode-name))
-        " "
-        dired-directory
-        " "
-        (:eval (dired-filter--describe-filters))))
 
 (provide 'setup-header-line)
