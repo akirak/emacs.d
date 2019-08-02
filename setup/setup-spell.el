43bb9757088f62a6361067660d9a6ddced416d63
Akira Komamura <akira.komamura@gmail.com>
2019-08-02 21:45:48 +0900

    [spell] Reconfigure hooks for enabling flyspell

--- setup/setup-spell.el
+++ setup/setup-spell.el
@@ -1,9 +1,13 @@
 (use-package flyspell
   :when (executable-find "ispell")
+  :config
+  (defun akirak/turn-on-flyspell-text-mode ()
+    (flyspell-mode 1))
+  (defun akirak/turn-on-flyspell-prog-mode-conditionally ()
+    (flyspell-prog-mode))
   :hook
-  ((text-mode . (lambda () (flyspell-mode 1)))
-   ((change-log-mode log-edit-mode) . (lambda () (flyspell-mode -1)))
-   (prog-mode . flyspell-prog-mode)))
+  ((text-mode . akirak/turn-on-flyspell-text-mode)
+   (prog-mode . akirak/turn-on-flyspell-prog-mode-conditionally)))
 
 (general-unbind :keymaps 'flyspell-mode-map :package 'flyspell
   "C-," "C-." "C-M-i" "C-c $" "C-;")
