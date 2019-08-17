a511695f05d77a3edaab1eea7d935f71b7f9113d
Akira Komamura <akira.komamura@gmail.com>
2019-08-18 02:02:13 +0900

    [ivy] Configure ivy-posframe

--- setup/setup-ivy.el
+++ setup/setup-ivy.el
@@ -83,4 +83,50 @@
 
 (general-def "C-x b" #'ivy-switch-buffer-2)
 
+(use-package ivy-posframe
+  ;; Use posframe to display candidates in ivy commands.
+  ;;
+  ;; 1. The default display function is ivy-posframe-display-at-frame-center.
+  ;;    However, if there is an EXWM window in the frame, use
+  ;;    ivy-posframe-display-at-window-center instead.
+  ;; 2. For swiper commands, display the posframe at the window bottom.
+  ;; 3. There may be specific situations where I prefer other display functions.
+  ;;
+  ;; If the current focus is on an EXWM window, ivy-posframe is never used.
+  ;; Instead, the default display function is used. This is configured in setup-exwm.el.
+  :config
+  (ivy-posframe-mode 1)
+  (defun akirak/ivy-posframe-window-bottom-left-size ()
+    (list
+     :height ivy-posframe-height
+     :width (window-body-width)
+     ;; :min-height (or ivy-posframe-min-height (+ ivy-height 1))
+     ;; :min-width (or ivy-posframe-min-width (round (* (frame-width) 0.62)))
+     ))
+  (defun akirak/ad-around-ivy-posframe-display-at-frame-center (orig str)
+    (if (akirak/frame-contains-exwm-window-p)
+        (ivy-posframe-display-at-window-center str)
+      (funcall orig str)))
+  (advice-add 'ivy-posframe-display-at-frame-center
+              :around 'akirak/ad-around-ivy-posframe-display-at-frame-center)
+  :config/el-patch
+  (el-patch-defun ivy-posframe-display-at-window-bottom-left (str)
+    (el-patch-wrap 1
+      (let ((ivy-posframe-size-function #'akirak/ivy-posframe-window-bottom-left-size))
+        (ivy-posframe--display str #'posframe-poshandler-window-bottom-left-corner))))
+  :custom
+  (ivy-posframe-height 12)
+  (ivy-posframe-width 100)
+  (org-starter-swiper-width-function (lambda () (- (window-body-width) 5)))
+  (ivy-posframe-display-functions-alist
+   `(,@(--map (cons it #'ivy-posframe-display-at-window-bottom-left)
+              '(swiper swiper-all swiper-multi org-starter-swiper-config-files))
+     (counsel-minibuffer-history . nil)
+     (counsel-yank-pop . ivy-posframe-display-at-point)
+     (t . ivy-posframe-display-at-frame-center))))
+
+(defun akirak/frame-contains-exwm-window-p (&optional frame)
+  (--any (eq 'exwm-mode (buffer-local-value 'major-mode (window-buffer it)))
+         (window-list frame)))
+
 (provide 'setup-ivy)
