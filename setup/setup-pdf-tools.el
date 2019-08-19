ff0ca2ecea286685a3bc48e4daf4171487028302
Akira Komamura <akira.komamura@gmail.com>
2019-08-20 00:27:17 +0900

    [pdf-tools] Add all missing autoloads needed for using org-noter

--- setup/setup-pdf-tools.el
+++ setup/setup-pdf-tools.el
@@ -1,7 +1,17 @@
 (use-package pdf-tools
   :straight nil
   :preface
+  (autoload 'pdf-annot-minor-mode "pdf-annot")
+  (autoload 'pdf-cache-minor-mode "pdf-cache")
+  (autoload 'pdf-dev-minor-mode "pdf-dev")
+  (autoload 'pdf-history-minor-mode "pdf-history")
+  (autoload 'pdf-isearch-minor-mode "pdf-isearch")
+  (autoload 'pdf-links-minor-mode "pdf-links")
+  (autoload 'pdf-misc-minor-mode "pdf-misc")
   (autoload 'pdf-occur-global-minor-mode "pdf-occur")
+  (autoload 'pdf-occur-ibuffer-minor-mode "pdf-occur")
+  (autoload 'pdf-outline-minor-mode "pdf-outline")
+  (autoload 'pdf-sync-minor-mode "pdf-sync")
   :config
   (pdf-tools-install))
 
