58993568f2d37762e795b7e5b2a237d34a84fb57
Akira Komamura <akira.komamura@gmail.com>
2019-08-19 22:13:42 +0900

    [apheleia] Add sgml-mode
    This is effective in Vue single component files.

--- setup/setup-apheleia.el
+++ setup/setup-apheleia.el
@@ -1,6 +1,7 @@
 (use-package apheleia
   :straight (apheleia :host github :repo "raxod502/apheleia")
   :config
+  (add-to-list 'apheleia-mode-alist '(sgml-mode . prettier))
   (apheleia-global-mode 1))
 
 (provide 'setup-apheleia)
