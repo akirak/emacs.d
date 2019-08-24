550a5ec04c21262a840d4ad8150e5a6ff9258c97
Akira Komamura <akira.komamura@gmail.com>
2019-08-24 23:13:08 +0900

    [pass] Prevent loading auth-source-pass if the dir does not exist

--- setup/setup-pass.el
+++ setup/setup-pass.el
@@ -1,3 +1,9 @@
+(defconst akirak/password-store-directory
+  (let ((dir (or (getenv "password_store_dir")
+                 "~/.password-store")))
+    (when (file-directory-p dir)
+      dir)))
+
 (use-package pass
   :commands (pass))
 
@@ -8,12 +14,14 @@
         (or (executable-find "gopass")
             ;; fallback to pass
             (executable-find "pass")))
-  :if (or password-store-executable
-          (progn
-            (message "gopass or pass is not installed, so password-store won't be available"))
-          nil))
+  :if (and (or password-store-executable
+               (progn
+                 (message "gopass or pass is not installed, so password-store won't be available"))
+               nil)))
 
 (use-package auth-source-pass
+  :if (and akirak/password-store-directory
+           (file-directory-p akirak/password-store-directory))
   :config
   (auth-source-pass-enable))
 
