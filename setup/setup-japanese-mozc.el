9a91032cf5337c521d2671cd21d89a370132d747
Akira Komamura <akira.komamura@gmail.com>
2019-08-27 19:07:37 +0900

    [mozc] Use mozc installed using Nix

--- setup/setup-japanese-mozc.el
+++ setup/setup-japanese-mozc.el
@@ -1,27 +1,9 @@
-;; You'll need mozc_emacs_helper executable to use mozc.el.
-;; If you are using Debian or its variant, it is available as
-;; emacs-mozc-bin package. I may add it to nixpkgs in the future.
-
-(add-to-list 'load-path (expand-file-name "contrib/mozc"
-                                          user-emacs-directory))
-
+;; Install mozc_emacs_helper as well as mozc.el using Nix
 (straight-use-package '(mozc :type built-in))
 
-;; mozc-mode lets you write Japanese
-(use-package mozc
-  ;; If you want to disable this package, set :disabled to t.
-  :if (or (executable-find "mozc_emacs_helper")
-          (cond
-           ((akirak/os-like-debian-p)
-            (let ((default-directory "/sudo::/"))
-              (= 0 (call-process "apt" nil "*apt install emacs-mozc-bin" t
-                                 "install" "--yes" "emacs-mozc-bin")))))
-          (progn
-            (message "Disabled mozc as mozc_emacs_helper is not installed")
-            nil)))
+(use-package mozc)
 
 (use-package mozc-cand-posframe
-  :straight (mozc-cand-posframe :host github :repo "akirak/mozc-posframe")
   :after mozc
   :if (posframe-workable-p)
   :config
