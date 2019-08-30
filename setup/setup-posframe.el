8496cb3a5152b7328f54089e8710a274089bc313
Akira Komamura <akira.komamura@gmail.com>
2019-08-31 02:01:13 +0900

    [posframe] Display at the bottom left if the window is too narrow

--- setup/setup-posframe.el
+++ setup/setup-posframe.el
@@ -4,8 +4,13 @@
     (--any (eq 'exwm-mode (buffer-local-value 'major-mode (window-buffer it)))
            (window-list frame)))
   (defun akirak/posframe-poshandler-smart-center (info)
-    (if (akirak/frame-contains-exwm-window-p)
-        (posframe-poshandler-window-center info)
-      (posframe-poshandler-frame-center info))))
+    (cond
+     ((< (window-width) (or (plist-get info :posframe-width)
+                           90))
+      (posframe-poshandler-window-bottom-left-corner info))
+    ((akirak/frame-contains-exwm-window-p)
+     (posframe-poshandler-window-center info))
+    (t
+     (posframe-poshandler-frame-center info)))))
 
 (provide 'setup-posframe)
