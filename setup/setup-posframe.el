1afb0f731ba0a962c3c8a16c4712ccf50451b691
Akira Komamura <akira.komamura@gmail.com>
2019-08-31 19:43:42 +0900

    [posframe] Make the poshandler even more flexible

--- setup/setup-posframe.el
+++ setup/setup-posframe.el
@@ -4,13 +4,20 @@
     (--any (eq 'exwm-mode (buffer-local-value 'major-mode (window-buffer it)))
            (window-list frame)))
   (defun akirak/posframe-poshandler-smart-center (info)
-    (cond
-     ((< (window-width) (or (plist-get info :posframe-width)
-                           90))
-      (posframe-poshandler-window-bottom-left-corner info))
-    ((akirak/frame-contains-exwm-window-p)
-     (posframe-poshandler-window-center info))
-    (t
-     (posframe-poshandler-frame-center info)))))
+    (let ((posframe-width (if-let ((pixel-width (plist-get info :posframe-width)))
+                              (/ pixel-width (frame-char-width))
+                            90))
+          (window-width (window-width))
+          (frame-width (frame-width))
+          (has-exwm-window (akirak/frame-contains-exwm-window-p)))
+      (cond
+       ((and has-exwm-window (< window-width posframe-width))
+        (posframe-poshandler-window-bottom-left-corner info))
+       (has-exwm-window
+        (posframe-poshandler-window-center info))
+       ((< frame-width posframe-width)
+        (posframe-poshandler-frame-bottom-left-corner info))
+       (t
+        (posframe-poshandler-frame-center info))))))
 
 (provide 'setup-posframe)
