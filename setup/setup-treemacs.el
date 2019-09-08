9dccda5c24174192664a5a1bbf6e014c78a8252c
Akira Komamura <akira.komamura@gmail.com>
2019-09-08 18:40:49 +0900

    [treemacs] Find the path to python executable

--- setup/setup-treemacs.el
+++ setup/setup-treemacs.el
@@ -1,14 +1,16 @@
 (use-package treemacs
+  :preface
+  (setq treemacs-python-executable (executable-find "python3"))
+  (pcase (cons (not (null (executable-find "git")))
+               (not (null treemacs-python-executable)))
+    (`(t . t)
+     (setq treemacs-git-mode 'deferred))
+    (`(t . _)
+     (setq treemacs-git-mode 'simple)))
   :config
   (treemacs-follow-mode 1)
   (treemacs-filewatch-mode 1)
-  (treemacs-fringe-indicator-mode 1)
-  (pcase (cons (not (null (executable-find "git")))
-               (not (null (executable-find "python3"))))
-    (`(t . t)
-     (treemacs-git-mode 'deferred))
-    (`(t . _)
-     (treemacs-git-mode 'simple))))
+  (treemacs-fringe-indicator-mode 1))
 
 (use-package treemacs-icons-dired
   :after (treemacs dired)
