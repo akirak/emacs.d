a6270cca31c8bd5c014843885776352b906f3302
Akira Komamura <akira.komamura@gmail.com>
2019-09-08 18:55:10 +0900

    [window-management] Keep the windows while agenda commands are displayed

--- setup/setup-window-management.el
+++ setup/setup-window-management.el
@@ -31,7 +31,7 @@
                    ("*Messages*" :align below :ratio 0.3 :noselect t)
                    ("*Warnings*" :align below :ratio 0.3 :noselect t)
                    ("*Backtrace*" :align below :ratio 0.4 :noselect t)
-                   ("\\*Agenda Commands\\*" :regexp t :align below :ratio 0.4)
+                   (" *Agenda Commands*" :align below :ratio 0.3)
                    ("*Calendar*" :align below :ratio 0.3)
                    ("*Org Links*" :ratio 0.1 :align below)
                    ;; ("*undo tree*" :size 0.2 :align right)
@@ -136,6 +136,198 @@ may have been stored before."
                initial-input hist def inherit-input-method
                args)
       (read-string prompt initial-input hist def inherit-input-method))))
+
+;; This is a big function, but it hasn't been modified for several years.
+;; I think it is relatively safe to patch the function.
+(el-patch-defun org-agenda-get-restriction-and-command (prefix-descriptions)
+  "The user interface for selecting an agenda command."
+  (catch 'exit
+    (let* ((bfn (buffer-file-name (buffer-base-buffer)))
+           (restrict-ok (and bfn (derived-mode-p 'org-mode)))
+           (region-p (org-region-active-p))
+           (custom org-agenda-custom-commands)
+           (selstring "")
+           restriction second-time
+           c entry key type match prefixes rmheader header-end custom1 desc
+           line lines left right n n1)
+      (save-window-excursion
+        (el-patch-remove (delete-other-windows))
+        (org-switch-to-buffer-other-window " *Agenda Commands*")
+        (erase-buffer)
+        (insert (eval-when-compile
+                  (let ((header
+                         "Press key for an agenda command:
+--------------------------------        <   Buffer, subtree/region restriction
+a   Agenda for current week or day      >   Remove restriction
+t   List of all TODO entries            e   Export agenda views
+m   Match a TAGS/PROP/TODO query        T   Entries with special TODO kwd
+s   Search for keywords                 M   Like m, but only TODO entries
+/   Multi-occur                         S   Like s, but only TODO entries
+?   Find :FLAGGED: entries              C   Configure custom agenda commands
+*   Toggle sticky agenda views          #   List stuck projects (!=configure)
+")
+                        (start 0))
+                    (while (string-match
+                            "\\(^\\|   \\|(\\)\\(\\S-\\)\\( \\|=\\)"
+                            header start)
+                      (setq start (match-end 0))
+                      (add-text-properties (match-beginning 2) (match-end 2)
+                                           '(face bold) header))
+                    header)))
+        (setq header-end (point-marker))
+        (while t
+          (setq custom1 custom)
+          (when (eq rmheader t)
+            (org-goto-line 1)
+            (re-search-forward ":" nil t)
+            (delete-region (match-end 0) (point-at-eol))
+            (forward-char 1)
+            (looking-at "-+")
+            (delete-region (match-end 0) (point-at-eol))
+            (move-marker header-end (match-end 0)))
+          (goto-char header-end)
+          (delete-region (point) (point-max))
+
+          ;; Produce all the lines that describe custom commands and prefixes
+          (setq lines nil)
+          (while (setq entry (pop custom1))
+            (setq key (car entry) desc (nth 1 entry)
+                  type (nth 2 entry)
+                  match (nth 3 entry))
+            (if (> (length key) 1)
+                (cl-pushnew (string-to-char key) prefixes :test #'equal)
+              (setq line
+                    (format
+                     "%-4s%-14s"
+                     (org-add-props (copy-sequence key)
+                         '(face bold))
+                     (cond
+                      ((string-match "\\S-" desc) desc)
+                      ((eq type 'agenda) "Agenda for current week or day")
+                      ((eq type 'agenda*) "Appointments for current week or day")
+                      ((eq type 'alltodo) "List of all TODO entries")
+                      ((eq type 'search) "Word search")
+                      ((eq type 'stuck) "List of stuck projects")
+                      ((eq type 'todo) "TODO keyword")
+                      ((eq type 'tags) "Tags query")
+                      ((eq type 'tags-todo) "Tags (TODO)")
+                      ((eq type 'tags-tree) "Tags tree")
+                      ((eq type 'todo-tree) "TODO kwd tree")
+                      ((eq type 'occur-tree) "Occur tree")
+                      ((functionp type) (if (symbolp type)
+                                            (symbol-name type)
+                                          "Lambda expression"))
+                      (t "???"))))
+              (cond
+               ((not (org-string-nw-p match)) nil)
+               (org-agenda-menu-show-matcher
+                (setq line
+                      (concat line ": "
+                              (cond
+                               ((stringp match)
+                                (propertize match 'face 'org-warning))
+                               ((listp type)
+                                (format "set of %d commands" (length type)))))))
+               (t
+                (org-add-props line nil 'help-echo (concat "Matcher: " match))))
+              (push line lines)))
+          (setq lines (nreverse lines))
+          (when prefixes
+            (mapc (lambda (x)
+                    (push
+                     (format "%s   %s"
+                             (org-add-props (char-to-string x)
+                                 nil 'face 'bold)
+                             (or (cdr (assoc (concat selstring
+                                                     (char-to-string x))
+                                             prefix-descriptions))
+                                 "Prefix key"))
+                     lines))
+                  prefixes))
+
+          ;; Check if we should display in two columns
+          (if org-agenda-menu-two-columns
+              (progn
+                (setq n (length lines)
+                      n1 (+ (/ n 2) (mod n 2))
+                      right (nthcdr n1 lines)
+                      left (copy-sequence lines))
+                (setcdr (nthcdr (1- n1) left) nil))
+            (setq left lines right nil))
+          (while left
+            (insert "\n" (pop left))
+            (when right
+              (if (< (current-column) 40)
+                  (move-to-column 40 t)
+                (insert "   "))
+              (insert (pop right))))
+
+          ;; Make the window the right size
+          (goto-char (point-min))
+          (if second-time
+              (when (not (pos-visible-in-window-p (point-max)))
+                (org-fit-window-to-buffer))
+            (setq second-time t)
+            (org-fit-window-to-buffer))
+
+          ;; Ask for selection
+          (message "Press key for agenda command%s:"
+                   (if (or restrict-ok org-agenda-overriding-restriction)
+                       (if org-agenda-overriding-restriction
+                           " (restriction lock active)"
+                         (if restriction
+                             (format " (restricted to %s)" restriction)
+                           " (unrestricted)"))
+                     ""))
+          (setq c (read-char-exclusive))
+          (message "")
+          (cond
+           ((assoc (char-to-string c) custom)
+            (setq selstring (concat selstring (char-to-string c)))
+            (throw 'exit (cons selstring restriction)))
+           ((memq c prefixes)
+            (setq selstring (concat selstring (char-to-string c))
+                  prefixes nil
+                  rmheader (or rmheader t)
+                  custom (delq nil (mapcar
+                                    (lambda (x)
+                                      (if (or (= (length (car x)) 1)
+                                              (/= (string-to-char (car x)) c))
+                                          nil
+                                        (cons (substring (car x) 1) (cdr x))))
+                                    custom))))
+           ((eq c ?*)
+            (call-interactively 'org-toggle-sticky-agenda)
+            (sit-for 2))
+           ((and (not restrict-ok) (memq c '(?1 ?0 ?<)))
+            (message "Restriction is only possible in Org buffers")
+            (ding) (sit-for 1))
+           ((eq c ?1)
+            (org-agenda-remove-restriction-lock 'noupdate)
+            (setq restriction 'buffer))
+           ((eq c ?0)
+            (org-agenda-remove-restriction-lock 'noupdate)
+            (setq restriction (if region-p 'region 'subtree)))
+           ((eq c ?<)
+            (org-agenda-remove-restriction-lock 'noupdate)
+            (setq restriction
+                  (cond
+                   ((eq restriction 'buffer)
+                    (if region-p 'region 'subtree))
+                   ((memq restriction '(subtree region))
+                    nil)
+                   (t 'buffer))))
+           ((eq c ?>)
+            (org-agenda-remove-restriction-lock 'noupdate)
+            (setq restriction nil))
+           ((and (equal selstring "") (memq c '(?s ?S ?a ?t ?m ?L ?C ?e ?T ?M ?# ?! ?/ ??)))
+            (throw 'exit (cons (setq selstring (char-to-string c)) restriction)))
+           ((and (> (length selstring) 0) (eq c ?\d))
+            (delete-window)
+            (org-agenda-get-restriction-and-command prefix-descriptions))
+
+           ((equal c ?q) (error "Abort"))
+           (t (user-error "Invalid key %c" c))))))))
 
 ;;;; Delete compilation window
 
