7a4b26a16499d288a29c91f2d0234967ce403d3c
Akira Komamura <akira.komamura@gmail.com>
2019-09-15 18:10:23 +0900

    [hydra] Add major-mode hydras for help and info modes

--- setup/setup-hydra.el
+++ setup/setup-hydra.el
@@ -117,4 +117,40 @@ See `pretty-hydra-define' and `pretty-hydra-define+'."
 
 ;; (advice-add 'hydra-keyboard-quit :around 'akirak/ad-around-hydra-keyboard-quit)
 
+;;;; Define some (but not all) major mode hydras
+;; These major-mode hydras are mostly for intended for mnemonics.
+(major-mode-hydra-define 'Info-mode
+  (:title "Info navigation")
+  ("Navigation"
+   (("n" Info-next "next")
+    ("p" Info-previous "previous")
+    ("[" Info-backward-node "backward")
+    ("]" Info-forward-node "forward")
+    ("h" Info-up "up")
+    ("m" Info-menu "menu")
+    ("T" Info-toc "toc"))
+   "History"
+   (("l" Info-history-back "back")
+    ("r" Info-history-forward "forward")
+    ("L" Info-history "display"))
+   "In this file"
+   (("<" Info-top-node "beginning")
+    (">" Info-final-node "end"))
+   "Cross reference"
+   (("f" Info-follow-reference "Follow"))))
+
+(major-mode-hydra-define 'help-mode
+  (:title "Help navigation")
+  ("History"
+   (("l" help-go-back "back")
+    ("r" help-go-forward "forward"))
+   "Button"
+   (("f" help-follow-symbol "Follow"))))
+
+(major-mode-hydra-define 'helpful-mode
+  (:title "Helpful navigation")
+  ("Buttons"
+   (("n" forward-button "Forward")
+    ("p" backward-button "Backward"))))
+
 (provide 'setup-hydra)
