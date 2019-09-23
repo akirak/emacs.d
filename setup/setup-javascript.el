fcc9eedcd09015a81858964dd83b0b92c53b690b
Akira Komamura <akira.komamura@gmail.com>
2019-09-23 12:06:10 +0900

    [javascript] Configure tide

--- setup/setup-javascript.el
+++ setup/setup-javascript.el
@@ -15,21 +15,9 @@
   :mode "\\.ts\\'")
 
 (use-package tide
-  :disabled t
-  :config
-  (defun setup-tide-mode ()
-    (interactive)
-    (tide-setup)
-    (flycheck-mode 1)
-    (setq flycheck-check-syntax-automatically '(save mode-enabled))
-    (eldoc-mode 1)
-    (tide-hl-identifier-mode 1)
-    (add-hook 'before-save-hook 'tide-format-before-save t t)
-    ;; company is an optional dependency. You have to
-    ;; install it separately via package-install
-    ;; `M-x package-install [ret] company`
-    (company-mode 1))
   :hook
-  (typescript-mode . (setup-tide-mode)))
+  ((js-mode typescript-mode) . tide-setup)
+  (tide-mode . tide-hl-identifier-mode)
+  (tide-mode . flycheck-mode))
 
 (provide 'setup-javascript)
