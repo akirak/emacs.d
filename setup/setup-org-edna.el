0a813368f60964402d788d97089abe6079b8d93d
Akira Komamura <akira.komamura@gmail.com>
2019-09-29 00:46:35 +0900

    [org-edna] Run org-edna-load in :init

--- setup/setup-org-edna.el
+++ setup/setup-org-edna.el
@@ -1,14 +1,11 @@
 (use-package org-edna
   :after org
   :straight (org-edna :host github :repo "akirak/org-edna" :branch "edit")
-  :config
+  :init
   (defun akirak/org-edna-edit-setup-company ()
     (setq company-backends '(company-capf))
     (company-mode 1))
-  (defvar org-edna-edit-mode-hook nil)
   (org-edna-load)
-  (advice-add 'org-edna-edit
-              :after (lambda (&rest _args) (run-hooks 'org-edna-edit-mode-hook)))
   :hook
   (org-edna-edit-mode . akirak/org-edna-edit-setup-company))
 
