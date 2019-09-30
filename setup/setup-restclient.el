c4daeba6129c7518efbbe4a4c52f4270f1749019
Akira Komamura <akira.komamura@gmail.com>
2019-09-30 23:37:09 +0900

    [restclient] Use json-mode; Add hydra

--- setup/setup-restclient.el
+++ setup/setup-restclient.el
@@ -1,5 +1,16 @@
 (use-package restclient
   :mode (("\\.rest\\'" . restclient-mode)
-         ("\\.restclient\\'" . restclient-mode)))
+         ("\\.restclient\\'" . restclient-mode))
+  :custom
+  (restclient-content-type-modes nil)
+  :config
+  (remove-hook 'restclient-mode-hook 'restclient-outline-mode)
+  (setcdr (assoc "application/json" restclient-content-type-modes #'string-equal)
+          'json-mode))
+
+(major-mode-hydra-define 'restclient-mode
+  (:title "Restclient")
+  ("Toggle"
+   (("o" restclient-outline-mode :toggle t))))
 
 (provide 'setup-restclient)
