75971e643bd01167b0d75355f6fb1288dc5a8a11
Akira Komamura <akira.komamura@gmail.com>
2019-10-03 02:05:51 +0900

    Use C-c ' for editing blocks in markdown and vue

--- setup/setup-markdown.el
+++ setup/setup-markdown.el
@@ -1,6 +1,9 @@
 (use-package markdown-mode
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode))
+  :general
+  (:keymaps 'markdown-mode-map
+            "C-c '" #'markdown-edit-code-block)
   :custom
   (markdown-command "pandoc -f gfm -t html"))
 
