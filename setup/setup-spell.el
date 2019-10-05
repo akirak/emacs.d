37344d5e580000b1bfd9b70eab67a4b72681c904
Akira Komamura <akira.komamura@gmail.com>
2019-10-05 13:54:20 +0900

    [spell] Disable by default
    Flyspell often slows down Emacs.
    
    Since I don't always work on a project where English is used as the
    primary language, I will disable flyspell by default.
    
    Instead, I will add directory-local variables to ~/lib/notes/writing,
    ~/lib/notes/wiki, and other projects where I need spell checking:
    
    ((text-mode
      (mode . flyspell))
     (prog-mode
      (mode . flyspell-prog)))

--- setup/setup-spell.el
+++ setup/setup-spell.el
@@ -1,13 +1,5 @@
 (use-package flyspell
-  :when (executable-find "ispell")
-  :config
-  (defun akirak/turn-on-flyspell-text-mode ()
-    (flyspell-mode 1))
-  (defun akirak/turn-on-flyspell-prog-mode-conditionally ()
-    (flyspell-prog-mode))
-  :hook
-  ((text-mode . akirak/turn-on-flyspell-text-mode)
-   (prog-mode . akirak/turn-on-flyspell-prog-mode-conditionally)))
+  :when (executable-find "ispell"))
 
 (general-unbind :keymaps 'flyspell-mode-map :package 'flyspell
   "C-," "C-." "C-M-i" "C-c $" "C-;")
