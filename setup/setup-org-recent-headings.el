f858e07277a74cd1bc7a7376544c78cd54744a8f
Akira Komamura <akira.komamura@gmail.com>
2019-10-05 23:13:36 +0900

    Add org-recent-headings

---
+++ setup/setup-org-recent-headings.el
@@ -0,0 +1,7 @@
+(use-package org-recent-headings
+  :config
+  (org-recent-headings-mode 1)
+  :custom
+  (org-recent-headings-helm-show-clock-history 'append))
+
+(provide 'setup-org-recent-headings)
