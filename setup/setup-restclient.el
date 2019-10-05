1be9387fc2b88712d721f9b67940059aa056ab09
Akira Komamura <akira.komamura@gmail.com>
2019-10-05 23:06:18 +0900

    [restclient] Remove invalid custom variable setting
    This was added for testing and not needed any more.

--- setup/setup-restclient.el
+++ setup/setup-restclient.el
@@ -1,8 +1,6 @@
 (use-package restclient
   :mode (("\\.rest\\'" . restclient-mode)
          ("\\.restclient\\'" . restclient-mode))
-  :custom
-  (restclient-content-type-modes nil)
   :config
   (remove-hook 'restclient-mode-hook 'restclient-outline-mode)
   (setcdr (assoc "application/json" restclient-content-type-modes #'string-equal)
