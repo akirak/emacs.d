22ee55792f94ee235c1178858eccbfb690ef12e4
Akira Komamura <akira.komamura@gmail.com>
2019-10-05 12:35:38 +0900

    [github] Enable forge

--- setup/setup-github.el
+++ setup/setup-github.el
@@ -1,6 +1,4 @@
-(use-package forge
-  :disabled t
-  :straight (forge :host github :repo "magit/forge"))
+(use-package forge)
 
 (use-package github-review
   :commands (github-review-forge-pr-at-point))
