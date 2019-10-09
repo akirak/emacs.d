f05b5e71612a133137b2313dc7a95fc4faf226af
Akira Komamura <akira.komamura@gmail.com>
2019-10-10 02:27:09 +0900

    [swiper] Remove checking the existence of isearch commands

--- setup/setup-swiper.el
+++ setup/setup-swiper.el
@@ -2,11 +2,6 @@
   :config
   (add-to-list 'swiper-font-lock-exclude 'emacs-lisp-mode)
   (add-to-list 'swiper-font-lock-exclude 'org-mode)
-  (when (and (not (fboundp 'swiper-isearch-backward))
-             (yes-or-no-p "swiper-isearch is unavailable. Would you want to upgrade swiper? "))
-    (straight-pull-package "swiper")
-    (straight-rebuild-package "swiper")
-    (load "swiper"))
   :general
   ("C-s" #'swiper
    "C-r" #'swiper-backward
