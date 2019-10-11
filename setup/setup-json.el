2fab8c4022391c2b4813883d8cf9bcc0c334994e
Akira Komamura <akira.komamura@gmail.com>
2019-10-11 15:28:30 +0900

    [json] Don't always disable font-lock-mode in json-mode

--- setup/setup-json.el
+++ setup/setup-json.el
@@ -7,8 +7,6 @@
                   :map json-mode-map
                   ("C-c <tab>" . json-mode-beautify))
   :config
-  (make-local-variable 'js-indent-level)
-  :hook
-  (json-mode . (lambda () (font-lock-mode -1))))
+  (make-local-variable 'js-indent-level))
 
 (provide 'setup-json)
