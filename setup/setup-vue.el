3e5657266af2bc0b134aeeeac0483e6489e367fa
Akira Komamura <akira.komamura@gmail.com>
2019-10-23 17:36:39 +0900

    [vue] Bind vue-mode-edit-indirect-at-point in js-mode-map

--- setup/setup-vue.el
+++ setup/setup-vue.el
@@ -2,7 +2,7 @@
   ;; Turn on vue-mode manually only when necessary
   ;; :mode "\\.vue\\'"
   :general
-  (:keymaps 'vue-mode-map
+  (:keymaps 'js-mode-map
             "C-c '" #'vue-mode-edit-indirect-at-point))
 
 (provide 'setup-vue)
