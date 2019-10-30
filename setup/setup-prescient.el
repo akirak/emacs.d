9a025b49eaaca4383be198bb7a1f69ad3d46b483
Akira Komamura <akira.komamura@gmail.com>
2019-10-31 02:21:49 +0900

    [ivy-omni-org] Prevent ivy-prescient

--- setup/setup-prescient.el
+++ setup/setup-prescient.el
@@ -7,7 +7,10 @@
   ;; See https://github.com/raxod502/prescient.el/tree/12ad508c447625918b4d0d93214a6f92f77d5dad#usage
   :after (ivy counsel)
   :config
-  (ivy-prescient-mode t))
+  (ivy-prescient-mode t)
+  ;; Disable ivy-prescient for the command.
+  ;; See https://github.com/raxod502/prescient.el/issues/38
+  (add-to-list 'ivy-sort-functions-alist '(ivy-omni-org . ivy-string<)))
 
 (use-package company-prescient
   :after company
