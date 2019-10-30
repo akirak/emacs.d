ace2b294315023aa3710f2a746f622c7e995a666
Akira Komamura <akira.komamura@gmail.com>
2019-10-31 00:46:06 +0900

    Reconfigure keybindings for browse-at-remote

--- setup/setup-github.el
+++ setup/setup-github.el
@@ -1,4 +1,12 @@
-(use-package forge)
+(use-package forge
+  :config
+  (akirak/bind-browse-at-remote
+    "w" #'forge-browse-dwim
+    "RET" #'forge-browse-post
+    "c" #'forge-browse-commit
+    "t" #'forge-browse-topic
+    "I" #'forge-browse-issues
+    "P" #'forge-browse-pullreqs))
 
 (use-package github-review
   :commands (github-review-forge-pr-at-point))
