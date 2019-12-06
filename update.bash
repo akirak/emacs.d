dd5ccfbb39bb3b6b006ea270038fcf99ff087b92
Akira Komamura <akira.komamura@gmail.com>
2019-12-06 09:25:05 +0900

    Use the functions provided by straight.el to update packages

--- update.bash
+++ update.bash
@@ -55,24 +55,13 @@ section "Updating the submodules in the Emacs configuration..."
 # Update the submodule
 git submodule update --recursive
 
-section "Updating my favorite packages..."
+echo -n "Update the packages? (y/n): "
+read update
+if [[ ${update} = [Yy]* ]]; then
+    section "Updating the packages..."
+    emacs --batch --load init.el --eval '(straight-pull-all)' \
+          --eval '(straight-rebuild-all)'
+fi
 
-# Auto-update packages I want to keep up-to-date
-# It is important to update the MELPA package cache to prevent missing package errors
-cd straight/repos
-for pkg in melpa org ivy counsel swiper org-starter org-reverse-datetree \
-           transient magit org-ql company-mode lsp-mode treemacs; do
-    [[ ! -d $pkg ]] && continue
-    echo "Trying to update $pkg package..."
-    cd $pkg
-    # Run git-safe-update if the program exists
-    if command -v git-safe-update >/dev/null; then
-        git-safe-update
-    else
-        git pull
-    fi
-    if [[ $? -gt 0 ]]; then
-        echo "Failed to update the repository of $pkg package"
-    fi
-    cd ..
-done
+echo "Done."
+exit 0
