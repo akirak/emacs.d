11e95b7b66bc39da2def2c7f09b61579ae8f8cb3
Akira Komamura <akira.komamura@gmail.com>
2019-12-13 01:39:00 +0900

    [straight] Rewrite the function for rebuilding outdated packages
    Run it after 10 minutes of idle time.

--- setup/setup-straight.el
+++ setup/setup-straight.el
@@ -2,35 +2,54 @@
 
 (defvar akirak/straight-rebuilt-outdated-packages nil)
 
+(defun akirak/straight-get-outdated-packages ()
+  (with-current-buffer "*Messages*"
+    (let (result)
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward (rx bol "Source file ‘"
+                                      (group (+? anything))
+                                      "’ newer than byte-compiled file" eol)
+                                  nil t)
+          (let* ((fpath (buffer-substring-no-properties
+                         (match-beginning 1)
+                         (match-end 1)))
+                 (package (when (string-match (rx (group (+ (not (any "/"))))
+                                                  "/" (+ (not (any "/"))) eol)
+                                              fpath)
+                            (match-string 1 fpath)))
+                 (real-package (cl-case package
+                                 ("helm-core" "helm")
+                                 (otherwise package))))
+            (when package
+              (push package result)))))
+      (set-difference (cl-remove-duplicates (nreverse result)
+                                            :test #'string-equal)
+                      akirak/straight-rebuilt-outdated-packages))))
+
 (defun akirak/straight-rebuild-outdated-packages (&optional reload)
-  "FIXME"
+  "Rebuild outdated packages."
   (interactive "P")
-  (let ((packages (with-current-buffer "*Messages*"
-                    (save-excursion
-                      (goto-char (point-min))
-                      (let (result
-                            (prefix (expand-file-name (locate-user-emacs-file "straight/build/"))))
-                        (while (re-search-forward (rx "Source file ‘"
-                                                      (group (+? anything))
-                                                      "’ newer than byte-compiled file")
-                                                  nil t)
-                          (let* ((data (match-data 1))
-                                 (filepath (buffer-substring (nth 2 data) (nth 3 data))))
-                            (when (string-prefix-p prefix filepath)
-                              (let ((package (string-remove-suffix
-                                              "/"
-                                              (file-name-directory
-                                               (string-remove-prefix prefix filepath)))))
-                                (unless (member package akirak/straight-rebuilt-outdated-packages)
-                                  (push package result))))))
-                        result)))))
-    (dolist (package (cl-remove-duplicates packages :test #'string-equal))
-      (straight-rebuild-package package)
+  (let* ((packages (akirak/straight-get-outdated-packages))
+         (total (length packages))
+         (i 1)
+         (start-time (current-time))
+         finish-time
+         package)
+    (while (setq package (pop packages))
+      (message "Rebuilding packages %s (%d/%d)..."
+               package i total)
+      (let (message-log-max)
+        (straight-rebuild-package package))
+      (push package akirak/straight-rebuilt-outdated-packages)
       (when reload
         (load package))
-      (push package akirak/straight-rebuilt-outdated-packages))))
+      (setq i (1+ i)))
+    (setq finish-time (current-time))
+    (message "Finished rebuilding %d package in %.1f seconds."
+             total (float-time (time-subtract finish-time start-time)))))
 
-;; During the first idle of 5 minutes, rebuild the outdated packages.
-(run-with-idle-timer 300 nil #'straight-check-all)
+;; During the first idle of 10 minutes, rebuild the outdated packages.
+(run-with-idle-timer 600 nil #'akirak/straight-rebuild-outdated-packages)
 
 (provide 'setup-straight)
