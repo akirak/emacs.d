8637d993254b91031d703a1fb764b00f8959b621
Akira Komamura <akira.komamura@gmail.com>
2019-12-28 21:53:01 +0900

    [org-edna] Move avy commands to the repository of org-edna

--- setup/setup-org-edna.el
+++ setup/setup-org-edna.el
@@ -1,48 +1,9 @@
 (use-package org-edna
   :after org
-  :straight (org-edna :host github :repo "akirak/org-edna" :branch "edit")
+  :straight (org-edna :host github :repo "akirak/org-edna")
   :config
   (org-edna-load)
   (setq-mode-local org-edna-edit-mode company-backends '(company-capf))
-  (defun akirak/org-edna--add-id (id old)
-    (if (string-match (rx "ids("
-                          (group (+ (any digit "-a-f"))
-                                 (*? (and (+ space) (+ (any digit "-a-f")))))
-                          ")")
-                      old)
-        (let ((new-ids (format "ids(%s %s)" (match-string 1 old) id)))
-          (s-replace (match-string 0 old) new-ids old))
-      (format "%s ids(%s)" old id)))
-  (defun akirak/org-edna-add-id-blocker (id)
-    (cond
-     ((derived-mode-p 'org-mode)
-      (let* ((old (org-entry-get nil "BLOCKER")))
-        (org-set-property "BLOCKER"
-                          (if old
-                              (akirak/org-edna--add-id id old)
-                            (format "ids(%s)" id)))))
-     ((derived-mode-p 'org-agenda-mode)
-      (let* ((buffer-orig (buffer-name))
-             (marker (or (org-get-at-bol 'org-hd-marker)
-                         (org-agenda-error)))
-             (buffer (marker-buffer marker))
-             (pos (marker-position marker)))
-        (with-current-buffer buffer
-          (org-with-wide-buffer
-           (goto-char marker)
-           (let ((old (org-entry-get nil "BLOCKER")))
-             (org-set-property "BLOCKER"
-                               (if old
-                                   (akirak/org-edna--add-id id old)
-                                 (format "ids(%s)" id))))))
-        (org-agenda-redo)))
-     (t (user-error "Unsupported mode"))))
-  (defun akirak/avy-add-org-edna-id-blocker ()
-    (interactive)
-    (unless (derived-mode-p 'org-mode 'org-agenda-mode)
-      (user-error "Not in org-mode or org-agenda-mode"))
-    (let ((id (save-selected-window (org-starter-utils-avy-id))))
-      (akirak/org-edna-add-id-blocker id)))
   (defun akirak/helm-org-rifle-add-edna-blocker-with-id (candidate)
     (unless (derived-mode-p 'org-mode 'org-agenda-mode)
       (user-error "Not in org-mode or org-agenda-mode"))
@@ -53,5 +14,8 @@
       (akirak/org-edna-add-id-blocker id)))
   :hook
   (org-edna-edit-mode . company-mode))
+
+(use-package org-edna-avy
+  :straight org-edna)
 
 (provide 'setup-org-edna)
