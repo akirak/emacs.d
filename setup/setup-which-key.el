a144a2a0e017b12dd83b7b9d9a87d1165324a4d6
Akira Komamura <akira.komamura@gmail.com>
2019-12-29 01:21:02 +0900

    [which-key] Tidy

--- setup/setup-which-key.el
+++ setup/setup-which-key.el
@@ -3,8 +3,7 @@
   :init
   (which-key-mode)
   :config
-  (which-key-setup-side-window-bottom) ; Display a popup window at bottom
-  )
+  (which-key-setup-side-window-bottom))
 
 (defmacro akirak/which-key-add-stripped-prefix (prefix)
   "Add PREFIX as a stripped prefix to `which-key-replacement-alist'."
