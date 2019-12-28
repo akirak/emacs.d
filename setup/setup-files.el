01aae0310d9fe028d80772df0bab5af25f569082
Akira Komamura <akira.komamura@gmail.com>
2019-12-28 22:03:37 +0900

    [files] Bind <f6>R to revert-buffer-with-coding-system

--- setup/setup-files.el
+++ setup/setup-files.el
@@ -1,3 +1,9 @@
+(akirak/bind-file-extra
+  "M" '(nil :wk "chmod")
+  "Mx" #'executable-set-magic
+  "MM" #'set-file-modes
+  "R" #'revert-buffer-with-coding-system)
+
 (use-package trashed
   :straight (:host github :repo "shingo256/trashed"))
 
