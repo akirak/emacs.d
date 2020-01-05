e08b3deaf935d24f1c82fa6865d5e75a18a7a3fc
Akira Komamura <akira.komamura@gmail.com>
2020-01-05 11:18:07 +0900

    [prescient] Allow loading ivy-prescient without loading counsel

--- setup/setup-prescient.el
+++ setup/setup-prescient.el
@@ -5,7 +5,7 @@
 (use-package ivy-prescient
   ;; You must load counsel before ivy-prescient
   ;; See https://github.com/raxod502/prescient.el/tree/12ad508c447625918b4d0d93214a6f92f77d5dad#usage
-  :after (ivy counsel)
+  :after ivy
   :config
   (ivy-prescient-mode t)
   ;; Explicitly disable sorting by ivy-prescient for these commands.
