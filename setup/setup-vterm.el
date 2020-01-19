7f682ef5a377244b6307c62c166dd89c7ff1a420
Akira Komamura <akira.komamura@gmail.com>
2020-01-20 01:56:12 +0900

    [vterm] Add an extended version of vterm-toggle-cd
    with support for prefix arguments.

--- setup/setup-vterm.el
+++ setup/setup-vterm.el
@@ -40,6 +40,54 @@
   ;; vterm-toggle uses pop-to-buffer to display the buffer, but it
   ;; deletes all the other windows by default.
   ;; To disable the behaviour, you have to set this variable to nil.
-  (vterm-toggle-fullscreen-p nil))
+  (vterm-toggle-fullscreen-p nil)
+  (vterm-toggle-use-dedicated-buffer t))
+
+(defun akirak/vterm-find-window ()
+  (catch 'done
+    (walk-window-tree
+     (lambda (w)
+       (when (eq 'vterm-mode
+                 (buffer-local-value 'major-mode (window-buffer w)))
+         (throw 'done w))))))
+
+(defun akirak/vterm-cleanup ()
+  (mapc (lambda (buf)
+          (when (and (eq 'vterm-mode
+                         (buffer-local-value 'major-mode buf))
+                     (not (process-live-p (get-buffer-process buf))))
+            (kill-buffer buf)))
+        (buffer-list)))
+
+(defun akirak/vterm-toggle-cd (&optional arg)
+  "Toggle the vterm window, create a new buffer, or visit an existing buffer."
+  (interactive "P")
+  (akirak/vterm-cleanup)
+  (pcase arg
+    ('(16)
+     (if-let ((buffer-list (--> (buffer-list)
+                                (cl-remove-if-not
+                                 (lambda (buf)
+                                   (eq 'vterm-mode
+                                       (buffer-local-value 'major-mode buf)))
+                                 it)
+                                (mapcar (lambda (buf)
+                                          (cons (buffer-name buf) buf))
+                                        it)))
+              (buffer (frog-menu-read "Select a vterm buffer" buffer-list)))
+         (if-let ((window (akirak/vterm-find-window)))
+             (progn
+               (select-window window)
+               (switch-to-buffer buffer))
+           (pop-to-buffer buffer))
+       (vterm-toggle-cd)))
+    ('(4)
+     (let ((cwd default-directory))
+       (when-let ((window (akirak/vterm-find-window)))
+         (select-window window))
+       (let ((default-directory cwd))
+         (vterm))))
+    (_ (vterm-toggle-cd))))
+
 
 (provide 'setup-vterm)
