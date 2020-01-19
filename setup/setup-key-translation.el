eac9a87e67741a98ff5a0474bb5ba1e801980076
Akira Komamura <akira.komamura@gmail.com>
2020-01-19 17:59:25 +0900

    Configure key translations for non-Crostini

--- setup/setup-key-translation.el
+++ setup/setup-key-translation.el
@@ -12,4 +12,26 @@
   ;; This is quite annoying, so I will use M-` as <f1>.
   "M-`" (kbd "<f1>"))
 
+(define-globalized-minor-mode akirak/emulate-chromeos-mode
+  nil
+  (lambda ()
+    (cond
+     (akirak/emulate-chromeos-mode
+      (dolist (n (number-sequence 1 9))
+        (define-key key-translation-map
+          (kbd (format "s-%d" n)) (kbd (format "<f%d>" n))))
+      (define-key key-translation-map
+        (kbd "s-0") (kbd "<f10>"))
+      (define-key key-translation-map
+        (kbd "s--") (kbd "<f11>"))
+      (define-key key-translation-map
+        (kbd "s-=") (kbd "<f12>")))
+     (t
+      (dolist (n (number-sequence 0 9))
+        (define-key key-translation-map
+          (kbd (format "s-%d" n)) nil))))))
+
+(unless (akirak/running-on-crostini-p)
+  (akirak/emulate-chromeos-mode 1))
+
 (provide 'setup-key-translation)
