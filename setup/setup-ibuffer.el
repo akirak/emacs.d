561d5598c6dad7a6f4e1e7d9cc05c003beacab3b
Akira Komamura <akira.komamura@gmail.com>
2020-02-01 12:03:26 +0900

    [ibuffer] Fix hook definition

--- setup/setup-ibuffer.el
+++ setup/setup-ibuffer.el
@@ -16,10 +16,12 @@
    (lambda (_name root) (abbreviate-file-name root))))
 
 (use-package ibuffer-vc
+  :config
+  (defun akirak/ibuffer-vc ()
+    (ibuffer-vc-set-filter-groups-by-vc-root)
+    (unless (eq ibuffer-sorting-mode 'alphabetic)
+      (ibuffer-do-sort-by-alphabetic)))
   :hook
-  (ibuffer . (defun akirak/ibuffer-vc ()
-               (ibuffer-vc-set-filter-groups-by-vc-root)
-               (unless (eq ibuffer-sorting-mode 'alphabetic)
-                 (ibuffer-do-sort-by-alphabetic)))))
+  (ibuffer . akirak/ibuffer-vc))
 
 (provide 'setup-ibuffer)
