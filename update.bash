c85d50182961505f54cbe3cfd31209008a93e10d
Akira Komamura <akira.komamura@gmail.com>
2020-02-04 00:40:28 +0900

    Meta: Don't run tests

--- update.bash
+++ update.bash
@@ -91,8 +91,5 @@ if [[ ${update} = [Yy]* ]]; then
           --eval '(progn (akirak/straight-rebuild-outdated-packages) (kill-emacs))'
 fi
 
-section "Testing non-GUI startup..."
-emacs -nw --debug-init --eval '(unless (get-buffer "*Backtrace*") (kill-emacs))'
-
 echo "Done."
 exit 0
