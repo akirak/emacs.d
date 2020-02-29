45e0caa825112462f0334238f5cc843b884f1d4f
Akira Komamura <akira.komamura@gmail.com>
2020-03-01 02:08:03 +0900

    [large-files] Prevent the prompt in elisp files; Use use-package

--- setup/setup-large-files.el
+++ setup/setup-large-files.el
@@ -1,20 +1,28 @@
-(setq-default large-file-warning-threshold (* 512 1024))
-
-;; https://stackoverflow.com/questions/18316665/how-to-improve-emacs-performance-when-view-large-file
-(defun my-find-file-check-make-large-file-read-only-hook ()
-  "If a file is over a given size, make the buffer read only."
-  (when (> (buffer-size) (* 512 1024))
-    (setq buffer-read-only t)
-    (buffer-disable-undo)
-    (unless (derived-mode-p 'pdf-view-mode 'nov-mode)
-      (fundamental-mode))))
-
-(add-hook 'find-file-hook 'my-find-file-check-make-large-file-read-only-hook)
-
-(defun akirak/ad-before-while-abort-if-file-too-large (size op-type filename)
-  (not (member (file-name-nondirectory filename) '("TAGS"))))
-
-(advice-add 'abort-if-file-too-large :before-while
-            #'akirak/ad-before-while-abort-if-file-too-large)
+(use-package files
+  :straight (:type built-in)
+  :config/el-patch
+  (defun abort-if-file-too-large (size op-type filename)
+    (when (and (el-patch-add (not (string-match-p (rx (or ".el" ".el.gz") eol) filename)))
+               (el-patch-add (not (member (file-name-nondirectory filename)
+                                          '("TAGS"))))
+               large-file-warning-threshold size
+               (> size large-file-warning-threshold)
+               (not (y-or-n-p (format "File %s is large (%s), really %s? "
+                                      (file-name-nondirectory filename)
+                                      (file-size-human-readable size) op-type))))
+      (user-error "Aborted")))
+  :config
+  ;; https://stackoverflow.com/questions/18316665/how-to-improve-emacs-performance-when-view-large-file
+  (defun my-find-file-check-make-large-file-read-only-hook ()
+    "If a file is over a given size, make the buffer read only."
+    (when (> (buffer-size) (* 512 1024))
+      (setq buffer-read-only t)
+      (buffer-disable-undo)
+      (unless (derived-mode-p 'pdf-view-mode 'nov-mode)
+        (fundamental-mode))))
+  :hook
+  (find-file . my-find-file-check-make-large-file-read-only-hook)
+  :custom
+  (large-file-warning-threshold (* 512 1024)))
 
 (provide 'setup-large-files)
