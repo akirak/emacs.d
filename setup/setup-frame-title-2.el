94af886775eea75621079819baf7028f7e311f50
Akira Komamura <akira.komamura@gmail.com>
2020-03-22 10:51:05 +0900

    Revert the commit for the frame title

--- setup/setup-frame-title-2.el
+++
@@ -1,5 +0,0 @@
-(setq-default frame-title-format
-              `(,(if (akirak/exwm-session-p) "EXWM@" "Emacs@")
-                ,(string-trim-right (shell-command-to-string "uname -n"))))
-
-(provide 'setup-frame-title-2)
