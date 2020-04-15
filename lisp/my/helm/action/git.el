9ae036856ef88a84c0cd99de8af43f4ca02607b7
Akira Komamura <akira.komamura@gmail.com>
2020-04-15 12:04:22 +0900

    Run magit-status after renaming a project via helm

--- lisp/my/helm/action/git.el
+++ lisp/my/helm/action/git.el
@@ -2,6 +2,7 @@
 
 (defconst akirak/helm-git-project-actions
   '(("Magit status" . magit-status)
-    ("Rename directory" . akirak/rename-git-repository)))
+    ("Rename directory and run magit status" . akirak/rename-git-repository-and-status)
+    ("Dired" . dired)))
 
 (provide 'my/helm/action/git)
