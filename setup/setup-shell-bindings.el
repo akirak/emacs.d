0a6d0e23de5784ef892ecdca24cb42692d94acdb
Akira Komamura <akira.komamura@gmail.com>
2020-04-16 10:02:19 +0900

    [shell-bindings] Rewrite C-a and organize code

--- setup/setup-shell-bindings.el
+++ setup/setup-shell-bindings.el
@@ -1,12 +1,25 @@
-(use-package akirak/edit
-  :straight nil)
-
 (general-def
   "C-h" 'backward-delete-char
-  "C-w" 'akirak/kill-region-or-backward-kill-word)
+  "C-w"
+  (defun akirak/kill-region-or-backward-kill-word (&optional arg)
+    "If a region is active, run `kill-region'. Otherwise, run `backward-kill-word'."
+    (interactive "p")
+    (if (region-active-p)
+        (kill-region (region-beginning) (region-end))
+      (backward-kill-word arg))))
 
 (general-def prog-mode-map
-  "C-a" 'akirak/back-to-indentation-or-beginning-of-line)
+  "C-a"
+  (defun akirak/back-to-indentation-or-beginning-of-line (&optional arg)
+    (interactive "P")
+    (if (and (not arg)
+             (or (looking-at "^")
+                 (string-match-p (rx (not (any space)))
+                                 (buffer-substring-no-properties
+                                  (line-beginning-position)
+                                  (point)))))
+        (back-to-indentation)
+      (beginning-of-line))))
 
 (general-def :keymaps 'org-mode-map :package 'org
   "C-a" #'org-beginning-of-line
