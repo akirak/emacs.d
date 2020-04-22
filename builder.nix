70eca655ef539a6293fbc8ef6f05711bb8c4e669
Akira Komamura <akira.komamura@gmail.com>
2020-04-22 23:52:59 +0900

    Tidy: Apply nixfmt

--- builder.nix
+++ builder.nix
@@ -1,13 +1,6 @@
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import <nixpkgs> { };
   emacs = pkgs.emacs;
-  readmeBuilder = (pkgs.emacsPackagesNgGen emacs).emacsWithPackages (epkgs:
-    (with epkgs.melpaPackages; [
-      org-make-toc
-    ]));
-in
-  pkgs.mkShell {
-    buildInputs = [
-      readmeBuilder
-    ];
-  }
+  readmeBuilder = (pkgs.emacsPackagesNgGen emacs).emacsWithPackages
+    (epkgs: (with epkgs.melpaPackages; [ org-make-toc ]));
+in pkgs.mkShell { buildInputs = [ readmeBuilder ]; }
