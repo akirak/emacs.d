d9b68e8e74b24fa36305dd03ef11a457ef56a29a
Akira Komamura <akira.komamura@gmail.com>
2020-05-05 11:58:43 +0900

    mode/major/enum: Fix syntactic error in cl-loop form

--- lisp/my/mode/major/enum.el
+++ lisp/my/mode/major/enum.el
@@ -1,5 +1,5 @@
 (defun akirak/known-major-mode-list ()
-  (cl-loop for sym the symbols of obarray
+  (cl-loop for sym being the symbols of obarray
            when (get sym 'derived-mode-parent)
            collect sym))
 
