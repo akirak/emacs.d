b368f51facac4b37cb9b4e294d8e66a356c239d5
Akira Komamura <akira.komamura@gmail.com>
2020-05-18 19:54:12 +0900

    Fix the recipe of calibredb

--- recipes.el
+++ recipes.el
@@ -1,4 +1,4 @@
-((calibredb :host github :repo "chenyangming/calibredb.el")
+((calibredb :host github :repo "chenyanming/calibredb.el")
  (helm-tail :host github :repo "akirak/helm-tail"
             :branch "feature/source-alist")
  (org-web-tools :host github :repo "akirak/org-web-tools"
