4b82684e428488da928e6269502fd390967f8759
Akira Komamura <akira.komamura@gmail.com>
2020-10-08 00:22:38 +0900

    [japanese-mozc] Disable mozc temporarily
    Stop writing Japanese using Emacs until the Nix build fixes.

--- setup/setup-japanese-mozc.el
+++ setup/setup-japanese-mozc.el
@@ -1,15 +1,18 @@
 ;; Install mozc_emacs_helper as well as mozc.el using Nix
 (straight-use-package '(mozc :type built-in))
 
-(use-package mozc)
+(use-package mozc
+  :disabled t)
 
 (use-package mozc-cand-posframe
+  :disabled t
   :after mozc
   :if (posframe-workable-p)
   :config
   (setq mozc-candidate-style 'posframe))
 
 (use-package mozc-temp
+  :disabled t
   :commands (mozc-temp-convert mozc-temp-convert-dwim))
 
 (provide 'setup-japanese-mozc)
