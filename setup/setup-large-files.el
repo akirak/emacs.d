c0a933e61a057de323a7fea5a8c933f4cc30c127
Akira Komamura <akira.komamura@gmail.com>
2020-10-07 23:45:55 +0900

    [large-files] Disable the workaround temporarily

--- setup/setup-large-files.el
+++ setup/setup-large-files.el
@@ -1,16 +1,18 @@
 (use-package files
   :straight (:type built-in)
-  :config/el-patch
-  (defun abort-if-file-too-large (size op-type filename)
-    (when (and (el-patch-add (not (string-match-p (rx (or ".el" ".el.gz") eol) filename)))
-               (el-patch-add (not (member (file-name-nondirectory filename)
-                                          '("TAGS"))))
-               large-file-warning-threshold size
-               (> size large-file-warning-threshold)
-               (not (y-or-n-p (format "File %s is large (%s), really %s? "
-                                      (file-name-nondirectory filename)
-                                      (file-size-human-readable size) op-type))))
-      (user-error "Aborted")))
+  ;; This does not work in Emacs 27. I have to investigate the issue.
+  ;;
+  ;; :config/el-patch
+  ;; (el-patch-defun abort-if-file-too-large (size op-type filename)
+  ;;   (when (and (el-patch-add (not (string-match-p (rx (or ".el" ".el.gz") eol) filename)))
+  ;;              (el-patch-add (not (member (file-name-nondirectory filename)
+  ;;                                         '("TAGS"))))
+  ;;              large-file-warning-threshold size
+  ;;              (> size large-file-warning-threshold)
+  ;;              (not (y-or-n-p (format "File %s is large (%s), really %s? "
+  ;;                                     (file-name-nondirectory filename)
+  ;;                                     (file-size-human-readable size) op-type))))
+  ;;     (user-error "Aborted")))
   :config
   ;; https://stackoverflow.com/questions/18316665/how-to-improve-emacs-performance-when-view-large-file
   (defun my-find-file-check-make-large-file-read-only-hook ()
