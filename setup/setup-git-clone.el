eca4f9fd2b7c3e0ceb0ee02774c3d920925b23ec
Akira Komamura <akira.komamura@gmail.com>
2020-11-05 15:09:27 +0900

    [git-clone] Parse a branch and a file path if any

--- setup/setup-git-clone.el
+++ setup/setup-git-clone.el
@@ -16,15 +16,23 @@
   "The user name on GitHub.")
 
 (defun akirak/remote-git-repo-clone-1 (repo)
-  (let ((url (akirak/remote-git-repo-url repo))
-        (dest (f-join (akirak/remote-git-repo-clone-parent repo)
-                      (akirak/remote-git-repo-name repo)))
-        (args (transient-args 'magit-clone)))
+  (let* ((url (akirak/remote-git-repo-url repo))
+         (branch (akirak/remote-git-repo-branch repo))
+         (branch (if (member branch '("master" "main"))
+                     nil
+                   branch))
+         (dest (f-join (akirak/remote-git-repo-clone-parent repo)
+                       (concat (akirak/remote-git-repo-name repo)
+                               (if branch
+                                   (concat "@" branch)
+                                 ""))))
+         (args (transient-args 'magit-clone)))
     (if (file-directory-p dest)
         (progn
           (message "Already exists: %s" dest)
           (magit-status dest))
-      (magit-clone-regular url dest args))))
+      (magit-clone-regular url dest `(,(and branch `("-b" ,branch))
+                                      ,@args)))))
 
 (cl-defgeneric akirak/remote-git-repo-clone-default (repo)
   (akirak/remote-git-repo-clone-1 repo))
@@ -36,10 +44,14 @@
 (cl-defgeneric akirak/remote-git-repo-owner (repo))
 (cl-defgeneric akirak/remote-git-repo-name (repo))
 (cl-defgeneric akirak/remote-git-repo-clone-parent (repo))
+(cl-defgeneric akirak/remote-git-repo-branch (_)
+  nil)
+(cl-defgeneric akirak/remote-git-repo-file (_)
+  nil)
 
 ;;;; GitHub repositories (https)
 
-(cl-defstruct akirak/github-https-repo owner name)
+(cl-defstruct akirak/github-https-repo owner name branch file)
 (cl-defmethod akirak/remote-git-repo-url ((repo akirak/github-https-repo))
   (concat "https://github.com/"
           (akirak/github-https-repo-owner repo)
@@ -52,12 +64,16 @@
   (akirak/github-https-repo-name repo))
 (cl-defmethod akirak/remote-git-repo-clone-parent ((repo akirak/github-https-repo))
   (akirak/git-clone-parent-for-host "github.com"))
-(cl-defgeneric akirak/remote-git-repo-url-github-p ((repo akirak/github-https-repo))
+(cl-defmethod akirak/remote-git-repo-url-github-p ((repo akirak/github-https-repo))
   t)
+(cl-defmethod akirak/remote-git-repo-branch ((repo akirak/github-https-repo))
+  (akirak/github-https-repo-branch repo))
+(cl-defmethod akirak/remote-git-repo-file ((repo akirak/github-https-repo))
+  (akirak/github-https-repo-file repo))
 
 ;;;; GitHub repositories (ssh)
 
-(cl-defstruct akirak/github-ssh-repo owner name)
+(cl-defstruct akirak/github-ssh-repo owner name branch file)
 (cl-defmethod akirak/remote-git-repo-url ((repo akirak/github-ssh-repo))
   (concat "git@github.com:"
           (akirak/github-ssh-repo-owner repo)
@@ -70,7 +86,7 @@
   (akirak/github-ssh-repo-name repo))
 (cl-defmethod akirak/remote-git-repo-clone-parent ((repo akirak/github-ssh-repo))
   (akirak/git-clone-parent-for-host "github.com"))
-(cl-defgeneric akirak/remote-git-repo-url-github-p ((repo akirak/github-ssh-repo))
+(cl-defmethod akirak/remote-git-repo-url-github-p ((repo akirak/github-ssh-repo))
   t)
 
 ;;;; Generic Git repositories
@@ -103,32 +119,40 @@
   (let* ((name-pattern (rx bol (group (+? (any alnum "-_."))) eol))
          (path-pattern (rx (group (+ (any alnum "-")))
                            "/"
-                           (group (+ (any alnum "-_.")))))
+                           (group (+? (any alnum "-_.")))))
          (host-pattern (rx (group (+ (not (any "./")))
                                   (+ (and "." (+ (not (any "./"))))))))
          (suffix (rx ".git"))
          (optional-suffix (rx (?  ".git")))
          (github-path-pattern (concat "^" path-pattern "$"))
          (https-pattern (concat "^https://" host-pattern "/" path-pattern
-                                optional-suffix))
-         (ssh-pattern (concat "^git@" host-pattern ":" path-pattern suffix "$")))
-    (pcase-let ((`(,protocol ,url ,host ,owner ,name)
-                 (or (-some->> (s-match name-pattern string)
-                       (append (list nil nil "github.com" akirak/github-login)))
-                     (-some->> (s-match github-path-pattern string)
-                       (cdr)
-                       (append (list nil nil "github.com")))
-                     (-some->> (s-match https-pattern string)
-                       (cons 'https))
-                     (-some->> (s-match ssh-pattern string)
-                       (cons 'ssh)))))
+                                (rx (? (or (and "/blob/" (group (+ (any alnum "-")))
+                                                "/" (group (and (any alnum ".")
+                                                                (+ anything))))
+                                           ".git"
+                                           "/"))
+                                    eol)))
+         (ssh-pattern (concat "^git@" host-pattern ":" path-pattern (rx ".git" eol))))
+    (pcase-let* ((`(,protocol ,url ,host ,owner ,name . ,rest)
+                  (or (-some->> (s-match name-pattern string)
+                        (append (list nil nil "github.com" akirak/github-login)))
+                      (-some->> (s-match github-path-pattern string)
+                        (cdr)
+                        (append (list nil nil "github.com")))
+                      (-some->> (s-match https-pattern string)
+                        (cons 'https))
+                      (-some->> (s-match ssh-pattern string)
+                        (cons 'ssh))))
+                 (`(,branch ,file) rest))
       (cond
        ((and (equal host "github.com") (equal owner akirak/github-login))
         (make-akirak/github-ssh-repo :owner owner
                                      :name (string-remove-suffix ".git" name)))
        ((equal host "github.com")
         (make-akirak/github-https-repo :owner owner
-                                       :name (string-remove-suffix ".git" name)))
+                                       :name (string-remove-suffix ".git" name)
+                                       :branch branch
+                                       :file file))
        ((and host owner name)
         (make-akirak/generic-git-repo :url string
                                       :protocol protocol
