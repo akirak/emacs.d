9cf9153795e1e1c47ffac73c12258f7b82f50d08
Akira Komamura <akira.komamura@gmail.com>
2020-11-07 12:08:28 +0900

    helm/action/git: Add magit-log action

--- lisp/my/helm/action/git.el
+++ lisp/my/helm/action/git.el
@@ -2,11 +2,15 @@
 
 (defconst akirak/helm-git-project-actions
   '(("Magit status" . magit-status)
-    ("Rename directory and run magit status" . akirak/rename-git-repository-and-status)
+    ("Magit log all" . (lambda (dir)
+                          (let ((default-directory dir))
+                            (magit-log-all))))
     ("Find files recursively" . akirak/find-file-recursively)
     ("Dired" . dired)
     ("Term" . (lambda (dir)
                 (let ((default-directory dir))
-                  (vterm))))))
+                  (vterm))))
+    ("Rename directory and run magit status"
+     . akirak/rename-git-repository-and-status)))
 
 (provide 'my/helm/action/git)
