e3ae9dc28121f8254286c201d7f23b3e540907cf
Akira Komamura <akira.komamura@gmail.com>
2020-11-27 11:17:56 +0900

    [org-edna] Drop the company configuration

--- setup/setup-org-edna.el
+++ setup/setup-org-edna.el
@@ -1,18 +1,5 @@
 (use-package org-edna
-  :after org
-  :config
-  ;; (org-edna-load)
-  (setq-mode-local org-edna-edit-mode company-backends '(company-capf))
-  ;; (defun akirak/helm-org-rifle-add-edna-blocker-with-id (candidate)
-  ;;   (unless (derived-mode-p 'org-mode 'org-agenda-mode)
-  ;;     (user-error "Not in org-mode or org-agenda-mode"))
-  ;;   (let ((id (with-current-buffer (car candidate)
-  ;;               (org-with-wide-buffer
-  ;;                (goto-char (cdr candidate))
-  ;;                (org-id-get-create t)))))
-  ;;     (org-edna-add-id-blocker id)))
-  :hook
-  (org-edna-edit-mode . company-mode))
+  :after org)
 
 (use-package org-linker
   :straight (:host github :repo "toshism/org-linker"))
