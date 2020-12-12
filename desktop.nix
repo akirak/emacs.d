47f92f89193407fca4ba4a27a3d66ea80de38061
Akira Komamura <akira.komamura@gmail.com>
2020-12-12 23:25:05 +0900

    Set the absolute path of an icon

--- desktop.nix
+++ desktop.nix
@@ -6,7 +6,7 @@ in
 
   xdg.menu.applications.emacs-akirak-2018 = {
     Name = "Emacs (akirak/emacs.d)";
-    Icon = "emacs";
+    Icon = "${pkgs.emacs}/share/icons/hicolor/scalable/apps/emacs.svg";
     Exec = "nix run --no-update-lock-file ${toString ./.}";
     StartupWMClass = "Emacs";
   };
