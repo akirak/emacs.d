3730ef6e4b078dc43f03bc1efcabe1b07e01b825
Akira Komamura <akira.komamura@gmail.com>
2020-12-13 11:57:43 +0900

    [straight] Supress the message when there is no package rebuilt

--- setup/setup-straight.el
+++ setup/setup-straight.el
@@ -32,19 +32,20 @@
          (start-time (current-time))
          finish-time
          package)
-    (while (setq package (pop packages))
-      (message "Rebuilding packages %s (%d/%d)..."
-               package i total)
-      (let (message-log-max)
-        (straight-rebuild-package package))
-      (push package akirak/straight-rebuilt-outdated-packages)
-      (when reload
-        (load package))
-      (setq i (1+ i)))
-    (setq finish-time (current-time))
-    (garbage-collect)
-    (message "Finished rebuilding %d package in %.1f seconds."
-             total (float-time (time-subtract finish-time start-time)))))
+    (unless (null packages)
+      (while (setq package (pop packages))
+        (message "Rebuilding packages %s (%d/%d)..."
+                 package i total)
+        (let (message-log-max)
+          (straight-rebuild-package package))
+        (push package akirak/straight-rebuilt-outdated-packages)
+        (when reload
+          (load package))
+        (setq i (1+ i)))
+      (setq finish-time (current-time))
+      (garbage-collect)
+      (message "Finished rebuilding %d package in %.1f seconds."
+               total (float-time (time-subtract finish-time start-time))))))
 
 ;; After an idle of 10 minutes, rebuild the outdated packages.
 (run-with-idle-timer 600 nil #'akirak/straight-rebuild-outdated-packages)
