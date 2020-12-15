cfbcdb09a7783597d0b31a714e4d9f0f05904d98
Akira Komamura <akira.komamura@gmail.com>
2020-12-16 00:51:16 +0900

    Add screenshot command

--- init.el
+++ init.el
@@ -945,3 +945,18 @@ outcommented org-mode headers)."
 (akirak/bind-admin
   "r" '(nil :wk "remote")
   "rk" #'helm-delete-tramp-connection)
+
+;;;; Others
+(general-def
+  "C-c z"
+  ;; https://www.reddit.com/r/emacs/comments/idz35e/emacs_27_can_take_svg_screenshots_of_itself/
+  (defun screenshot-svg ()
+    "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+    (interactive)
+    (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+           (data (x-export-frames nil 'svg)))
+      (with-temp-file filename
+        (insert data))
+      (kill-new filename)
+      (message filename))))
