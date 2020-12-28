5a308e0b34cba40a98c6718cb6d23b3504755a2f
Akira Komamura <akira.komamura@gmail.com>
2020-12-28 12:54:55 +0900

    [perfect-margin] Turn on conditionally

--- setup/setup-perfect-margin.el
+++ setup/setup-perfect-margin.el
@@ -1,7 +1,33 @@
 (use-package perfect-margin
   :straight (:host github :repo "akirak/perfect-margin")
+  :if (window-system)
   :config
-  (perfect-margin-mode 1)
+
+  (defcustom akirak/perfect-margin-threshold 170
+    "FIXME"
+    :type 'number)
+
+  (defsubst akirak/perfect-margin-threshold-p ()
+    (> (frame-width) akirak/perfect-margin-threshold))
+
+  (defun akirak/perfect-margin-conditionally (&rest _args)
+    (when (frame-size-changed-p)
+      (if (akirak/perfect-margin-threshold-p)
+          (if perfect-margin-mode
+              (perfect-margin-margin-windows)
+            (perfect-margin-mode 1))
+        (when perfect-margin-mode
+          (perfect-margin-mode -1)))))
+
+  (advice-add 'perfect-margin-margin-frame
+              :override
+              #'akirak/perfect-margin-conditionally)
+
+  (add-hook 'window-size-change-functions
+            #'akirak/perfect-margin-conditionally)
+
+  (perfect-margin-mode (or (akirak/perfect-margin-threshold-p) -1))
+
   :custom
   (perfect-margin-visible-width 92)
   (perfect-margin-ignore-modes '(exwm-mode
