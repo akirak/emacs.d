147dec3f49fab578d716fe09f58a072c7e392dc7
Akira Komamura <akira.komamura@gmail.com>
2021-01-01 17:13:55 +0900

    helm/source/web: Allow choosing URLs in visible areas

--- lisp/my/helm/source/web.el
+++ lisp/my/helm/source/web.el
@@ -10,8 +10,31 @@
   ((candidates :initform 'akirak/browse-url-bookmarks)
    (action :initform '(("Browse" . akirak/browse-url-or-query)))))
 
+(defun akirak/helm-window-visible-url-candidates (window)
+  (let ((buffer (window-buffer window))
+
+        (start (window-start window))
+        (end (window-end window))
+        result)
+    (with-current-buffer buffer
+      (save-excursion
+        (goto-char start)
+        (while (re-search-forward (rx "http" (?  "s") "://") end t)
+          (when-let (url (or (thing-at-point 'url)
+                             (url-get-url-at-point)))
+            (push (cons (format "%s (%s)" url (buffer-name buffer))
+                        url)
+                  result)))))
+    (nreverse result)))
+
 (defun akirak/helm-web-sources ()
-  (list (helm-make-source "Browser bookmarks"
+  (list (helm-build-sync-source "URLs in visible windows"
+          :candidates
+          (->> (window-list)
+               (-map #'akirak/helm-window-visible-url-candidates)
+               (-flatten-n 1))
+          :action 'akirak/helm-web-dummy-source-actions)
+        (helm-make-source "Browser bookmarks"
             'akirak/helm-source-browser-bookmarks)
         (helm-make-source "Query history"
             'akirak/helm-source-sync-web-query
