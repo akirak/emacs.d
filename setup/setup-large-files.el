f8b9cf15347716d86720694ca09db7c393636b05
Akira Komamura <akira.komamura@gmail.com>
2021-01-04 10:51:30 +0900

    [large-files] Change the threshold to 1 MB

--- setup/setup-large-files.el
+++ setup/setup-large-files.el
@@ -17,7 +17,7 @@
   ;; https://stackoverflow.com/questions/18316665/how-to-improve-emacs-performance-when-view-large-file
   (defun my-find-file-check-make-large-file-read-only-hook ()
     "If a file is over a given size, make the buffer read only."
-    (when (> (buffer-size) (* 512 1024))
+    (when (> (buffer-size) large-file-warning-threshold)
       (setq buffer-read-only t)
       (buffer-disable-undo)
       (unless (derived-mode-p 'pdf-view-mode 'nov-mode
@@ -26,6 +26,6 @@
   :hook
   (find-file . my-find-file-check-make-large-file-read-only-hook)
   :custom
-  (large-file-warning-threshold (* 512 1024)))
+  (large-file-warning-threshold (* 1024 1024)))
 
 (provide 'setup-large-files)
