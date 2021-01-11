0c50f9e364700ffce1a14e6ee7de289b6523965e
Akira Komamura <akira.komamura@gmail.com>
2021-01-11 18:43:00 +0900

    [reformatter] Add mix format

--- setup/setup-reformatter.el
+++ setup/setup-reformatter.el
@@ -7,7 +7,14 @@
 
   (reformatter-define prettier
     :program "prettier"
-    :args (list "--stdin-filepath" (buffer-file-name))))
+    :args (list "--stdin-filepath" (buffer-file-name)))
+
+  (reformatter-define mix-format
+    :program "mix"
+    :mode t
+    ;; In "mix format", if any of the files is -, then the output is
+    ;; read from stdin and written to stdout.
+    :args '("format" "-")))
 
 (use-package purty
   :after purescript-mode
