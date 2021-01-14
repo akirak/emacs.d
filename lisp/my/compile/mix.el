a58a4dafd91a48ada7ca8af9e1c921b507f38754
Akira Komamura <akira.komamura@gmail.com>
2021-01-14 23:22:20 +0900

    compile/mix: Cache the mix command list

--- lisp/my/compile/mix.el
+++ lisp/my/compile/mix.el
@@ -1,12 +1,19 @@
+(defvar akirak/mix-command-alist-cache nil)
+
 (defun akirak/mix-command-alist ()
-  (->> (process-lines "mix" "help")
-       (-map (lambda (s)
-               (save-match-data
-                 (when (string-match (rx bol (group "mix" (* (not (any "#"))))
-                                         " # " (group (+ anything)) eol)
-                                     s)
-                   (cons (string-trim-right (match-string 1 s))
-                         (match-string 2 s))))))
-       (delq nil)))
+  (or (alist-get default-directory akirak/mix-command-alist-cache
+                 nil nil #'string-equal)
+      (let ((result (->> (process-lines "mix" "help")
+                         (-map (lambda (s)
+                                 (save-match-data
+                                   (when (string-match (rx bol (group "mix" (* (not (any "#"))))
+                                                           " # " (group (+ anything)) eol)
+                                                       s)
+                                     (cons (string-trim-right (match-string 1 s))
+                                           (match-string 2 s))))))
+                         (delq nil))))
+        (push (cons default-directory result)
+              akirak/mix-command-alist-cache)
+        result)))
 
 (provide 'my/compile/mix)
