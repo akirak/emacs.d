dc622afefb750edba9669bbf97ef748a3fa9209e
Akira Komamura <akira.komamura@gmail.com>
2021-01-15 10:58:21 +0900

    Add a macro for adding/updating the git identity

--- setup/setup-repos.el
+++ setup/setup-repos.el
@@ -4,6 +4,16 @@
   :general
   (:keymaps 'magit-status-mode-map :package 'magit
             "I" #'git-identity-info)
+
+  :config
+  (cl-defmacro akirak/git-identity-add (address &rest args)
+    (declare (indent 1))
+    (let ((cell (assoc address git-identity-list)))
+      (if cell
+          (setcdr cell args)
+        (push (cons address args)
+              git-identity-list))))
+
   :custom
   (git-identity-magit-mode t))
 
