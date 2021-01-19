0868803d91f87db81c1affe66f85a7824df53c57
Akira Komamura <akira.komamura@gmail.com>
2021-01-20 01:08:13 +0900

    Add japanese-skk

---
+++ setup/setup-japanese-skk.el
@@ -0,0 +1,19 @@
+;; I use SKK mostly for writing English words denotated in katakana.
+(use-package skk
+  :straight ddskk
+  :config
+  (require 'skk-leim)
+  ;; There is skk-dicts package in Nixpkgs, but it is poorly packaged
+  ;; in that all files are directory under share directory.
+  ;;
+  ;; I don't want to install it, so I will build the expression directly.
+  (setq skk-large-jisyo (f-join (string-trim-right
+                                 (call-process-with-args "nix-build" "-E"
+                                   "(import <nixpkgs> {}).skk-dicts"))
+                                "share"
+                                "SKK-JISYO.L"))
+  :custom
+  (skk-jisyo-code 'utf-8-unix)
+  (skk-jisx0213-prohibit t))
+
+(provide 'setup-japanese-skk)
