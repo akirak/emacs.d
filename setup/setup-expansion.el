28501fdf0b4b92fde41d63f3266696659fff95be
Akira Komamura <akira.komamura@gmail.com>
2021-02-10 18:55:36 +0900

    [expansion] Add experimental infrastructure for yankpad categories

--- setup/setup-expansion.el
+++ setup/setup-expansion.el
@@ -65,8 +65,92 @@
                                                        (cadr snippets)))
                                          snippets))))))
       (yankpad--run-snippet (assoc (completing-read "Snippet: " snippets) snippets))))
+
+  ;; I do not want to use the projectile integration.
   (advice-add #'yankpad-local-category-to-projectile
               :override (lambda ()))
+
+  (defun akirak/yankpad-get-category-data ()
+    (let ((olp (-map #'substring-no-properties
+                     (org-get-outline-path t t))))
+      (list (-last-item olp)
+            :olp olp
+            :include (-some--> (org-entry-get nil "INCLUDE")
+                       (split-string it "|")))))
+
+  (defun akirak/yankpad-helm-category-candidates ()
+    (let ((data (org-ql-select yankpad-file
+                  `(level ,yankpad-category-heading-level)
+                  :action #'akirak/yankpad-get-category-data)))
+      (cl-labels
+          ((inherited-categories
+            (category)
+            (let* ((plist (cdr (assoc category data)))
+                   (categories (plist-get plist :include)))
+              (append categories
+                      (-flatten-n 1 (-map #'inherited-categories categories))))))
+        (-map (pcase-lambda (`(,name . ,plist))
+                (cons (let ((categories (inherited-categories name)))
+                        (concat (org-format-outline-path
+                                 (plist-get plist :olp))
+                                (if categories
+                                    (format " (inherits %s)"
+                                            (string-join categories ","))
+                                  "")))
+                      name))
+              data))))
+
+  (defvar akirak/yankpad-org-clock-category nil)
+
+  (with-eval-after-load 'org-clock
+    (add-hook 'org-clock-in-hook
+              (defun akirak/yankpad-set-org-clock-category ()
+                "Set `akirak/yankpad-org-clock-category' based on the Org context."
+                (let* ((all-categories (yankpad--categories))
+                       (choice (or (org-entry-get nil "YANKPAD_CATEGORY" t)
+                                   (org-get-category))))
+                  (setq akirak/yankpad-org-clock-category
+                        (when (and choice
+                                   (member choice all-categories))
+                          choice)))))
+    (add-hook 'org-clock-out-hook
+              (defun akirak/yankpad-unset-org-clock-category ()
+                (setq akirak/yankpad-org-clock-category nil))))
+
+  (general-add-hook '(find-file-hook
+                      after-revert-hook
+                      scratch-create-buffer-hook)
+                    (defun akirak/yankpad-set-category-from-org-clock ()
+                      (yankpad-set-local-category (or akirak/yankpad-org-clock-category
+                                                      yankpad-default-category))))
+
+  (defun akirak/yankpad-set-category-explicitly (category)
+    (yankpad-set-local-category category)
+    (message "Set the yankpad category to %x" category)
+    (when (and (org-clocking-p)
+               (yes-or-no-p "Record this category to the clocked Org file?"))
+      (let (ancestors)
+        (with-current-buffer (marker-buffer org-clock-marker)
+          (org-with-wide-buffer
+           (goto-char org-clock-marker)
+           (while (org-up-heading-safe)
+             (push (propertize (thing-at-point 'line t)
+                               'position (point))
+                   ancestors))
+           (let* ((heading (completing-read "Heading to add the category: "
+                                            (nreverse ancestors)))
+                  (marker (get-char-property 0 'position heading)))
+             (org-entry-put point "YANKPAD_CATEGORY" category)))))))
+
+  (defvar akirak/yankpad-helm-category-source
+    (helm-build-sync-source "Categories from yankpad-file"
+      :candidates #'akirak/yankpad-helm-category-candidates
+      :action #'akirak/yankpad-set-category-explicitly))
+
+  (defun akirak/yankpad-helm-set-category ()
+    (interactive)
+    (helm :prompt (format "Yankpad category [%s]: " yankpad-category)
+          :sources akirak/yankpad-helm-category-source))
 
   (setq yankpad-auto-category-functions
         (list #'yankpad-major-mode-category
@@ -74,14 +158,14 @@
                 (when-let (filename (buffer-file-name (or (buffer-base-buffer)
                                                           (current-buffer))))
                   (file-name-nondirectory filename)))
-              (defun akirak/yankpad-org-category ()
-                (and (eq major-mode 'org-mode)
-                     (when-let (filename (buffer-file-name (org-base-buffer (current-buffer))))
-                       (cond
-                        ((member (expand-file-name filename) org-starter-known-files)
-                         (file-name-base filename))
-                        ((when-let (plist (org-multi-wiki-entry-file-p))
-                           (symbol-name (plist-get plist :namespace))))))))
+              ;; (defun akirak/yankpad-org-file-category ()
+              ;;   (and (eq major-mode 'org-mode)
+              ;;        (when-let (filename (buffer-file-name (org-base-buffer (current-buffer))))
+              ;;          (cond
+              ;;           ((member (expand-file-name filename) org-starter-known-files)
+              ;;            (file-name-base filename))
+              ;;           ((when-let (plist (org-multi-wiki-entry-file-p))
+              ;;              (symbol-name (plist-get plist :namespace))))))))
               (defun akirak/yankpad-project-category ()
                 (and (featurep 'project)
                      (when-let* ((name (-some-> (project-current)
@@ -108,7 +192,7 @@
 (define-key global-map (kbd "C-x a") (make-sparse-keymap))
 (general-def :prefix "C-x a"
   "a" #'yankpad-append-category
-  "c" #'yankpad-set-category
+  "c" #'akirak/yankpad-helm-set-category
   "p" #'yankpad-aya-persist
   "C-c" #'yankpad-capture-snippet)
 
