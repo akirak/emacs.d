1aa5adbc2978689fd33fa80755e0aa4a8c02de01
Akira Komamura <akira.komamura@gmail.com>
2021-02-20 15:48:51 +0900

    [edit] Remove unused keybindings

--- setup/setup-edit.el
+++ setup/setup-edit.el
@@ -1,11 +1,3 @@
 ;; Mode-agnostic editing commands
 
-(akirak/bind-generic
-  "k f" 'flush-lines
-  "k l" 'delete-blank-lines)
-
-(akirak/bind-generic
-  "r p" #'projectile-replace
-  "r M-p" #'projectile-replace-regexp)
-
 (provide 'setup-edit)
