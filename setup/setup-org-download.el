ecdd75535a1e65f0eb42c6eaec1fecdb982bc51a
Akira Komamura <akira.komamura@gmail.com>
2021-04-24 17:09:15 +0900

    [org-download] Use flameshot for taking screenshots on Linux

--- setup/setup-org-download.el
+++ setup/setup-org-download.el
@@ -1,7 +1,11 @@
 (use-package org-download
+  :config
+  (cond
+   ((and (eq system-type 'gnu/linux)
+         (eq window-system 'x))
+    (setq-default org-download-screenshot-method "flameshot gui --raw > %s")))
   :custom
   (org-download-image-dir "image")
-  (org-download-edit-cmd "pinta %s")
-  (org-download-screenshot-method "scrot -s %s"))
+  (org-download-edit-cmd "pinta %s"))
 
 (provide 'setup-org-download)
