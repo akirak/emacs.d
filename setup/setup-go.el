c8bd9a800c63a75f3a76879254cfae0e0dc1b4bb
Akira Komamura <akira.komamura@gmail.com>
2021-06-06 21:48:04 +0900

    Re-add golang

--- setup/setup-go.el
+++ setup/setup-go.el
@@ -1,23 +1,7 @@
-(use-package go-mode)
-
-;; Use lsp-mode
-
-(use-package go-rename
-  ;; Prefer LSP.
-  :disabled t
-  :after go-mode)
-
-(use-package go-eldoc
-  ;; Prefer LSP.
-  :disabled t
-  :after go-mode
-  :hook
-  (go-mode . go-eldoc-setup))
-
-(use-package company-go
-  ;; Prefer LSP.
-  :disabled t
-  :after (company go-mode)
-  :company go-mode)
+(use-package go-mode
+  :config
+  (add-hook 'go-mode-hook
+            (defun akirak/go-setup ()
+              (add-hook 'before-save-hook #'gofmt nil t))))
 
 (provide 'setup-go)
