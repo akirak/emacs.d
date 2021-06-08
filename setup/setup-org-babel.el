3a96aa8cdcaf1ea37e4e11672233fa975ad6d23d
Akira Komamura <akira.komamura@gmail.com>
2021-06-09 03:06:21 +0900

    Cosmetics: Organize org-babel backends

--- setup/setup-org-babel.el
+++ setup/setup-org-babel.el
@@ -6,24 +6,34 @@
     (org-babel-do-load-languages 'org-babel-load-languages
                                  org-babel-load-languages))
   (setq-default org-babel-load-languages
-                '((emacs-lisp . t)
-                  (shell . t)
-                  (sqlite . t)
-                  (graphql . t)
+                '((translate . t)
                   (mermaid . t)
                   (erd . t)
-                  (translate . t)
-                  (restclient . t)))
+                  (shell . t)
+                  (emacs-lisp . t)
+                  (sqlite . t)
+                  (restclient . t)
+                  (graphql . t)))
   :hook
   (emacs-startup . akirak/org-babel-do-load-languages)
   :custom
   (org-confirm-babel-evaluate nil))
 
-(use-package ob-async)
+;;;; Natural languages
 
-;;;; Language supports
+(use-package ob-translate
+  :after ob)
 
-;;;;; Contributed packages shipped with the main org package
+;;;; Diagramming
+
+(use-package ob-mermaid
+  :after ob)
+
+(use-package ob-erd
+  :straight (:host github :repo "akirak/ob-erd")
+  :after ob)
+
+;;;; Everyday scripting
 
 (use-package ob-shell
   :straight (:type built-in))
@@ -34,22 +44,12 @@
 (use-package ob-sqlite
   :straight (:type built-in))
 
-;;;;; Third-party packages
-
-(use-package ob-mermaid
-  :after ob)
+;;;; Web
 
 (use-package ob-restclient
   :after ob)
 
-(use-package ob-erd
-  :straight (:host github :repo "akirak/ob-erd")
-  :after ob)
-
 (use-package ob-graphql
-  :after ob)
-
-(use-package ob-translate
   :after ob)
 
 (provide 'setup-org-babel)
