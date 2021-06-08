a593b71472b3f04cd52436783045a6b93d9d3453
Akira Komamura <akira.komamura@gmail.com>
2021-06-09 02:44:48 +0900

    Drop org-babel backends I don't think I'll use

--- setup/setup-org-babel.el
+++ setup/setup-org-babel.el
@@ -7,19 +7,13 @@
                                  org-babel-load-languages))
   (setq-default org-babel-load-languages
                 '((emacs-lisp . t)
-                  (java . t)
                   (shell . t)
-                  (elixir . t)
-                  (python . t)
                   (sqlite . t)
-                  (go . t)
                   (graphql . t)
                   (mermaid . t)
                   (erd . t)
-                  (dot . t)
-                  (ditaa . t)
-                  (restclient . t)
-                  (typescript . t)))
+                  (translate . t)
+                  (restclient . t)))
   :hook
   (emacs-startup . akirak/org-babel-do-load-languages)
   :custom
@@ -34,24 +28,11 @@
 (use-package ob-shell
   :straight (:type built-in))
 
-(use-package ob-python
-  :straight (:type built-in))
-
 (use-package ob-emacs-lisp
   :straight (:type built-in))
 
 (use-package ob-sqlite
   :straight (:type built-in))
-
-(use-package ob-dot
-  :after ob
-  :straight nil
-  :init
-  (add-to-list 'org-src-lang-modes '("dot" . graphviz-dot)))
-
-(use-package ob-ditaa
-  :after ob
-  :straight nil)
 
 ;;;;; Third-party packages
 
@@ -59,17 +40,6 @@
   :after ob)
 
 (use-package ob-restclient
-  :after ob)
-
-;; You'll need tsc and node in the path to use this package.
-(use-package ob-typescript
-  :after ob)
-
-(use-package ob-elixir
-  :straight (:host github :repo "akirak/ob-elixir" :branch "lexical-binding")
-  :after ob)
-
-(use-package ob-go
   :after ob)
 
 (use-package ob-erd
