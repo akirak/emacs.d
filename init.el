2c5bcc2e1fc9ec94d3d4effb2d3f1307975833b4
Akira Komamura <akira.komamura@gmail.com>
2021-08-31 21:56:09 +0900

    Disable remote sources, since it takes seconds to load it

--- init.el
+++ init.el
@@ -556,10 +556,11 @@ connection identities of recent files."
     (helm :prompt "Directory/repository: "
           :sources
           (list akirak/helm-directory-bookmark-as-git-source
-                akirak/helm-magit-list-repos-source
-                akirak/helm-toplevel-repos-submodules-source
-                akirak/helm-remote-repo-dummy-source
-                akirak/helm-github-following-source)))
+                ;; Disabled sources
+                ;; akirak/helm-toplevel-repos-submodules-source
+                ;; akirak/helm-remote-repo-dummy-source
+                ;; akirak/helm-github-following-source
+                akirak/helm-magit-list-repos-source)))
   "C-x j"
   (defun akirak/switch-to-org-buffer ()
     (interactive)
