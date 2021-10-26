e76060c37985bbb638d63e5e6c4a60d524cd85b7
Akira Komamura <akira.komamura@gmail.com>
2021-10-26 12:15:25 +0900

    Switch back the source of org-ql to the upstream

--- recipes.el
+++ recipes.el
@@ -14,8 +14,6 @@
                            :files ("helm-org-recent-headings.el"))
  (helm-tail :host github :repo "akirak/helm-tail"
             :branch "feature/source-alist")
- ;; Use my development branch until alphapapa is back
- (org-ql :host github :repo "akirak/org-ql" :branch "devel")
  (org-web-tools :host github :repo "akirak/org-web-tools"
                 :branch "encoding")
  (org-multi-wiki :host github :repo "akirak/org-multi-wiki" :branch "devel")
