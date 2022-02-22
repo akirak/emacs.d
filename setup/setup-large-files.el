c5055c8d8e8d91e6143a9a87e13fc11ac227afd6
Akira Komamura <akira.komamura@gmail.com>
2022-02-22 18:53:31 +0900

    Allow opening large files in org-mode
    I am currently migrating from org-journal to several large org-reverse-datetree
    files setup. It should allow large Org files.

--- setup/setup-large-files.el
+++ setup/setup-large-files.el
@@ -17,7 +17,8 @@
   ;; https://stackoverflow.com/questions/18316665/how-to-improve-emacs-performance-when-view-large-file
   (defun my-find-file-check-make-large-file-read-only-hook ()
     "If a file is over a given size, make the buffer read only."
-    (when (> (buffer-size) large-file-warning-threshold)
+    (when (and (> (buffer-size) large-file-warning-threshold)
+               (not (derived-mode-p 'org-mode)))
       (setq buffer-read-only t)
       (buffer-disable-undo)
       (unless (derived-mode-p 'pdf-view-mode 'nov-mode
